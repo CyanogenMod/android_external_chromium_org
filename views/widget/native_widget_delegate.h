CyanogenMod/android_external_chromium_org

commit 10a1c684204fa0d5028be61fcbeaf668400d594c

Start working on compositor tree painting.
As a first step, maintain a skia canvas in each transformed view. Each
transformed view will acquire a texture-id for the canvas it owns. Whenever the
canvas is updated, it will trigger a refresh in the compositor tree.

BUG=none
TEST=none

Review URL: http://codereview.chromium.org/6594125

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78149 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: c797cd43680b525ea771dc226ca353078c5c56f1

views/widget/native_widget_delegate.h
@@ -7,6 +7,7 @@
 #pragma once
 
 namespace gfx {
+class Canvas;
 class Size;
 }
 
@@ -35,6 +36,10 @@ class NativeWidgetDelegate {
 
   // Returns true if the delegate has a FocusManager.
   virtual bool HasFocusManager() const = 0;
+
+  // Paints the rootview in the canvas. This will also refresh the compositor
+  // tree if necessary when accelerated painting is enabled.
+  virtual void OnPaint(gfx::Canvas* canvas) = 0;
 };
 
 }  // namespace internal

views/widget/widget.cc
@@ -6,6 +6,7 @@
 
 #include "base/logging.h"
 #include "base/message_loop.h"
+#include "ui/gfx/compositor.h"
 #include "views/focus/view_storage.h"
 #include "views/widget/default_theme_provider.h"
 #include "views/widget/root_view.h"
@@ -264,6 +265,11 @@ bool Widget::HasFocusManager() const {
   return !!focus_manager_.get();
 }
 
+void Widget::OnPaint(gfx::Canvas* canvas) {
+  GetRootView()->Paint(canvas);
+  RefreshCompositeTree();
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // Widget, FocusTraversable implementation:
 
@@ -309,6 +315,31 @@ void Widget::DestroyRootView() {
 
 void Widget::ReplaceFocusManager(FocusManager* focus_manager) {
   focus_manager_.reset(focus_manager);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Widget, private:
+
+void Widget::RefreshCompositeTree() {
+  if (!EnsureCompositor())
+    return;
+
+  compositor_->NotifyStart();
+  root_view_->PaintComposite(compositor_.get());
+  compositor_->NotifyEnd();
+}
+
+bool Widget::EnsureCompositor() {
+  if (compositor_.get())
+    return true;
+
+  // TODO(sad): If there is a parent Widget, then use the same compositor
+  //            instead of creating a new one here.
+  gfx::AcceleratedWidget widget = native_widget_->GetAcceleratedWidget();
+  if (widget != gfx::kNullAcceleratedWidget)
+    compositor_ = ui::Compositor::Create(widget);
+
+  return compositor_.get() != NULL;
 }
 
 }  // namespace views

views/widget/widget.h
@@ -8,6 +8,7 @@
 
 #include <vector>
 
+#include "base/ref_counted.h"
 #include "base/scoped_ptr.h"
 #include "ui/base/accessibility/accessibility_types.h"
 #include "ui/gfx/native_widget_types.h"
@@ -15,6 +16,7 @@
 #include "views/widget/native_widget_delegate.h"
 
 namespace gfx {
+class Canvas;
 class Path;
 class Point;
 class Rect;
@@ -22,6 +24,7 @@ class Rect;
 
 namespace ui {
 class Accelerator;
+class Compositor;
 class OSExchangeData;
 class ThemeProvider;
 }
@@ -272,6 +275,7 @@ class Widget : public internal::NativeWidgetDelegate,
   virtual void OnNativeWidgetCreated() OVERRIDE;
   virtual void OnSizeChanged(const gfx::Size& new_size) OVERRIDE;
   virtual bool HasFocusManager() const OVERRIDE;
+  virtual void OnPaint(gfx::Canvas* canvas) OVERRIDE;
 
   // Overridden from FocusTraversable:
   virtual FocusSearch* GetFocusSearch() OVERRIDE;
@@ -299,6 +303,14 @@ class Widget : public internal::NativeWidgetDelegate,
   void ReplaceFocusManager(FocusManager* focus_manager);
 
  private:
+  // Refresh the compositor tree. This is called by a View whenever its texture
+  // is updated.
+  void RefreshCompositeTree();
+
+  // Try to create a compositor if one hasn't been created yet. Returns false if
+  // a compositor couldn't be created.
+  bool EnsureCompositor();
+
   NativeWidget* native_widget_;
 
   // Non-owned pointer to the Widget's delegate.  May be NULL if no delegate is
@@ -322,6 +334,9 @@ class Widget : public internal::NativeWidgetDelegate,
   // Valid for the lifetime of RunShellDrag(), indicates the view the drag
   // started from.
   View* dragged_view_;
+
+  // The compositor for accelerated drawing.
+  scoped_refptr<ui::Compositor> compositor_;
 
   DISALLOW_COPY_AND_ASSIGN(Widget);
 };

views/widget/widget_gtk.cc
@@ -955,7 +955,7 @@ gboolean WidgetGtk::OnPaint(GtkWidget* widget, GdkEventExpose* event) {
   gfx::CanvasSkiaPaint canvas(event);
   if (!canvas.is_empty()) {
     canvas.set_composite_alpha(is_transparent());
-    GetRootView()->Paint(&canvas);
+    delegate_->OnPaint(&canvas);
   }
   return false;  // False indicates other widgets should get the event as well.
 }
@@ -1268,6 +1268,11 @@ void WidgetGtk::HandleGrabBroke() {
 
 RootView* WidgetGtk::CreateRootView() {
   return new RootView(this);
+}
+
+gfx::AcceleratedWidget WidgetGtk::GetAcceleratedWidget() {
+  DCHECK(window_contents_ && window_contents_->window);
+  return GDK_WINDOW_XID(window_contents_->window);
 }
 
 gboolean WidgetGtk::OnWindowPaint(GtkWidget* widget, GdkEventExpose* event) {

views/widget/widget_win.cc
@@ -820,7 +820,7 @@ LRESULT WidgetWin::OnNotify(int w_param, NMHDR* l_param) {
 void WidgetWin::OnPaint(HDC dc) {
   scoped_ptr<gfx::CanvasPaint> canvas(
       gfx::CanvasPaint::CreateCanvasPaint(hwnd()));
-  GetRootView()->Paint(canvas->AsCanvas());
+  delegate_->OnPaint(canvas->AsCanvas());
 }
 
 LRESULT WidgetWin::OnPowerBroadcast(DWORD power_event, DWORD data) {
@@ -1121,6 +1121,11 @@ void WidgetWin::ClientAreaSizeChanged() {
     layered_window_contents_.reset(
         new gfx::CanvasSkia(s.width(), s.height(), false));
   }
+}
+
+gfx::AcceleratedWidget WidgetWin::GetAcceleratedWidget() {
+  NOTIMPLEMENTED();
+  return gfx::kNullAcceleratedWidget;
 }
 
 ////////////////////////////////////////////////////////////////////////////////

ui/gfx/canvas.h
@@ -15,6 +15,8 @@
 
 namespace ui {
 class Transform;
+
+typedef unsigned int TextureID;
 }
 
 namespace gfx {
@@ -214,6 +216,9 @@ class Canvas {
 
   // Apply transformation on the canvas.
   virtual void Transform(const ui::Transform& transform) = 0;
+
+  // Create a texture ID that can be used for accelerated drawing.
+  virtual ui::TextureID GetTextureID() = 0;
 
   // TODO(beng): remove this once we don't need to use any skia-specific methods
   //             through this interface.

ui/gfx/canvas_direct2d.cc
@@ -345,6 +345,11 @@ void CanvasDirect2D::Transform(const ui::Transform& transform) {
   NOTIMPLEMENTED();
 }
 
+ui::TextureID CanvasDirect2D::GetTextureID() {
+  NOTIMPLEMENTED();
+  return 0;
+}
+
 CanvasSkia* CanvasDirect2D::AsCanvasSkia() {
   return NULL;
 }

ui/gfx/canvas_direct2d.h
@@ -78,6 +78,7 @@ class CanvasDirect2D : public Canvas {
   virtual gfx::NativeDrawingContext BeginPlatformPaint();
   virtual void EndPlatformPaint();
   virtual void Transform(const ui::Transform& transform);
+  virtual ui::TextureID GetTextureID();
   virtual CanvasSkia* AsCanvasSkia();
   virtual const CanvasSkia* AsCanvasSkia() const;
 

commit 08f38015cfda9a2867a3bfa1ec4d488cc550e6d5

Fix clang build, failure caused by r78149.
BUG=
TEST=

Review URL: http://codereview.chromium.org/6697013

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78154 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: baa8ace6a56d64883c1269b758d318bb95a5a246

views/widget/native_widget_delegate.h
@@ -39,7 +39,7 @@ class NativeWidgetDelegate {
 
   // Paints the rootview in the canvas. This will also refresh the compositor
   // tree if necessary when accelerated painting is enabled.
-  virtual void OnPaint(gfx::Canvas* canvas) = 0;
+  virtual void OnNativeWidgetPaint(gfx::Canvas* canvas) = 0;
 };
 
 }  // namespace internal

views/widget/widget.cc
@@ -265,7 +265,7 @@ bool Widget::HasFocusManager() const {
   return !!focus_manager_.get();
 }
 
-void Widget::OnPaint(gfx::Canvas* canvas) {
+void Widget::OnNativeWidgetPaint(gfx::Canvas* canvas) {
   GetRootView()->Paint(canvas);
   RefreshCompositeTree();
 }

views/widget/widget.h
@@ -275,7 +275,7 @@ class Widget : public internal::NativeWidgetDelegate,
   virtual void OnNativeWidgetCreated() OVERRIDE;
   virtual void OnSizeChanged(const gfx::Size& new_size) OVERRIDE;
   virtual bool HasFocusManager() const OVERRIDE;
-  virtual void OnPaint(gfx::Canvas* canvas) OVERRIDE;
+  virtual void OnNativeWidgetPaint(gfx::Canvas* canvas) OVERRIDE;
 
   // Overridden from FocusTraversable:
   virtual FocusSearch* GetFocusSearch() OVERRIDE;

views/widget/widget_gtk.cc
@@ -955,7 +955,7 @@ gboolean WidgetGtk::OnPaint(GtkWidget* widget, GdkEventExpose* event) {
   gfx::CanvasSkiaPaint canvas(event);
   if (!canvas.is_empty()) {
     canvas.set_composite_alpha(is_transparent());
-    delegate_->OnPaint(&canvas);
+    delegate_->OnNativeWidgetPaint(&canvas);
   }
   return false;  // False indicates other widgets should get the event as well.
 }

views/widget/widget_win.cc
@@ -820,7 +820,7 @@ LRESULT WidgetWin::OnNotify(int w_param, NMHDR* l_param) {
 void WidgetWin::OnPaint(HDC dc) {
   scoped_ptr<gfx::CanvasPaint> canvas(
       gfx::CanvasPaint::CreateCanvasPaint(hwnd()));
-  delegate_->OnPaint(canvas->AsCanvas());
+  delegate_->OnNativeWidgetPaint(canvas->AsCanvas());
 }
 
 LRESULT WidgetWin::OnPowerBroadcast(DWORD power_event, DWORD data) {