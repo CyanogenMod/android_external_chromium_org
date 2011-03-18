CyanogenMod/android_external_chromium_org

commit b008235b1de85a9fe925410ad7cfcf50babb03a2

Move a bunch more random files from chrome\renderer to content\renderer.
TBR=avi
Review URL: http://codereview.chromium.org/6685072

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78640 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 921f159b85924d2098ec2c9682017e79fe0063e0

content/content_renderer.gypi
@@ -43,12 +43,16 @@
         'renderer/gpu_video_service_host.h',
         'renderer/indexed_db_dispatcher.cc',
         'renderer/indexed_db_dispatcher.h',
+        'renderer/load_progress_tracker.cc',
+        'renderer/load_progress_tracker.h',
         'renderer/media/audio_renderer_impl.cc',
         'renderer/media/audio_renderer_impl.h',
         'renderer/media/gles2_video_decode_context.cc',
         'renderer/media/gles2_video_decode_context.h',
         'renderer/media/ipc_video_decoder.cc',
         'renderer/media/ipc_video_decoder.h',
+        'renderer/navigation_state.cc',
+        'renderer/navigation_state.h',
         'renderer/notification_provider.cc',
         'renderer/notification_provider.h',
         'renderer/p2p/ipc_network_manager.cc',
@@ -59,6 +63,12 @@
         'renderer/p2p/socket_client.h',
         'renderer/p2p/socket_dispatcher.cc',
         'renderer/p2p/socket_dispatcher.h',
+        'renderer/paint_aggregator.cc',
+        'renderer/paint_aggregator.h',
+        'renderer/pepper_platform_context_3d_impl.cc',
+        'renderer/pepper_platform_context_3d_impl.h',
+        'renderer/pepper_plugin_delegate_impl.cc',
+        'renderer/pepper_plugin_delegate_impl.h',
         'renderer/plugin_channel_host.cc',
         'renderer/plugin_channel_host.h',
         'renderer/speech_input_dispatcher.cc',
@@ -75,6 +85,8 @@
         'renderer/webworker_base.h',
         'renderer/webworker_proxy.cc',
         'renderer/webworker_proxy.h',
+        'renderer/web_ui_bindings.cc',
+        'renderer/web_ui_bindings.h',
       ],
       'conditions': [
         ['enable_gpu==1', {

chrome/chrome_renderer.gypi
@@ -99,20 +99,11 @@
         'renderer/external_extension.h',
         'renderer/localized_error.cc',
         'renderer/localized_error.h',
-        'renderer/load_progress_tracker.cc',
-        'renderer/load_progress_tracker.h',
-        'renderer/navigation_state.cc',
-        'renderer/navigation_state.h',
-        'renderer/paint_aggregator.cc',
         'renderer/page_click_listener.h',
         'renderer/page_click_tracker.cc',
         'renderer/page_click_tracker.h',
         'renderer/page_load_histograms.cc',
         'renderer/page_load_histograms.h',
-        'renderer/pepper_platform_context_3d_impl.cc',
-        'renderer/pepper_platform_context_3d_impl.h',
-        'renderer/pepper_plugin_delegate_impl.cc',
-        'renderer/pepper_plugin_delegate_impl.h',
         'renderer/print_web_view_helper.cc',
         'renderer/print_web_view_helper.h',
         'renderer/print_web_view_helper_linux.cc',
@@ -213,8 +204,6 @@
         'renderer/user_script_slave.h',
         'renderer/visitedlink_slave.cc',
         'renderer/visitedlink_slave.h',
-        'renderer/web_ui_bindings.cc',
-        'renderer/web_ui_bindings.h',
       ],
       'link_settings': {
         'mac_bundle_resources': [

chrome/chrome_tests.gypi
@@ -1751,7 +1751,6 @@
         'renderer/extensions/json_schema_unittest.cc',
         'renderer/net/predictor_queue_unittest.cc',
         'renderer/net/renderer_predictor_unittest.cc',
-        'renderer/paint_aggregator_unittest.cc',
         'renderer/render_process_unittest.cc',
         'renderer/render_thread_unittest.cc',
         'renderer/render_widget_unittest.cc',
@@ -1840,6 +1839,7 @@
         '../content/renderer/audio_message_filter_unittest.cc',
         '../content/renderer/gpu_video_decoder_host_unittest.cc',
         '../content/renderer/media/audio_renderer_impl_unittest.cc',
+        '../content/renderer/paint_aggregator_unittest.cc',
         '../testing/gtest_mac_unittest.mm',
         '../third_party/cld/encodings/compact_lang_det/compact_lang_det_unittest_small.cc',
         '../webkit/fileapi/file_system_dir_url_request_job_unittest.cc',

chrome/renderer/chrome_content_renderer_client.cc
@@ -4,7 +4,9 @@
 
 #include "chrome/renderer/chrome_content_renderer_client.h"
 
+#include "grit/locale_settings.h"
 #include "grit/renderer_resources.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 
 namespace chrome {
@@ -13,4 +15,7 @@ SkBitmap* ChromeContentRendererClient::GetSadPluginBitmap() {
   return ResourceBundle::GetSharedInstance().GetBitmapNamed(IDR_SAD_PLUGIN);
 }
 
+std::string ChromeContentRendererClient::GetDefaultEncoding() {
+  return l10n_util::GetStringUTF8(IDS_DEFAULT_ENCODING);
+}
 }  // namespace chrome

chrome/renderer/chrome_content_renderer_client.h
@@ -13,6 +13,7 @@ namespace chrome {
 class ChromeContentRendererClient : public content::ContentRendererClient {
  public:
   virtual SkBitmap* GetSadPluginBitmap();
+  virtual std::string GetDefaultEncoding();
 };
 
 }  // namespace chrome

chrome/renderer/external_host_bindings.h
@@ -6,7 +6,7 @@
 #define CHROME_RENDERER_EXTERNAL_HOST_BINDINGS_H_
 #pragma once
 
-#include "chrome/renderer/web_ui_bindings.h"
+#include "content/renderer/web_ui_bindings.h"
 #include "ipc/ipc_message.h"
 
 // ExternalHostBindings is the class backing the "externalHost" object

chrome/renderer/loadtimes_extension_bindings.cc
@@ -7,7 +7,7 @@
 #include <math.h>
 
 #include "base/time.h"
-#include "chrome/renderer/navigation_state.h"
+#include "content/renderer/navigation_state.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebFrame.h"
 #include "v8/include/v8.h"
 

chrome/renderer/page_load_histograms.cc
@@ -9,8 +9,8 @@
 #include "base/metrics/histogram.h"
 #include "base/time.h"
 #include "chrome/common/chrome_constants.h"
-#include "chrome/renderer/navigation_state.h"
 #include "chrome/renderer/render_thread.h"
+#include "content/renderer/navigation_state.h"
 #include "googleurl/src/gurl.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebFrame.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebPerformance.h"

commit 0348e59674753f3daae878c934f965f3c7e22e61

Fix shared linking (regressed in r78640).
TBR=jam

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78657 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 83e9db88c7afcd9a2c2b82624cdd6643b9541c3b

content/content_renderer.gypi
@@ -10,6 +10,7 @@
       'type': '<(library)',
       'dependencies': [
         'content_common',
+        '../ppapi/ppapi.gyp:ppapi_proxy',
         '../skia/skia.gyp:skia',
         '../third_party/ffmpeg/ffmpeg.gyp:ffmpeg',
         '../third_party/libjingle/libjingle.gyp:libjingle',