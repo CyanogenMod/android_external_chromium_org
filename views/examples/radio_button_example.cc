CyanogenMod/android_external_chromium_org

commit 3fef08fc97c653834c1000038ade3b55205e79b6

Resubmitting change to fix a build break in the clang builder.  A virtual method in a derived class was not marked as virtual.
Resubmitting change to fix a build break in the arm builder.  The only change
from the original code is in text_button.cc, line 402, where text_x is
initialized to 0.

Add classes for native themed push buttons, radio buttons, and checkboxes.
These controls expose the same public interface and the existing controls
of the same type to make it easier to change between the implementations.

BUG=None
TEST=The new controls should look and feel like native platform controls
R=ben@chromium.org

Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=83373
Review URL: http://codereview.chromium.org/6853015

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83535 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 9f16277c1c3dc976961b6fdeb1fc28495cf37451

views/examples/radio_button_example.cc
@@ -25,16 +25,17 @@ void RadioButtonExample::CreateExampleView(views::View* container) {
   select_ = new views::TextButton(this, L"Select");
   status_ = new views::TextButton(this, L"Show Status");
 
-  int all = arraysize(radio_buttons_);
-
-  // divide buttons into 2 groups
-  int group_count = all / 2;
-  for (int i = 0; i < all; i++) {
-    int group = i / group_count;
+  int group = 1;
+  for (int i = 0; i < arraysize(radio_buttons_); ++i) {
     radio_buttons_[i] = new views::RadioButton(
-        base::StringPrintf(
-            L"Radio %d in group %d", (i % group_count + 1), group),
-        group);
+        base::StringPrintf( L"Radio %d in group %d", i + 1, group), group);
+  }
+
+  ++group;
+  for (int i = 0; i < arraysize(radio_buttons_nt_); ++i) {
+    radio_buttons_nt_[i] = new views::RadioButtonNt(
+        base::StringPrintf( L"Radio %d in group %d", i + 1, group), group);
+    radio_buttons_nt_[i]->SetFocusable(true);
   }
 
   views::GridLayout* layout = new views::GridLayout(container);
@@ -43,9 +44,13 @@ void RadioButtonExample::CreateExampleView(views::View* container) {
   views::ColumnSet* column_set = layout->AddColumnSet(0);
   column_set->AddColumn(views::GridLayout::FILL, views::GridLayout::FILL,
                         1.0f, views::GridLayout::USE_PREF, 0, 0);
-  for (int i = 0; i < all; i++) {
+  for (int i = 0; i < arraysize(radio_buttons_); i++) {
     layout->StartRow(0, 0);
     layout->AddView(radio_buttons_[i]);
+  }
+  for (int i = 0; i < arraysize(radio_buttons_nt_); i++) {
+    layout->StartRow(0, 0);
+    layout->AddView(radio_buttons_nt_[i]);
   }
   layout->StartRow(0, 0);
   layout->AddView(select_);
@@ -57,16 +62,16 @@ void RadioButtonExample::ButtonPressed(views::Button* sender,
                                        const views::Event& event) {
   if (sender == select_) {
     radio_buttons_[0]->SetChecked(true);
-    radio_buttons_[5]->SetChecked(true);
+    radio_buttons_nt_[2]->SetChecked(true);
   } else if (sender == status_) {
     // Show the state of radio buttons.
     PrintStatus(L"Group1: 1:%ls, 2:%ls, 3:%ls   Group2: 1:%ls, 2:%ls, 3:%ls",
         IntToOnOff(radio_buttons_[0]->checked()),
         IntToOnOff(radio_buttons_[1]->checked()),
         IntToOnOff(radio_buttons_[2]->checked()),
-        IntToOnOff(radio_buttons_[3]->checked()),
-        IntToOnOff(radio_buttons_[4]->checked()),
-        IntToOnOff(radio_buttons_[5]->checked()));
+        IntToOnOff(radio_buttons_nt_[0]->checked()),
+        IntToOnOff(radio_buttons_nt_[1]->checked()),
+        IntToOnOff(radio_buttons_nt_[2]->checked()));
   }
 }
 

views/controls/button/checkbox.cc
@@ -14,6 +14,9 @@ namespace views {
 // static
 const char Checkbox::kViewClassName[] = "views/Checkbox";
 
+// static
+const char CheckboxNt::kViewClassName[] = "views/CheckboxNt";
+
 static const int kCheckboxLabelSpacing = 4;
 static const int kLabelFocusPaddingHorizontal = 2;
 static const int kLabelFocusPaddingVertical = 1;
@@ -216,6 +219,103 @@ void Checkbox::Init(const std::wstring& label_text) {
   label_->SetHasFocusBorder(true);
   label_->SetHorizontalAlignment(Label::ALIGN_LEFT);
   AddChildView(label_);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// CheckboxNt, public:
+
+CheckboxNt::CheckboxNt(const std::wstring& label)
+    : TextButtonBase(NULL, label),
+      checked_(false) {
+  set_border(new TextButtonNativeThemeBorder(this));
+}
+
+CheckboxNt::~CheckboxNt() {
+}
+
+void CheckboxNt::SetChecked(bool checked) {
+  checked_ = checked;
+  SchedulePaint();
+}
+
+void CheckboxNt::SetMultiLine(bool multiline) {
+  NOTREACHED() << "Not yet implemented";
+}
+
+gfx::Size CheckboxNt::GetPreferredSize() {
+  gfx::Size prefsize(TextButtonBase::GetPreferredSize());
+  gfx::NativeTheme::ExtraParams extra;
+  gfx::NativeTheme::State state = GetThemeState(&extra);
+  gfx::Size size = gfx::NativeTheme::instance()->GetPartSize(GetThemePart(),
+                                                             state,
+                                                             extra);
+  prefsize.Enlarge(size.width(), 0);
+  prefsize.set_height(std::max(prefsize.height(), size.height()));
+
+  if (max_width_ > 0)
+    prefsize.set_width(std::min(max_width_, prefsize.width()));
+
+  return prefsize;
+}
+
+std::string CheckboxNt::GetClassName() const {
+  return kViewClassName;
+}
+
+void CheckboxNt::GetAccessibleState(ui::AccessibleViewState* state) {
+  TextButtonBase::GetAccessibleState(state);
+  state->role = ui::AccessibilityTypes::ROLE_CHECKBUTTON;
+  state->state = checked() ? ui::AccessibilityTypes::STATE_CHECKED : 0;
+}
+
+void CheckboxNt::OnPaintFocusBorder(gfx::Canvas* canvas) {
+  if (HasFocus() && (IsFocusable() || IsAccessibilityFocusableInRootView())) {
+    gfx::Rect bounds(GetTextBounds());
+    // Increate the bounding box by one on each side so that that focus border
+    // does not draw on top of the letters.
+    bounds.Inset(-1, -1, -1, -1);
+    canvas->DrawFocusRect(bounds.x(), bounds.y(), bounds.width(),
+                          bounds.height());
+  }
+}
+
+void CheckboxNt::NotifyClick(const views::Event& event) {
+  SetChecked(!checked());
+  RequestFocus();
+  TextButtonBase::NotifyClick(event);
+}
+
+gfx::NativeTheme::Part CheckboxNt::GetThemePart() const {
+  return gfx::NativeTheme::kCheckbox;
+}
+
+gfx::Rect CheckboxNt::GetThemePaintRect() const {
+  gfx::NativeTheme::ExtraParams extra;
+  gfx::NativeTheme::State state = GetThemeState(&extra);
+  gfx::Size size(gfx::NativeTheme::instance()->GetPartSize(GetThemePart(),
+                                                           state,
+                                                           extra));
+  gfx::Insets insets = GetInsets();
+  gfx::Rect rect(insets.left(), 0, size.width(), height());
+  rect.set_x(GetMirroredXForRect(rect));
+  return rect;
+}
+
+void CheckboxNt::GetExtraParams(gfx::NativeTheme::ExtraParams* params) const {
+  TextButtonBase::GetExtraParams(params);
+  params->button.is_default = false;
+  params->button.checked = checked_;
+}
+
+gfx::Rect CheckboxNt::GetTextBounds() const {
+  gfx::Rect bounds(TextButtonBase::GetTextBounds());
+  gfx::NativeTheme::ExtraParams extra;
+  gfx::NativeTheme::State state = GetThemeState(&extra);
+  gfx::Size size(gfx::NativeTheme::instance()->GetPartSize(GetThemePart(),
+                                                           state,
+                                                           extra));
+  bounds.Offset(size.width() + kCheckboxLabelSpacing, 0);
+  return bounds;
 }
 
 }  // namespace views

views/controls/button/checkbox.h
@@ -9,6 +9,7 @@
 #include <string>
 
 #include "views/controls/button/native_button.h"
+#include "views/controls/button/text_button.h"
 
 namespace views {
 
@@ -85,6 +86,60 @@ class Checkbox : public NativeButtonBase {
   bool checked_;
 
   DISALLOW_COPY_AND_ASSIGN(Checkbox);
+};
+
+// A native themed class representing a checkbox.  This class does not use
+// platform specific objects to replicate the native platforms looks and feel.
+//
+// This class will eventually be renamed to Checkbox to replace the class
+// above.
+//
+// TODO: A Checkbox feature not support by CheckboxNt is multi-line.
+class CheckboxNt : public TextButtonBase {
+ public:
+  explicit CheckboxNt(const std::wstring& label);
+  virtual ~CheckboxNt();
+
+  // Sets a listener for this checkbox. Checkboxes aren't required to have them
+  // since their state can be read independently of them being toggled.
+  void set_listener(ButtonListener* listener) { listener_ = listener; }
+
+  // Sets/Gets whether or not the checkbox is checked.
+  virtual void SetChecked(bool checked);
+  bool checked() const { return checked_; }
+
+  // Sets whether or not the checkbox label should wrap multiple lines of text.
+  // If true, long lines are wrapped, and this is reflected in the preferred
+  // size returned by GetPreferredSize. If false, text that will not fit within
+  // the available bounds for the label will be cropped.
+  // TODO: not yet implemented.
+  void SetMultiLine(bool multiline);
+
+ protected:
+  // Overridden from View:
+  virtual gfx::Size GetPreferredSize() OVERRIDE;
+  virtual std::string GetClassName() const OVERRIDE;
+  virtual void GetAccessibleState(ui::AccessibleViewState* state) OVERRIDE;
+  virtual void OnPaintFocusBorder(gfx::Canvas* canvas) OVERRIDE;
+
+ private:
+  // Overridden from Button:
+  virtual void NotifyClick(const views::Event& event) OVERRIDE;
+
+  // Overridden from TextButtonBase:
+  virtual gfx::NativeTheme::Part GetThemePart() const OVERRIDE;
+  virtual gfx::Rect GetThemePaintRect() const OVERRIDE;
+  virtual void GetExtraParams(
+      gfx::NativeTheme::ExtraParams* params) const OVERRIDE;
+  virtual gfx::Rect GetTextBounds() const OVERRIDE;
+
+  // True if the checkbox is checked.
+  bool checked_;
+
+  // The button's class name.
+  static const char kViewClassName[];
+
+  DISALLOW_COPY_AND_ASSIGN(CheckboxNt);
 };
 
 }  // namespace views

views/controls/button/radio_button.cc
@@ -13,6 +13,9 @@ namespace views {
 // static
 const char RadioButton::kViewClassName[] = "views/RadioButton";
 
+// static
+const char RadioButtonNt::kViewClassName[] = "views/RadioButtonNt";
+
 ////////////////////////////////////////////////////////////////////////////////
 // RadioButton, public:
 
@@ -110,6 +113,93 @@ std::string RadioButton::GetClassName() const {
 
 NativeButtonWrapper* RadioButton::CreateWrapper() {
   return NativeButtonWrapper::CreateRadioButtonWrapper(this);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// RadioButtonNt
+//
+////////////////////////////////////////////////////////////////////////////////
+
+RadioButtonNt::RadioButtonNt(const std::wstring& label, int group_id)
+    : CheckboxNt(label) {
+  SetGroup(group_id);
+}
+
+RadioButtonNt::~RadioButtonNt() {
+}
+
+void RadioButtonNt::SetChecked(bool checked) {
+  if (checked == RadioButtonNt::checked())
+    return;
+  if (checked) {
+    // We can't just get the root view here because sometimes the radio
+    // button isn't attached to a root view (e.g., if it's part of a tab page
+    // that is currently not active).
+    View* container = parent();
+    while (container && container->parent())
+      container = container->parent();
+    if (container) {
+      std::vector<View*> other;
+      container->GetViewsWithGroup(GetGroup(), &other);
+      std::vector<View*>::iterator i;
+      for (i = other.begin(); i != other.end(); ++i) {
+        if (*i != this) {
+          if ((*i)->GetClassName() != kViewClassName) {
+            NOTREACHED() << "radio-button-nt has same group as other non "
+                            "radio-button-nt views.";
+            continue;
+          }
+          RadioButtonNt* peer = static_cast<RadioButtonNt*>(*i);
+          peer->SetChecked(false);
+        }
+      }
+    }
+  }
+  CheckboxNt::SetChecked(checked);
+}
+
+std::string RadioButtonNt::GetClassName() const {
+  return kViewClassName;
+}
+
+void RadioButtonNt::GetAccessibleState(ui::AccessibleViewState* state) {
+  CheckboxNt::GetAccessibleState(state);
+  state->role = ui::AccessibilityTypes::ROLE_RADIOBUTTON;
+}
+
+View* RadioButtonNt::GetSelectedViewForGroup(int group_id) {
+  std::vector<View*> views;
+  GetRootView()->GetViewsWithGroup(group_id, &views);
+  if (views.empty())
+    return NULL;
+
+  for (std::vector<View*>::const_iterator iter = views.begin();
+       iter != views.end(); ++iter) {
+    // REVIEW: why don't we check the runtime type like is done above?
+    RadioButtonNt* radio_button = static_cast<RadioButtonNt*>(*iter);
+    if (radio_button->checked())
+      return radio_button;
+  }
+  return NULL;
+}
+
+bool RadioButtonNt::IsGroupFocusTraversable() const {
+  // When focusing a radio button with tab/shift+tab, only the selected button
+  // from the group should be focused.
+  return false;
+}
+
+void RadioButtonNt::NotifyClick(const views::Event& event) {
+  // Set the checked state to true only if we are unchecked, since we can't
+  // be toggled on and off like a checkbox.
+  if (!checked())
+    SetChecked(true);
+  RequestFocus();
+}
+
+gfx::NativeTheme::Part RadioButtonNt::GetThemePart() const {
+  return gfx::NativeTheme::kRadio;
 }
 
 }  // namespace views

views/controls/button/radio_button.h
@@ -46,6 +46,37 @@ class RadioButton : public Checkbox {
   DISALLOW_COPY_AND_ASSIGN(RadioButton);
 };
 
+// A native themed class representing a radio button.  This class does not use
+// platform specific objects to replicate the native platforms looks and feel.
+//
+// This class will eventually be renamed to RadioButton to replace the class
+// above.
+class RadioButtonNt : public CheckboxNt {
+ public:
+  // The button's class name.
+  static const char kViewClassName[];
+
+  RadioButtonNt(const std::wstring& label, int group_id);
+  virtual ~RadioButtonNt();
+
+  // Overridden from View:
+  virtual std::string GetClassName() const OVERRIDE;
+  virtual void GetAccessibleState(ui::AccessibleViewState* state) OVERRIDE;
+  virtual View* GetSelectedViewForGroup(int group_id) OVERRIDE;
+  virtual bool IsGroupFocusTraversable() const OVERRIDE;
+
+  // Overridden from Button:
+  virtual void NotifyClick(const views::Event& event) OVERRIDE;
+
+  // Overridden from TextButtonBase:
+  virtual gfx::NativeTheme::Part GetThemePart() const OVERRIDE;
+
+  // Overridden from CheckboxNt:
+  virtual void SetChecked(bool checked) OVERRIDE;
+
+  DISALLOW_COPY_AND_ASSIGN(RadioButtonNt);
+};
+
 }  // namespace views
 
 #endif  // VIEWS_CONTROLS_BUTTON_RADIO_BUTTON_H_

views/controls/button/text_button.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -15,6 +15,7 @@
 #include "ui/gfx/font.h"
 #include "views/border.h"
 #include "views/controls/button/custom_button.h"
+#include "views/native_theme_delegate.h"
 
 namespace views {
 
@@ -35,10 +36,8 @@ class TextButtonBorder : public Border {
   TextButtonBorder();
   virtual ~TextButtonBorder();
 
-  // Render the background for the provided view
+  // Implementation of Border:
   virtual void Paint(const View& view, gfx::Canvas* canvas) const;
-
-  // Returns the insets for the border.
   virtual void GetInsets(gfx::Insets* insets) const;
 
  protected:
@@ -67,14 +66,41 @@ class TextButtonBorder : public Border {
 
 ////////////////////////////////////////////////////////////////////////////////
 //
-// TextButton
+// TextButtonNativeThemeBorder
 //
-//  A button which displays text and/or and icon that can be changed in
-//  response to actions. TextButton reserves space for the largest string
+//  A Border subclass that paints a TextButton's background layer using the
+//  platform's native theme look.  This handles normal/disabled/hot/pressed
+//  states, with possible animation between states.
+//
+////////////////////////////////////////////////////////////////////////////////
+class TextButtonNativeThemeBorder : public Border {
+ public:
+   TextButtonNativeThemeBorder(NativeThemeDelegate* delegate);
+  virtual ~TextButtonNativeThemeBorder();
+
+  // Implementation of Border:
+  virtual void Paint(const View& view, gfx::Canvas* canvas) const;
+  virtual void GetInsets(gfx::Insets* insets) const;
+
+ private:
+  // The delegate the controls the appearance of this border.
+  NativeThemeDelegate* delegate_;
+
+  DISALLOW_COPY_AND_ASSIGN(TextButtonNativeThemeBorder);
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// TextButtonBase
+//
+//  A base ckass for different types of buttons, like push buttons, radio
+//  buttons, and checkboxes, that do not depende on native components for
+//  look and feel. TextButton reserves space for the largest string
 //  passed to SetText. To reset the cached max size invoke ClearMaxTextSize.
 //
 ////////////////////////////////////////////////////////////////////////////////
-class TextButton : public CustomButton {
+class TextButtonBase : public CustomButton, public NativeThemeDelegate {
  public:
   // The menu button's class name.
   static const char kViewClassName[];
@@ -92,8 +118,7 @@ class TextButton : public CustomButton {
     PREFIX_SHOW
   };
 
-  TextButton(ButtonListener* listener, const std::wstring& text);
-  virtual ~TextButton();
+  virtual ~TextButtonBase();
 
   // Call SetText once per string in your set of possible values at button
   // creation time, so that it can contain the largest of them and avoid
@@ -110,6 +135,154 @@ class TextButton : public CustomButton {
   void set_alignment(TextAlignment alignment) { alignment_ = alignment; }
 
   void set_prefix_type(PrefixType type) { prefix_type_ = type; }
+
+  const ui::Animation* GetAnimation() const;
+
+  void SetIsDefault(bool is_default);
+  bool is_default() const { return is_default_; }
+
+  // TextButton remembers the maximum display size of the text passed to
+  // SetText. This method resets the cached maximum display size to the
+  // current size.
+  void ClearMaxTextSize();
+
+  void set_max_width(int max_width) { max_width_ = max_width; }
+  void SetFont(const gfx::Font& font);
+  // Return the font used by this button.
+  gfx::Font font() const { return font_; }
+
+  void SetEnabledColor(SkColor color);
+  void SetDisabledColor(SkColor color);
+  void SetHighlightColor(SkColor color);
+  void SetHoverColor(SkColor color);
+  void SetTextHaloColor(SkColor color);
+  // The shadow color used is determined by whether the widget is active or
+  // inactive. Both possible colors are set in this method, and the
+  // appropriate one is chosen during Paint.
+  void SetTextShadowColors(SkColor active_color, SkColor inactive_color);
+
+  bool normal_has_border() const { return normal_has_border_; }
+  void SetNormalHasBorder(bool normal_has_border);
+
+  // Sets whether or not to show the hot and pushed states for the button icon
+  // (if present) in addition to the normal state.  Defaults to true.
+  bool show_multiple_icon_states() const { return show_multiple_icon_states_; }
+  void SetShowMultipleIconStates(bool show_multiple_icon_states);
+
+  // Paint the button into the specified canvas. If |mode| is |PB_FOR_DRAG|, the
+  // function paints a drag image representation into the canvas.
+  enum PaintButtonMode { PB_NORMAL, PB_FOR_DRAG };
+  virtual void PaintButton(gfx::Canvas* canvas, PaintButtonMode mode);
+
+  // Overridden from View:
+  virtual gfx::Size GetPreferredSize() OVERRIDE;
+  virtual gfx::Size GetMinimumSize() OVERRIDE;
+  virtual void SetEnabled(bool enabled) OVERRIDE;
+  virtual void OnPaint(gfx::Canvas* canvas) OVERRIDE;
+
+  // Text colors.
+  static const SkColor kEnabledColor;
+  static const SkColor kHighlightColor;
+  static const SkColor kDisabledColor;
+  static const SkColor kHoverColor;
+
+  // Returns views/TextButton.
+  virtual std::string GetClassName() const;
+
+ protected:
+  TextButtonBase(ButtonListener* listener, const std::wstring& text);
+
+  // Called when enabled or disabled state changes, or the colors for those
+  // states change.
+  virtual void UpdateColor();
+
+  // Updates text_size_ and max_text_size_ from the current text/font. This is
+  // invoked when the font or text changes.
+  void UpdateTextSize();
+
+  // Overridden from NativeThemeDelegate:
+  virtual gfx::Rect GetThemePaintRect() const OVERRIDE;
+  virtual gfx::NativeTheme::State GetThemeState(
+      gfx::NativeTheme::ExtraParams* params) const OVERRIDE;
+  virtual const ui::Animation* GetThemeAnimation() const OVERRIDE;
+  virtual gfx::NativeTheme::State GetBackgroundThemeState(
+      gfx::NativeTheme::ExtraParams* params) const OVERRIDE;
+  virtual gfx::NativeTheme::State GetForegroundThemeState(
+      gfx::NativeTheme::ExtraParams* params) const OVERRIDE;
+
+  virtual void GetExtraParams(gfx::NativeTheme::ExtraParams* params) const;
+
+  virtual gfx::Rect GetTextBounds() const;
+
+  // The text string that is displayed in the button.
+  string16 text_;
+
+  // The size of the text string.
+  gfx::Size text_size_;
+
+  // Track the size of the largest text string seen so far, so that
+  // changing text_ will not resize the button boundary.
+  gfx::Size max_text_size_;
+
+  // The alignment of the text string within the button.
+  TextAlignment alignment_;
+
+  // The font used to paint the text.
+  gfx::Font font_;
+
+  // Text color.
+  SkColor color_;
+
+  // State colors.
+  SkColor color_enabled_;
+  SkColor color_disabled_;
+  SkColor color_highlight_;
+  SkColor color_hover_;
+
+  // An optional halo around text.
+  SkColor text_halo_color_;
+  bool has_text_halo_;
+
+  // Optional shadow text colors for active and inactive widget states.
+  SkColor active_text_shadow_color_;
+  SkColor inactive_text_shadow_color_;
+  bool has_shadow_;
+
+  // The width of the button will never be larger than this value. A value <= 0
+  // indicates the width is not constrained.
+  int max_width_;
+
+  // This is true if normal state has a border frame; default is false.
+  bool normal_has_border_;
+
+  // Whether or not to show the hot and pushed icon states.
+  bool show_multiple_icon_states_;
+
+  // Whether or not the button appears and behaves as the default button in its
+  // current context.
+  bool is_default_;
+
+  PrefixType prefix_type_;
+
+  DISALLOW_COPY_AND_ASSIGN(TextButtonBase);
+};
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// TextButton
+//
+//  A button which displays text and/or and icon that can be changed in
+//  response to actions. TextButton reserves space for the largest string
+//  passed to SetText. To reset the cached max size invoke ClearMaxTextSize.
+//
+////////////////////////////////////////////////////////////////////////////////
+class TextButton : public TextButtonBase {
+ public:
+  // The button's class name.
+  static const char kViewClassName[];
+
+  TextButton(ButtonListener* listener, const std::wstring& text);
+  virtual ~TextButton();
 
   void set_icon_text_spacing(int icon_text_spacing) {
     icon_text_spacing_ = icon_text_spacing;
@@ -133,99 +306,27 @@ class TextButton : public CustomButton {
     icon_placement_ = icon_placement;
   }
 
-  // TextButton remembers the maximum display size of the text passed to
-  // SetText. This method resets the cached maximum display size to the
-  // current size.
-  void ClearMaxTextSize();
-
-  void set_max_width(int max_width) { max_width_ = max_width; }
-  void SetFont(const gfx::Font& font);
-  // Return the font used by this button.
-  gfx::Font font() const { return font_; }
-
-  void SetEnabledColor(SkColor color);
-  void SetDisabledColor(SkColor color);
-  void SetHighlightColor(SkColor color);
-  void SetHoverColor(SkColor color);
-  void SetTextHaloColor(SkColor color);
-  // The shadow color used is determined by whether the widget is active or
-  // inactive. Both possible colors are set in this method, and the
-  // appropriate one is chosen during Paint.
-  void SetTextShadowColors(SkColor active_color, SkColor inactive_color);
-  void SetNormalHasBorder(bool normal_has_border);
-  // Sets whether or not to show the hot and pushed states for the button icon
-  // (if present) in addition to the normal state.  Defaults to true.
-  void SetShowMultipleIconStates(bool show_multiple_icon_states);
-
-  // Paint the button into the specified canvas. If |mode| is |PB_FOR_DRAG|, the
-  // function paints a drag image representation into the canvas.
-  enum PaintButtonMode { PB_NORMAL, PB_FOR_DRAG };
-  virtual void PaintButton(gfx::Canvas* canvas, PaintButtonMode mode);
-
   // Overridden from View:
-  virtual gfx::Size GetPreferredSize();
-  virtual gfx::Size GetMinimumSize();
-  virtual void SetEnabled(bool enabled);
+  virtual gfx::Size GetPreferredSize() OVERRIDE;
+  virtual std::string GetClassName() const OVERRIDE;
 
-  // Text colors.
-  static const SkColor kEnabledColor;
-  static const SkColor kHighlightColor;
-  static const SkColor kDisabledColor;
-  static const SkColor kHoverColor;
-
-  // Returns views/TextButton.
-  virtual std::string GetClassName() const;
+  // Overridden from TextButtonBase:
+  virtual void PaintButton(gfx::Canvas* canvas, PaintButtonMode mode) OVERRIDE;
 
  protected:
   SkBitmap icon() const { return icon_; }
 
-  virtual void OnPaint(gfx::Canvas* canvas);
+  // Overridden from NativeThemeDelegate:
+  virtual gfx::NativeTheme::Part GetThemePart() const OVERRIDE;
 
-  // Called when enabled or disabled state changes, or the colors for those
-  // states change.
-  virtual void UpdateColor();
-
-  // Updates text_size_ and max_text_size_ from the current text/font. This is
-  // invoked when the font or text changes.
-  void UpdateTextSize();
-
-  // The text string that is displayed in the button.
-  string16 text_;
+  // Overridden from TextButtonBase:
+  virtual void GetExtraParams(
+      gfx::NativeTheme::ExtraParams* params) const OVERRIDE;
+  virtual gfx::Rect GetTextBounds() const OVERRIDE;
 
  private:
-  // The size of the text string.
-  gfx::Size text_size_;
-
-  // Track the size of the largest text string seen so far, so that
-  // changing text_ will not resize the button boundary.
-  gfx::Size max_text_size_;
-
-  // The alignment of the text string within the button.
-  TextAlignment alignment_;
-
   // The position of the icon.
   IconPlacement icon_placement_;
-
-  // The font used to paint the text.
-  gfx::Font font_;
-
-  // Text color.
-  SkColor color_;
-
-  // State colors.
-  SkColor color_enabled_;
-  SkColor color_disabled_;
-  SkColor color_highlight_;
-  SkColor color_hover_;
-
-  // An optional halo around text.
-  SkColor text_halo_color_;
-  bool has_text_halo_;
-
-  // Optional shadow text colors for active and inactive widget states.
-  SkColor active_text_shadow_color_;
-  SkColor inactive_text_shadow_color_;
-  bool has_shadow_;
 
   // An icon displayed with the text.
   SkBitmap icon_;
@@ -237,18 +338,6 @@ class TextButton : public CustomButton {
   // An optional different version of the icon for pushed state.
   SkBitmap icon_pushed_;
   bool has_pushed_icon_;
-
-  // The width of the button will never be larger than this value. A value <= 0
-  // indicates the width is not constrained.
-  int max_width_;
-
-  // This is true if normal state has a border frame; default is false.
-  bool normal_has_border_;
-
-  // Whether or not to show the hot and pushed icon states.
-  bool show_multiple_icon_states_;
-
-  PrefixType prefix_type_;
 
   // Space between icon and text.
   int icon_text_spacing_;

commit 03dd64f16d6917ff151bca559bec49b4c25dd69e

Fix compile error: 'comparison between signed and unsigned integer expressions.' (regressed in r83535)
TBR=rogerta@chromium.org

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83574 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 0724b0b7e0c54e34da8087bf1c2efbbc4bfd683f

views/examples/radio_button_example.cc
@@ -26,13 +26,13 @@ void RadioButtonExample::CreateExampleView(views::View* container) {
   status_ = new views::TextButton(this, L"Show Status");
 
   int group = 1;
-  for (int i = 0; i < arraysize(radio_buttons_); ++i) {
+  for (size_t i = 0; i < arraysize(radio_buttons_); ++i) {
     radio_buttons_[i] = new views::RadioButton(
         base::StringPrintf( L"Radio %d in group %d", i + 1, group), group);
   }
 
   ++group;
-  for (int i = 0; i < arraysize(radio_buttons_nt_); ++i) {
+  for (size_t i = 0; i < arraysize(radio_buttons_nt_); ++i) {
     radio_buttons_nt_[i] = new views::RadioButtonNt(
         base::StringPrintf( L"Radio %d in group %d", i + 1, group), group);
     radio_buttons_nt_[i]->SetFocusable(true);
@@ -44,11 +44,11 @@ void RadioButtonExample::CreateExampleView(views::View* container) {
   views::ColumnSet* column_set = layout->AddColumnSet(0);
   column_set->AddColumn(views::GridLayout::FILL, views::GridLayout::FILL,
                         1.0f, views::GridLayout::USE_PREF, 0, 0);
-  for (int i = 0; i < arraysize(radio_buttons_); i++) {
+  for (size_t i = 0; i < arraysize(radio_buttons_); i++) {
     layout->StartRow(0, 0);
     layout->AddView(radio_buttons_[i]);
   }
-  for (int i = 0; i < arraysize(radio_buttons_nt_); i++) {
+  for (size_t i = 0; i < arraysize(radio_buttons_nt_); i++) {
     layout->StartRow(0, 0);
     layout->AddView(radio_buttons_nt_[i]);
   }