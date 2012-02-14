CyanogenMod/android_external_chromium_org

commit 7c410168ce0f60e7ec23d11d036d4e785ab28d7b

Remove BrowserThread::UnsafeGetBrowserThread, add UnsafeGetMessageLoopForThread.
This also removes several accessors on BrowserProcess that are no longer used.

I wanted to remove all retrieval of MessageLoop objects via BrowserThread, but this proved harder than it looked, because several net:: classes use MessageLoop and have fairly deep assumptions that it is a real MessageLoop, e.g. they use MessageLoopForIO with its IO observers and so forth.  Therefore, we now have UnsafeGetMessageLoopForThread but UnsafeGetBrowserThread is gone.

TBR=abodenha@chromium.org
BUG=98716


Review URL: http://codereview.chromium.org/8769013

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113377 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: ed10dd138cfcb174ac890133e7fe40da91e5b04f

chrome/browser/sessions/base_session_service.cc
@@ -14,9 +14,11 @@
 #include "chrome/browser/sessions/session_types.h"
 #include "chrome/common/url_constants.h"
 #include "content/browser/tab_contents/navigation_entry.h"
+#include "content/public/browser/browser_thread.h"
 #include "content/public/common/referrer.h"
 #include "webkit/glue/webkit_glue.h"
 
+using content::BrowserThread;
 using WebKit::WebReferrerPolicy;
 
 // InternalGetCommandsRequest -------------------------------------------------
@@ -69,7 +71,6 @@ BaseSessionService::BaseSessionService(SessionType type,
                                        const FilePath& path)
     : profile_(profile),
       path_(path),
-      backend_thread_(NULL),
       ALLOW_THIS_IN_INITIALIZER_LIST(weak_factory_(this)),
       pending_reset_(false),
       commands_since_reset_(0) {
@@ -80,22 +81,18 @@ BaseSessionService::BaseSessionService(SessionType type,
   backend_ = new SessionBackend(type,
       profile_ ? profile_->GetPath() : path_);
   DCHECK(backend_.get());
-  backend_thread_ = g_browser_process->file_thread();
-  if (!backend_thread_)
-    backend_->Init();
-  // If backend_thread is non-null, backend will init itself as appropriate.
+
+  RunTaskOnBackendThread(FROM_HERE,
+                         base::Bind(&SessionBackend::Init, backend_));
 }
 
 BaseSessionService::~BaseSessionService() {
 }
 
 void BaseSessionService::DeleteLastSession() {
-  if (!backend_thread()) {
-    backend()->DeleteLastSession();
-  } else {
-    backend_thread()->message_loop()->PostTask(
-        FROM_HERE, base::Bind(&SessionBackend::DeleteLastSession, backend()));
-  }
+  RunTaskOnBackendThread(
+      FROM_HERE,
+      base::Bind(&SessionBackend::DeleteLastSession, backend()));
 }
 
 void BaseSessionService::ScheduleCommand(SessionCommand* command) {
@@ -122,16 +119,12 @@ void BaseSessionService::Save() {
   if (pending_commands_.empty())
     return;
 
-  if (!backend_thread()) {
-    backend()->AppendCommands(
-        new std::vector<SessionCommand*>(pending_commands_), pending_reset_);
-  } else {
-    backend_thread()->message_loop()->PostTask(
-        FROM_HERE,
-        base::Bind(&SessionBackend::AppendCommands, backend(),
-                   new std::vector<SessionCommand*>(pending_commands_),
-                   pending_reset_));
-  }
+  RunTaskOnBackendThread(
+      FROM_HERE,
+      base::Bind(&SessionBackend::AppendCommands, backend(),
+                 new std::vector<SessionCommand*>(pending_commands_),
+                 pending_reset_));
+
   // Backend took ownership of commands.
   pending_commands_.clear();
 
@@ -275,14 +268,10 @@ BaseSessionService::Handle BaseSessionService::ScheduleGetLastSessionCommands(
     CancelableRequestConsumerBase* consumer) {
   scoped_refptr<InternalGetCommandsRequest> request_wrapper(request);
   AddRequest(request, consumer);
-  if (backend_thread()) {
-    backend_thread()->message_loop()->PostTask(
-        FROM_HERE,
-        base::Bind(&SessionBackend::ReadLastSessionCommands, backend(),
-                   request_wrapper));
-  } else {
-    backend()->ReadLastSessionCommands(request);
-  }
+  RunTaskOnBackendThread(
+      FROM_HERE,
+      base::Bind(&SessionBackend::ReadLastSessionCommands, backend(),
+                 request_wrapper));
   return request->handle();
 }
 
@@ -292,13 +281,23 @@ BaseSessionService::Handle
         CancelableRequestConsumerBase* consumer) {
   scoped_refptr<InternalGetCommandsRequest> request_wrapper(request);
   AddRequest(request, consumer);
-  if (backend_thread()) {
-    backend_thread()->message_loop()->PostTask(
-        FROM_HERE,
-        base::Bind(&SessionBackend::ReadCurrentSessionCommands, backend(),
-                   request_wrapper));
-  } else {
-    backend()->ReadCurrentSessionCommands(request);
-  }
+  RunTaskOnBackendThread(
+      FROM_HERE,
+      base::Bind(&SessionBackend::ReadCurrentSessionCommands, backend(),
+                 request_wrapper));
   return request->handle();
+}
+
+bool BaseSessionService::RunTaskOnBackendThread(
+    const tracked_objects::Location& from_here,
+    const base::Closure& task) {
+  if (profile_ && BrowserThread::IsMessageLoopValid(BrowserThread::FILE)) {
+    return BrowserThread::PostTask(BrowserThread::FILE, from_here, task);
+  } else {
+    // Fall back to executing on the main thread if the file thread
+    // has gone away (around shutdown time) or if we're running as
+    // part of a unit test that does not set profile_.
+    task.Run();
+    return true;
+  }
 }

chrome/browser/sessions/base_session_service.h
@@ -8,6 +8,7 @@
 
 #include "base/basictypes.h"
 #include "base/callback.h"
+#include "base/location.h"
 #include "base/file_path.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
@@ -21,10 +22,6 @@ class Profile;
 class SessionBackend;
 class SessionCommand;
 class TabNavigation;
-
-namespace base {
-class Thread;
-}
 
 // BaseSessionService is the super class of both tab restore service and
 // session service. It contains commonality needed by both, in particular
@@ -84,9 +81,6 @@ class BaseSessionService : public CancelableRequestProvider,
 
   // Returns the backend.
   SessionBackend* backend() const { return backend_; }
-
-  // Returns the thread the backend runs on. This returns NULL during testing.
-  base::Thread* backend_thread() const { return backend_thread_; }
 
   // Returns the set of commands that needed to be scheduled. The commands
   // in the vector are owned by BaseSessionService, until they are scheduled
@@ -157,6 +151,12 @@ class BaseSessionService : public CancelableRequestProvider,
       InternalGetCommandsRequest* request,
       CancelableRequestConsumerBase* consumer);
 
+  // In production, this posts the task to the FILE thread.  For
+  // tests, it immediately runs the specified task on the current
+  // thread.
+  bool RunTaskOnBackendThread(const tracked_objects::Location& from_here,
+                              const base::Closure& task);
+
   // Max number of navigation entries in each direction we'll persist.
   static const int max_persist_navigation_count;
 
@@ -169,9 +169,6 @@ class BaseSessionService : public CancelableRequestProvider,
 
   // The backend.
   scoped_refptr<SessionBackend> backend_;
-
-  // Thread backend tasks are run on, is NULL during testing.
-  base::Thread* backend_thread_;
 
   // Used to invoke Save.
   base::WeakPtrFactory<BaseSessionService> weak_factory_;

chrome/browser/browser_process.h
@@ -44,10 +44,6 @@ class TabCloseableStateWatcher;
 class ThumbnailGenerator;
 class WatchDogThread;
 
-namespace base {
-class Thread;
-}
-
 #if defined(OS_CHROMEOS)
 namespace browser {
 class OomPriorityManager;
@@ -114,34 +110,18 @@ class BrowserProcess {
   // Returns the manager for desktop notifications.
   virtual NotificationUIManager* notification_ui_manager() = 0;
 
-  // Returns the thread that we perform I/O coordination on (network requests,
-  // communication with renderers, etc.
-  // NOTE: You should ONLY use this to pass to IPC or other objects which must
-  // need a MessageLoop*.  If you just want to post a task, use
-  // BrowserThread::PostTask (or other variants) as they take care of checking
-  // that a thread is still alive, race conditions, lifetime differences etc.
-  // If you still must use this check the return value for NULL.
+  // Returns the state object for the thread that we perform I/O
+  // coordination on (network requests, communication with renderers,
+  // etc.
+  //
+  // Can be NULL close to startup and shutdown.
+  //
+  // NOTE: If you want to post a task to the IO thread, use
+  // BrowserThread::PostTask (or other variants).
   virtual IOThread* io_thread() = 0;
-
-  // Returns the thread that we perform random file operations on. For code
-  // that wants to do I/O operations (not network requests or even file: URL
-  // requests), this is the thread to use to avoid blocking the UI thread.
-  // It might be nicer to have a thread pool for this kind of thing.
-  virtual base::Thread* file_thread() = 0;
-
-  // Returns the thread that is used for database operations such as the web
-  // database. History has its own thread since it has much higher traffic.
-  virtual base::Thread* db_thread() = 0;
 
   // Returns the thread that is used for health check of all browser threads.
   virtual WatchDogThread* watchdog_thread() = 0;
-
-#if defined(OS_CHROMEOS)
-  // Returns thread for websocket to TCP proxy.
-  // TODO(dilmah): remove this thread.  Instead provide this functionality via
-  // hooks into websocket bridge layer.
-  virtual base::Thread* web_socket_proxy_thread() = 0;
-#endif
 
   virtual policy::BrowserPolicyConnector* browser_policy_connector() = 0;
 

chrome/browser/browser_process_impl.cc
@@ -291,6 +291,10 @@ void BrowserProcessImpl::PostStopThread(BrowserThread::ID thread_id) {
       // Reset associated state right after actual thread is stopped,
       // as io_thread_.global_ cleanup happens in CleanUp on the IO
       // thread, i.e. as the thread exits its message loop.
+      //
+      // This is important also because in various places, the
+      // IOThread object being NULL is considered synonymous with the
+      // IO thread having stopped.
       io_thread_.reset();
       break;
     default:
@@ -414,16 +418,6 @@ IOThread* BrowserProcessImpl::io_thread() {
   return io_thread_.get();
 }
 
-base::Thread* BrowserProcessImpl::file_thread() {
-  DCHECK(CalledOnValidThread());
-  return BrowserThread::UnsafeGetBrowserThread(BrowserThread::FILE);
-}
-
-base::Thread* BrowserProcessImpl::db_thread() {
-  DCHECK(CalledOnValidThread());
-  return BrowserThread::UnsafeGetBrowserThread(BrowserThread::DB);
-}
-
 WatchDogThread* BrowserProcessImpl::watchdog_thread() {
   DCHECK(CalledOnValidThread());
   if (!created_watchdog_thread_)
@@ -431,13 +425,6 @@ WatchDogThread* BrowserProcessImpl::watchdog_thread() {
   DCHECK(watchdog_thread_.get() != NULL);
   return watchdog_thread_.get();
 }
-
-#if defined(OS_CHROMEOS)
-base::Thread* BrowserProcessImpl::web_socket_proxy_thread() {
-  DCHECK(CalledOnValidThread());
-  return BrowserThread::UnsafeGetBrowserThread(BrowserThread::WEB_SOCKET_PROXY);
-}
-#endif
 
 ProfileManager* BrowserProcessImpl::profile_manager() {
   DCHECK(CalledOnValidThread());

chrome/browser/browser_process_impl.h
@@ -64,12 +64,7 @@ class BrowserProcessImpl : public BrowserProcess,
   virtual ResourceDispatcherHost* resource_dispatcher_host() OVERRIDE;
   virtual MetricsService* metrics_service() OVERRIDE;
   virtual IOThread* io_thread() OVERRIDE;
-  virtual base::Thread* file_thread() OVERRIDE;
-  virtual base::Thread* db_thread() OVERRIDE;
   virtual WatchDogThread* watchdog_thread() OVERRIDE;
-#if defined(OS_CHROMEOS)
-  virtual base::Thread* web_socket_proxy_thread() OVERRIDE;
-#endif
   virtual ProfileManager* profile_manager() OVERRIDE;
   virtual PrefService* local_state() OVERRIDE;
   virtual SidebarManager* sidebar_manager() OVERRIDE;

chrome/browser/chrome_browser_main.cc
@@ -1426,8 +1426,9 @@ void ChromeBrowserMainParts::PostStartThread(
 #if defined(USE_LINUX_BREAKPAD)
       // Needs to be called after we have chrome::DIR_USER_DATA and
       // g_browser_process.  This happens in PreCreateThreads.
-      g_browser_process->file_thread()->message_loop()->PostTask(
-          FROM_HERE, base::Bind(&GetLinuxDistroCallback));
+      BrowserThread::PostTask(BrowserThread::FILE,
+                              FROM_HERE,
+                              base::Bind(&GetLinuxDistroCallback));
 
       if (IsCrashReportingEnabled(local_state_))
         InitCrashReporter();

chrome/browser/chromeos/boot_times_loader.cc
@@ -121,7 +121,7 @@ BootTimesLoader* BootTimesLoader::Get() {
 BootTimesLoader::Handle BootTimesLoader::GetBootTimes(
     CancelableRequestConsumerBase* consumer,
     BootTimesLoader::GetBootTimesCallback* callback) {
-  if (!g_browser_process->file_thread()) {
+  if (!BrowserThread::IsMessageLoopValid(BrowserThread::FILE)) {
     // This should only happen if Chrome is shutting down, so we don't do
     // anything.
     return 0;

chrome/browser/chromeos/version_loader.cc
@@ -48,7 +48,7 @@ VersionLoader::Handle VersionLoader::GetVersion(
     CancelableRequestConsumerBase* consumer,
     const VersionLoader::GetVersionCallback& callback,
     VersionFormat format) {
-  if (!g_browser_process->file_thread()) {
+  if (!BrowserThread::IsMessageLoopValid(BrowserThread::FILE)) {
     // This should only happen if Chrome is shutting down, so we don't do
     // anything.
     return 0;
@@ -57,7 +57,8 @@ VersionLoader::Handle VersionLoader::GetVersion(
   scoped_refptr<GetVersionRequest> request(new GetVersionRequest(callback));
   AddRequest(request, consumer);
 
-  g_browser_process->file_thread()->message_loop()->PostTask(
+  BrowserThread::PostTask(
+      BrowserThread::FILE,
       FROM_HERE,
       NewRunnableMethod(backend_.get(), &Backend::GetVersion, request, format));
   return request->handle();
@@ -66,7 +67,7 @@ VersionLoader::Handle VersionLoader::GetVersion(
 VersionLoader::Handle VersionLoader::GetFirmware(
     CancelableRequestConsumerBase* consumer,
     const VersionLoader::GetFirmwareCallback& callback) {
-  if (!g_browser_process->file_thread()) {
+  if (!BrowserThread::IsMessageLoopValid(BrowserThread::FILE)) {
     // This should only happen if Chrome is shutting down, so we don't do
     // anything.
     return 0;
@@ -75,7 +76,8 @@ VersionLoader::Handle VersionLoader::GetFirmware(
   scoped_refptr<GetFirmwareRequest> request(new GetFirmwareRequest(callback));
   AddRequest(request, consumer);
 
-  g_browser_process->file_thread()->message_loop()->PostTask(
+  BrowserThread::PostTask(
+      BrowserThread::FILE,
       FROM_HERE,
       NewRunnableMethod(backend_.get(), &Backend::GetFirmware, request));
   return request->handle();

commit 00a1666c824f10d99e12457ac375a782822fa555

Allow SessionBackend::Init() to occur just-in-time in production builds.
This fixes a regression introduced in r113377 that caused the "old
session" to be switched at start-up before being used to populate the
recently closed menu.

This is the simplest possible fix, intended for merge to M18.  A more
robust fix that prevents future regressions is likely possible and may
be investigated as a follow-up.

BUG=110785
TEST=see bug repro steps

Review URL: https://chromiumcodereview.appspot.com/9361056

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121878 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: dde09b167921faa9e550373b0746042d6e50df81

chrome/browser/sessions/base_session_service.cc
@@ -91,8 +91,15 @@ BaseSessionService::BaseSessionService(SessionType type,
       profile_ ? profile_->GetPath() : path_);
   DCHECK(backend_.get());
 
-  RunTaskOnBackendThread(FROM_HERE,
-                         base::Bind(&SessionBackend::Init, backend_));
+  if (!RunningInProduction()) {
+    // We seem to be running as part of a test, in which case we need
+    // to explicitly initialize the backend.  In production, the
+    // backend will automatically initialize itself just in time.
+    //
+    // Note that it's important not to initialize too early in
+    // production; this can cause e.g. http://crbug.com/110785.
+    backend_->Init();
+  }
 }
 
 BaseSessionService::~BaseSessionService() {
@@ -322,10 +329,14 @@ BaseSessionService::Handle BaseSessionService::ScheduleGetLastSessionCommands(
   return request->handle();
 }
 
+bool BaseSessionService::RunningInProduction() const {
+  return profile_ && BrowserThread::IsMessageLoopValid(BrowserThread::FILE);
+}
+
 bool BaseSessionService::RunTaskOnBackendThread(
     const tracked_objects::Location& from_here,
     const base::Closure& task) {
-  if (profile_ && BrowserThread::IsMessageLoopValid(BrowserThread::FILE)) {
+  if (RunningInProduction()) {
     return BrowserThread::PostTask(BrowserThread::FILE, from_here, task);
   } else {
     // Fall back to executing on the main thread if the file thread

chrome/browser/sessions/base_session_service.h
@@ -149,6 +149,11 @@ class BaseSessionService : public CancelableRequestProvider,
       InternalGetCommandsRequest* request,
       CancelableRequestConsumerBase* consumer);
 
+  // Returns true if we appear to be running in production, false if
+  // we appear to be running as part of a unit test or if the FILE
+  // thread has gone away.
+  bool RunningInProduction() const;
+
   // In production, this posts the task to the FILE thread.  For
   // tests, it immediately runs the specified task on the current
   // thread.