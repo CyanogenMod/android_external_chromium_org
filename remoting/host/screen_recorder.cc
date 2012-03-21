CyanogenMod/android_external_chromium_org

commit 585abcf7130e1d4a96d69f924f025e409d6e1ce9

Use scoped_ptr<> to pass ownership in more places.
Review URL: http://codereview.chromium.org/9720019

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127767 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 3361e1f24a5dd45ae86478af20c617d0b1ce40e0

remoting/host/screen_recorder.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -234,42 +234,30 @@ void ScreenRecorder::DoInvalidateFullScreen() {
 
 // Network thread --------------------------------------------------------------
 
-void ScreenRecorder::DoSendVideoPacket(VideoPacket* packet) {
+void ScreenRecorder::DoSendVideoPacket(scoped_ptr<VideoPacket> packet) {
   DCHECK(network_loop_->BelongsToCurrentThread());
 
-  bool last = (packet->flags() & VideoPacket::LAST_PARTITION) != 0;
-
-  if (network_stopped_ || connections_.empty()) {
-    delete packet;
+  if (network_stopped_ || connections_.empty())
     return;
-  }
 
-  for (ConnectionToClientList::const_iterator i = connections_.begin();
-       i < connections_.end(); ++i) {
-    base::Closure done_task;
-
-    // Call FrameSentCallback() only for the last packet in the first
-    // connection.
-    if (last && i == connections_.begin()) {
-      done_task = base::Bind(&ScreenRecorder::FrameSentCallback, this, packet);
-    } else {
-      // TODO(hclam): Fix this code since it causes multiple deletion if there's
-      // more than one connection.
-      done_task = base::Bind(&base::DeletePointer<VideoPacket>, packet);
-    }
-
-    (*i)->video_stub()->ProcessVideoPacket(packet, done_task);
-  }
+  // TODO(sergeyu): Currently we send the data only to the first
+  // connection. Send it to all connections if necessary.
+  connections_.front()->video_stub()->ProcessVideoPacket(
+      packet.get(), base::Bind(
+          &ScreenRecorder::VideoPacketSentCallback, this,
+          base::Passed(packet.Pass())));
 }
 
-void ScreenRecorder::FrameSentCallback(VideoPacket* packet) {
-  delete packet;
-
+void ScreenRecorder::VideoPacketSentCallback(scoped_ptr<VideoPacket> packet) {
   if (network_stopped_)
     return;
 
-  capture_loop_->PostTask(
-      FROM_HERE, base::Bind(&ScreenRecorder::DoFinishOneRecording, this));
+  if ((packet->flags() & VideoPacket::LAST_PARTITION) != 0) {
+    // Post DoFinishOneRecording() if that was the last packet for the
+    // frame.
+    capture_loop_->PostTask(
+        FROM_HERE, base::Bind(&ScreenRecorder::DoFinishOneRecording, this));
+  }
 }
 
 void ScreenRecorder::DoStopOnNetworkThread(const base::Closure& done_task) {
@@ -297,11 +285,11 @@ void ScreenRecorder::DoEncode(
   // Early out if there's nothing to encode.
   if (!capture_data || capture_data->dirty_region().isEmpty()) {
     // Send an empty video packet to keep network active.
-    VideoPacket* packet = new VideoPacket();
+    scoped_ptr<VideoPacket> packet(new VideoPacket());
     packet->set_flags(VideoPacket::LAST_PARTITION);
     network_loop_->PostTask(
         FROM_HERE, base::Bind(&ScreenRecorder::DoSendVideoPacket,
-                              this, packet));
+                              this, base::Passed(packet.Pass())));
     return;
   }
 
@@ -322,7 +310,8 @@ void ScreenRecorder::DoStopOnEncodeThread(const base::Closure& done_task) {
   capture_loop_->PostTask(FROM_HERE, done_task);
 }
 
-void ScreenRecorder::EncodedDataAvailableCallback(VideoPacket* packet) {
+void ScreenRecorder::EncodedDataAvailableCallback(
+    scoped_ptr<VideoPacket> packet) {
   DCHECK_EQ(encode_loop_, MessageLoop::current());
 
   if (encoder_stopped_)
@@ -338,7 +327,8 @@ void ScreenRecorder::EncodedDataAvailableCallback(VideoPacket* packet) {
   }
 
   network_loop_->PostTask(
-      FROM_HERE, base::Bind(&ScreenRecorder::DoSendVideoPacket, this, packet));
+      FROM_HERE, base::Bind(&ScreenRecorder::DoSendVideoPacket, this,
+                            base::Passed(packet.Pass())));
 }
 
 }  // namespace remoting

remoting/base/codec_test.cc
@@ -211,16 +211,14 @@ class EncoderTester {
     EXPECT_GT(data_available_, 0);
   }
 
-  void DataAvailable(VideoPacket *packet) {
+  void DataAvailable(scoped_ptr<VideoPacket> packet) {
     ++data_available_;
-    message_tester_->ReceivedPacket(packet);
+    message_tester_->ReceivedPacket(packet.get());
 
     // Send the message to the DecoderTester.
     if (decoder_tester_) {
-      decoder_tester_->ReceivedPacket(packet);
+      decoder_tester_->ReceivedPacket(packet.get());
     }
-
-    delete packet;
   }
 
   void AddRects(const SkIRect* rects, int count) {

remoting/base/encoder.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -27,9 +27,7 @@ class Encoder {
   // DataAvailableCallback is called as blocks of data are made available
   // from the encoder. Data made available by the encoder is in the form
   // of HostMessage to reduce the amount of memory copies.
-  // The callback takes ownership of the HostMessage and is responsible for
-  // deleting it.
-  typedef base::Callback<void(VideoPacket*)> DataAvailableCallback;
+  typedef base::Callback<void(scoped_ptr<VideoPacket>)> DataAvailableCallback;
 
   virtual ~Encoder() {}
 

remoting/base/encoder_row_based.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -86,21 +86,21 @@ void EncoderRowBased::EncodeRect(const SkIRect& rect, bool last) {
 
   compressor_->Reset();
 
-  VideoPacket* packet = new VideoPacket();
-  PrepareUpdateStart(rect, packet);
+  scoped_ptr<VideoPacket> packet(new VideoPacket());
+  PrepareUpdateStart(rect, packet.get());
   const uint8* in = capture_data_->data_planes().data[0] +
       rect.fTop * strides + rect.fLeft * bytes_per_pixel;
   // TODO(hclam): Fill in the sequence number.
-  uint8* out = GetOutputBuffer(packet, packet_size_);
+  uint8* out = GetOutputBuffer(packet.get(), packet_size_);
   int filled = 0;
   int row_pos = 0;  // Position in the current row in bytes.
   int row_y = 0;  // Current row.
   bool compress_again = true;
   while (compress_again) {
     // Prepare a message for sending out.
-    if (!packet) {
-      packet = new VideoPacket();
-      out = GetOutputBuffer(packet, packet_size_);
+    if (!packet.get()) {
+      packet.reset(new VideoPacket());
+      out = GetOutputBuffer(packet.get(), packet_size_);
       filled = 0;
     }
 
@@ -132,8 +132,7 @@ void EncoderRowBased::EncodeRect(const SkIRect& rect, bool last) {
     // If we have filled the message or we have reached the end of stream.
     if (filled == packet_size_ || !compress_again) {
       packet->mutable_data()->resize(filled);
-      callback_.Run(packet);
-      packet = NULL;
+      callback_.Run(packet.Pass());
     }
 
     // Reached the end of input row and we're not at the last row.
@@ -168,6 +167,5 @@ uint8* EncoderRowBased::GetOutputBuffer(VideoPacket* packet, size_t size) {
   return const_cast<uint8*>(reinterpret_cast<const uint8*>(
       packet->mutable_data()->data()));
 }
-
 
 }  // namespace remoting

remoting/base/encoder_vp8.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -259,41 +259,41 @@ void EncoderVp8::Encode(scoped_refptr<CaptureData> capture_data,
 
   // TODO(hclam): Make sure we get exactly one frame from the packet.
   // TODO(hclam): We should provide the output buffer to avoid one copy.
-  VideoPacket* message = new VideoPacket();
+  scoped_ptr<VideoPacket> packet(new VideoPacket());
 
   while (!got_data) {
-    const vpx_codec_cx_pkt_t* packet = vpx_codec_get_cx_data(codec_.get(),
-                                                             &iter);
-    if (!packet)
+    const vpx_codec_cx_pkt_t* vpx_packet = vpx_codec_get_cx_data(codec_.get(),
+                                                                 &iter);
+    if (!vpx_packet)
       continue;
 
-    switch (packet->kind) {
+    switch (vpx_packet->kind) {
       case VPX_CODEC_CX_FRAME_PKT:
         got_data = true;
         // TODO(sergeyu): Split each frame into multiple partitions.
-        message->set_data(packet->data.frame.buf, packet->data.frame.sz);
+        packet->set_data(vpx_packet->data.frame.buf, vpx_packet->data.frame.sz);
         break;
       default:
         break;
     }
   }
 
-  message->mutable_format()->set_encoding(VideoPacketFormat::ENCODING_VP8);
-  message->set_flags(VideoPacket::FIRST_PACKET | VideoPacket::LAST_PACKET |
+  packet->mutable_format()->set_encoding(VideoPacketFormat::ENCODING_VP8);
+  packet->set_flags(VideoPacket::FIRST_PACKET | VideoPacket::LAST_PACKET |
                      VideoPacket::LAST_PARTITION);
-  message->mutable_format()->set_screen_width(capture_data->size().width());
-  message->mutable_format()->set_screen_height(capture_data->size().height());
-  message->set_capture_time_ms(capture_data->capture_time_ms());
-  message->set_client_sequence_number(capture_data->client_sequence_number());
+  packet->mutable_format()->set_screen_width(capture_data->size().width());
+  packet->mutable_format()->set_screen_height(capture_data->size().height());
+  packet->set_capture_time_ms(capture_data->capture_time_ms());
+  packet->set_client_sequence_number(capture_data->client_sequence_number());
   for (size_t i = 0; i < updated_rects.size(); ++i) {
-    Rect* rect = message->add_dirty_rects();
+    Rect* rect = packet->add_dirty_rects();
     rect->set_x(updated_rects[i].fLeft);
     rect->set_y(updated_rects[i].fTop);
     rect->set_width(updated_rects[i].width());
     rect->set_height(updated_rects[i].height());
   }
 
-  data_available_callback.Run(message);
+  data_available_callback.Run(packet.Pass());
 }
 
 }  // namespace remoting

remoting/base/encoder_vp8_unittest.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -29,8 +29,7 @@ TEST(EncoderVp8Test, TestEncoder) {
 
 class EncoderCallback {
  public:
-  void DataAvailable(VideoPacket *packet) {
-    delete packet;
+  void DataAvailable(scoped_ptr<VideoPacket> packet) {
   }
 };
 

remoting/host/chromoting_host.cc
@@ -261,10 +261,10 @@ void ChromotingHost::OnIncomingSession(
   LOG(INFO) << "Client connected: " << session->jid();
 
   // Create a client object.
-  protocol::ConnectionToClient* connection =
-      new protocol::ConnectionToClient(session);
+  scoped_ptr<protocol::ConnectionToClient> connection(
+      new protocol::ConnectionToClient(session));
   ClientSession* client = new ClientSession(
-      this, connection, desktop_environment_->event_executor(),
+      this, connection.Pass(), desktop_environment_->event_executor(),
       desktop_environment_->capturer());
   clients_.push_back(client);
 }

remoting/host/chromoting_host_unittest.cc
@@ -104,8 +104,10 @@ class ChromotingHostTest : public testing::Test {
     local_input_monitor_ = new MockLocalInputMonitor();
     it2me_host_user_interface_.reset(new It2MeHostUserInterface(host_,
                                                                 &context_));
-    it2me_host_user_interface_->InitFrom(disconnect_window_, continue_window_,
-                                         local_input_monitor_);
+    it2me_host_user_interface_->InitFrom(
+        scoped_ptr<DisconnectWindow>(disconnect_window_),
+        scoped_ptr<ContinueWindow>(continue_window_),
+        scoped_ptr<LocalInputMonitor>(local_input_monitor_));
 
     session_ = new MockSession();
     session2_ = new MockSession();
@@ -178,10 +180,12 @@ class ChromotingHostTest : public testing::Test {
 
   // Helper method to pretend a client is connected to ChromotingHost.
   void SimulateClientConnection(int connection_index, bool authenticate) {
-    protocol::ConnectionToClient* connection = (connection_index == 0) ?
-        owned_connection_.release() : owned_connection2_.release();
+    scoped_ptr<protocol::ConnectionToClient> connection =
+        ((connection_index == 0) ? owned_connection_ : owned_connection2_).
+        PassAs<protocol::ConnectionToClient>();
+    protocol::ConnectionToClient* connection_ptr = connection.get();
     ClientSession* client = new ClientSession(
-        host_.get(), connection, event_executor_,
+        host_.get(), connection.Pass(), event_executor_,
         desktop_environment_->capturer());
     connection->set_host_stub(client);
 
@@ -191,7 +195,7 @@ class ChromotingHostTest : public testing::Test {
     if (authenticate) {
       context_.network_message_loop()->PostTask(
           FROM_HERE, base::Bind(&ClientSession::OnConnectionOpened,
-                                base::Unretained(client), connection));
+                                base::Unretained(client), connection_ptr));
     }
 
     if (connection_index == 0) {

commit 0014ba0ed362a5f7550e2c4e72604dbd89e9e41a

Fix crash caused by r127767 due to undefined order of argument parsing.
Previous ScreenRecorder was calling .get() and .Pass() for the same scoped_ptr
variable in one function call. These calls were executed in undefined order, so
it was crashing when compiled with GCC or VC, but not with clang.

Review URL: https://chromiumcodereview.appspot.com/9815031

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128102 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: f5ed21484ff1609b847c4e477b86a6bc67d151c9

remoting/host/screen_recorder.cc
@@ -240,10 +240,12 @@ void ScreenRecorder::DoSendVideoPacket(scoped_ptr<VideoPacket> packet) {
   if (network_stopped_ || connections_.empty())
     return;
 
+  VideoPacket* packet_ptr = packet.get();
+
   // TODO(sergeyu): Currently we send the data only to the first
   // connection. Send it to all connections if necessary.
   connections_.front()->video_stub()->ProcessVideoPacket(
-      packet.get(), base::Bind(
+      packet_ptr, base::Bind(
           &ScreenRecorder::VideoPacketSentCallback, this,
           base::Passed(packet.Pass())));
 }