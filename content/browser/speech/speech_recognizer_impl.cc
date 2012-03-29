CyanogenMod/android_external_chromium_org

commit 04b8de80543132697930d9db09470e08a15e9576

Fixed a regression on speech input caused by issue 9858007 (r128895) that caused renderer hang on speech input complete / abort.
BUG=120173
TEST=Speech input must not freeze the page upon abort/completion.


Review URL: http://codereview.chromium.org/9856021

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129173 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 3652800bfca27dd1817b2175abecec91572895b4

content/browser/speech/speech_recognizer_impl.cc
@@ -57,6 +57,7 @@ bool DetectClipping(const speech::AudioChunk& chunk) {
   return false;
 }
 
+void OnAudioClosed(AudioInputController*) {}
 }  // namespace
 
 SpeechRecognizer* SpeechRecognizer::Create(
@@ -157,7 +158,7 @@ void SpeechRecognizerImpl::AbortRecognition() {
 
   // Stop recording if required.
   if (audio_controller_.get()) {
-    CloseAudioControllerSynchronously();
+    CloseAudioControllerAsynchronously();
   }
 
   VLOG(1) << "SpeechRecognizer canceling recognition.";
@@ -172,7 +173,7 @@ void SpeechRecognizerImpl::StopAudioCapture() {
   if (!audio_controller_.get())
     return;
 
-  CloseAudioControllerSynchronously();
+  CloseAudioControllerAsynchronously();
   listener_->OnSoundEnd(caller_id_);
   listener_->OnAudioEnd(caller_id_);
 
@@ -323,17 +324,14 @@ void SpeechRecognizerImpl::InformErrorAndAbortRecognition(
   listener_->OnRecognitionError(caller_id_, error);
 }
 
-void SpeechRecognizerImpl::CloseAudioControllerSynchronously() {
+void SpeechRecognizerImpl::CloseAudioControllerAsynchronously() {
   VLOG(1) << "SpeechRecognizer stopping record.";
-
-  // TODO(satish): investigate the possibility to utilize the closure
-  // and switch to async. version of this method. Compare with how
-  // it's done in e.g. the AudioRendererHost.
-  base::WaitableEvent closed_event(true, false);
-  audio_controller_->Close(base::Bind(&base::WaitableEvent::Signal,
-                           base::Unretained(&closed_event)));
-  closed_event.Wait();
-  audio_controller_ = NULL;  // Releases the ref ptr.
+  // Issues a Close on the audio controller, passing an empty callback. The only
+  // purpose of such callback is to keep the audio controller refcounted until
+  // Close has completed (in the audio thread) and automatically destroy it
+  // afterwards (upon return from OnAudioClosed).
+  audio_controller_->Close(base::Bind(&OnAudioClosed, audio_controller_));
+  audio_controller_ = NULL;  // The controller is still refcounted by Bind.
 }
 
 bool SpeechRecognizerImpl::IsActive() const {

content/browser/speech/speech_recognizer_impl.h
@@ -84,8 +84,9 @@ class CONTENT_EXPORT SpeechRecognizerImpl
   // Handles OnData in the IO thread. Takes ownership of |raw_audio|.
   void HandleOnData(AudioChunk* raw_audio);
 
-  // Helper method which closes the audio controller and blocks until done.
-  void CloseAudioControllerSynchronously();
+  // Helper method which closes the audio controller and frees it asynchronously
+  // without blocking the IO thread.
+  void CloseAudioControllerAsynchronously();
 
   void SetAudioManagerForTesting(AudioManager* audio_manager);
 

commit 127e52850821d726664301f08e78c662003ce06f

Fixed a regression on speech arisen after r129173 .
Made OnAudioClosed a member of SpeechRecognizerImpl. In this way, the closure used in the call to audio_controller_->Close() will hold a reference on SpeechRecognizerImpl until the Close has finished. Without this, the OnData method might be called from the audio thread after the object has been destructed.

BUG=120173
TEST=none.

Review URL: https://chromiumcodereview.appspot.com/9791070

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129606 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 2a8c471f6fbeb867cd5187e7149288591de571b2

content/browser/speech/speech_recognizer_impl.cc
@@ -57,7 +57,6 @@ bool DetectClipping(const speech::AudioChunk& chunk) {
   return false;
 }
 
-void OnAudioClosed(AudioInputController*) {}
 }  // namespace
 
 SpeechRecognizer* SpeechRecognizer::Create(
@@ -302,6 +301,8 @@ void SpeechRecognizerImpl::HandleOnData(scoped_refptr<AudioChunk> raw_audio) {
     StopAudioCapture();
 }
 
+void SpeechRecognizerImpl::OnAudioClosed(AudioInputController*) {}
+
 void SpeechRecognizerImpl::OnSpeechRecognitionEngineResult(
     const content::SpeechRecognitionResult& result) {
   // Guard against the listener freeing us until we finish our job.
@@ -331,7 +332,8 @@ void SpeechRecognizerImpl::CloseAudioControllerAsynchronously() {
   // purpose of such callback is to keep the audio controller refcounted until
   // Close has completed (in the audio thread) and automatically destroy it
   // afterwards (upon return from OnAudioClosed).
-  audio_controller_->Close(base::Bind(&OnAudioClosed, audio_controller_));
+  audio_controller_->Close(base::Bind(&SpeechRecognizerImpl::OnAudioClosed,
+                                      this, audio_controller_));
   audio_controller_ = NULL;  // The controller is still refcounted by Bind.
 }
 

content/browser/speech/speech_recognizer_impl.h
@@ -85,6 +85,8 @@ class CONTENT_EXPORT SpeechRecognizerImpl
   // Handles OnData in the IO thread.
   void HandleOnData(scoped_refptr<AudioChunk> raw_audio);
 
+  void OnAudioClosed(media::AudioInputController*);
+
   // Helper method which closes the audio controller and frees it asynchronously
   // without blocking the IO thread.
   void CloseAudioControllerAsynchronously();