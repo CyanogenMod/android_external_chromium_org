CyanogenMod/android_external_chromium_org

commit c6ed439b4816f45f19b9fa68cea8bbf39a1b462e

net: Move the logic reading upload elements to UploadData::Element.
This is yet another clean-up patch to simplify UploadDataStream
in preparation for making UploadDataStream::Read asynchronous.

Managing the offset and the file stream inside UploadData::Element
is cleaner anyway.

BUG=72001
TEST=net_unittests


Review URL: http://codereview.chromium.org/9452040

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123677 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: ff3993b56a8cdd80418a86545dff86fee6b9c9f5

net/base/upload_data.cc
@@ -37,6 +37,7 @@ UploadData::Element::Element()
       override_content_length_(false),
       content_length_computed_(false),
       content_length_(-1),
+      offset_(0),
       file_stream_(NULL) {
 }
 
@@ -82,7 +83,7 @@ uint64 UploadData::Element::GetContentLength() {
   // We need to open the file here to decide if we should report the file's
   // size or zero.  We cache the open file, so that we can still read it when
   // it comes time to.
-  file_stream_ = NewFileStreamForReading();
+  file_stream_ = OpenFileStream();
   if (!file_stream_)
     return 0;
 
@@ -98,16 +99,22 @@ uint64 UploadData::Element::GetContentLength() {
   return content_length_;
 }
 
-FileStream* UploadData::Element::NewFileStreamForReading() {
-  // In common usage GetContentLength() will call this first and store the
-  // result into |file_| and a subsequent call (from UploadDataStream) will
-  // get the cached open FileStream.
-  if (file_stream_) {
-    FileStream* file = file_stream_;
-    file_stream_ = NULL;
-    return file;
+int UploadData::Element::ReadSync(char* buf, int buf_len) {
+  if (type_ == UploadData::TYPE_BYTES || type_ == UploadData::TYPE_CHUNK) {
+    return ReadFromMemorySync(buf, buf_len);
+  } else if (type_ == UploadData::TYPE_FILE) {
+    return ReadFromFileSync(buf, buf_len);
   }
 
+  NOTREACHED();
+  return 0;
+}
+
+uint64 UploadData::Element::BytesRemaining() {
+  return GetContentLength() - offset_;
+}
+
+FileStream* UploadData::Element::OpenFileStream() {
   scoped_ptr<FileStream> file(new FileStream(NULL));
   int64 rv = file->OpenSync(
       file_path_,
@@ -129,6 +136,61 @@ FileStream* UploadData::Element::NewFileStreamForReading() {
   }
 
   return file.release();
+}
+
+int UploadData::Element::ReadFromMemorySync(char* buf, int buf_len) {
+  DCHECK_LT(0, buf_len);
+  DCHECK(type_ == UploadData::TYPE_BYTES || type_ == UploadData::TYPE_CHUNK);
+
+  const size_t num_bytes_to_read = std::min(BytesRemaining(),
+                                            static_cast<uint64>(buf_len));
+
+  // Check if we have anything to copy first, because we are getting
+  // the address of an element in |bytes_| and that will throw an
+  // exception if |bytes_| is an empty vector.
+  if (num_bytes_to_read > 0) {
+    memcpy(buf, &bytes_[offset_], num_bytes_to_read);
+  }
+
+  offset_ += num_bytes_to_read;
+  return num_bytes_to_read;
+}
+
+int UploadData::Element::ReadFromFileSync(char* buf, int buf_len) {
+  DCHECK_LT(0, buf_len);
+  DCHECK_EQ(UploadData::TYPE_FILE, type_);
+
+  // Open the file of the current element if not yet opened.
+  // In common usage, GetContentLength() opened it already.
+  if (!file_stream_) {
+    // Temporarily allow until fix: http://crbug.com/72001.
+    base::ThreadRestrictions::ScopedAllowIO allow_io;
+    file_stream_ = OpenFileStream();
+  }
+
+  const int num_bytes_to_read =
+      static_cast<int>(std::min(BytesRemaining(),
+                                static_cast<uint64>(buf_len)));
+  if (num_bytes_to_read > 0) {
+    int num_bytes_consumed = 0;
+    // Temporarily allow until fix: http://crbug.com/72001.
+    base::ThreadRestrictions::ScopedAllowIO allow_io;
+    // file_stream_ is NULL if the target file is
+    // missing or not readable.
+    if (file_stream_) {
+      num_bytes_consumed =
+          file_stream_->ReadSync(buf, num_bytes_to_read);
+    }
+    if (num_bytes_consumed <= 0) {
+      // If there's less data to read than we initially observed, then
+      // pad with zero.  Otherwise the server will hang waiting for the
+      // rest of the data.
+      memset(buf, 0, num_bytes_to_read);
+    }
+  }
+
+  offset_ += num_bytes_to_read;
+  return num_bytes_to_read;
 }
 
 UploadData::UploadData()
@@ -205,6 +267,11 @@ bool UploadData::IsInMemory() const {
       return false;
   }
   return true;
+}
+
+void UploadData::ResetOffset() {
+  for (size_t i = 0; i < elements_.size(); ++i)
+    elements_[i].ResetOffset();
 }
 
 void UploadData::SetElements(const std::vector<Element>& elements) {

net/base/upload_data.h
@@ -110,13 +110,31 @@ class NET_EXPORT UploadData : public base::RefCounted<UploadData> {
     // is returned.  This is done for consistency with Mozilla.
     uint64 GetContentLength();
 
-    // Returns a FileStream opened for reading for this element, positioned at
-    // |file_range_offset_|.  The caller gets ownership and is responsible
-    // for cleaning up the FileStream. Returns NULL if this element is not of
-    // type TYPE_FILE or if the file is not openable.
-    FileStream* NewFileStreamForReading();
+    // Reads up to |buf_len| bytes synchronously. Returns the number of bytes
+    // read. This function never fails. If there's less data to read than we
+    // initially observed, then pad with zero (this can happen with files).
+    // |buf_len| must be greater than 0.
+    int ReadSync(char* buf, int buf_len);
+
+    // Returns the number of bytes remaining to read.
+    uint64 BytesRemaining();
+
+    // Resets the offset to zero, so that the element can be reread.
+    void ResetOffset() { offset_ = 0; }
 
    private:
+    // Returns a FileStream opened for reading for this element, positioned
+    // at |file_range_offset_|. Returns NULL if the file is not openable.
+    FileStream* OpenFileStream();
+
+    // Reads up to |buf_len| bytes synchronously from memory (i.e. type_ is
+    // TYPE_BYTES or TYPE_CHUNK).
+    int ReadFromMemorySync(char* buf, int buf_len);
+
+    // Reads up to |buf_len| bytes synchronously from a file (i.e. type_ is
+    // TYPE_FILE).
+    int ReadFromFileSync(char* buf, int buf_len);
+
     // Allows tests to override the result of GetContentLength.
     void SetContentLength(uint64 content_length) {
       override_content_length_ = true;
@@ -134,6 +152,12 @@ class NET_EXPORT UploadData : public base::RefCounted<UploadData> {
     bool override_content_length_;
     bool content_length_computed_;
     uint64 content_length_;
+
+    // The byte offset from the beginning of the element data. Used to track
+    // the current position when reading data.
+    size_t offset_;
+
+    // The stream of the element data, if this element is of TYPE_FILE.
     FileStream* file_stream_;
 
     FRIEND_TEST_ALL_PREFIXES(UploadDataStreamTest, FileSmallerThanLength);
@@ -179,6 +203,11 @@ class NET_EXPORT UploadData : public base::RefCounted<UploadData> {
   // Returns true if the upload data is entirely in memory (i.e. the
   // upload data is not chunked, and all elemnts are of TYPE_BYTES).
   bool IsInMemory() const;
+
+  // Resets the offset of each upload data element to zero, so that the
+  // upload data can be reread. This can happen if the same upload data is
+  // reused for a new UploadDataStream.
+  void ResetOffset();
 
   std::vector<Element>* elements() {
     return &elements_;

net/base/upload_data_stream.cc
@@ -18,8 +18,6 @@ bool UploadDataStream::merge_chunks_ = true;
 UploadDataStream::UploadDataStream(UploadData* upload_data)
     : upload_data_(upload_data),
       element_index_(0),
-      element_offset_(0),
-      element_file_bytes_remaining_(0),
       total_size_(0),
       current_position_(0),
       initialized_successfully_(false) {
@@ -57,6 +55,10 @@ int UploadDataStream::Init() {
     }
   }
 
+  // Reset the offset, as upload_data_ may already be read (i.e. UploadData
+  // can be reused for a new UploadDataStream).
+  upload_data_->ResetOffset();
+
   initialized_successfully_ = true;
   return OK;
 }
@@ -66,79 +68,13 @@ int UploadDataStream::Read(IOBuffer* buf, int buf_len) {
 
   int bytes_copied = 0;
   while (bytes_copied < buf_len && element_index_ < elements.size()) {
-    bool advance_to_next_element = false;
-
-    // This is not const as GetContentLength() is not const.
     UploadData::Element& element = elements[element_index_];
 
-    const size_t free_buffer_space = buf_len - bytes_copied;
-    if (element.type() == UploadData::TYPE_BYTES ||
-        element.type() == UploadData::TYPE_CHUNK) {
-      const std::vector<char>& element_data = element.bytes();
-      const size_t num_bytes_left_in_element =
-          element_data.size() - element_offset_;
+    bytes_copied += element.ReadSync(buf->data() + bytes_copied,
+                                     buf_len - bytes_copied);
 
-      const size_t num_bytes_to_copy = std::min(num_bytes_left_in_element,
-                                                free_buffer_space);
-
-      // Check if we have anything to copy first, because we are getting
-      // the address of an element in |element_data| and that will throw an
-      // exception if |element_data| is an empty vector.
-      if (num_bytes_to_copy > 0) {
-        memcpy(buf->data() + bytes_copied, &element_data[element_offset_],
-               num_bytes_to_copy);
-        bytes_copied += num_bytes_to_copy;
-        element_offset_ += num_bytes_to_copy;
-      }
-
-      // Advance to the next element if we have consumed all data in the
-      // current element.
-      if (element_offset_ == element_data.size())
-        advance_to_next_element = true;
-    } else {
-      DCHECK(element.type() == UploadData::TYPE_FILE);
-
-      // Open the file of the current element if not yet opened.
-      if (!element_file_stream_.get()) {
-        element_file_bytes_remaining_ = element.GetContentLength();
-        // Temporarily allow until fix: http://crbug.com/72001.
-        base::ThreadRestrictions::ScopedAllowIO allow_io;
-        element_file_stream_.reset(element.NewFileStreamForReading());
-      }
-
-      const int num_bytes_to_read =
-          static_cast<int>(std::min(element_file_bytes_remaining_,
-                                    static_cast<uint64>(free_buffer_space)));
-      if (num_bytes_to_read > 0) {
-        int num_bytes_consumed = 0;
-        // Temporarily allow until fix: http://crbug.com/72001.
-        base::ThreadRestrictions::ScopedAllowIO allow_io;
-        // element_file_stream_.get() is NULL if the target file is
-        // missing or not readable.
-        if (element_file_stream_.get()) {
-          num_bytes_consumed =
-              element_file_stream_->ReadSync(buf->data() + bytes_copied,
-                                             num_bytes_to_read);
-        }
-        if (num_bytes_consumed <= 0) {
-          // If there's less data to read than we initially observed, then
-          // pad with zero.  Otherwise the server will hang waiting for the
-          // rest of the data.
-          memset(buf->data() + bytes_copied, 0, num_bytes_to_read);
-          num_bytes_consumed = num_bytes_to_read;
-        }
-        bytes_copied += num_bytes_consumed;
-        element_file_bytes_remaining_ -= num_bytes_consumed;
-      }
-
-      // Advance to the next element if we have consumed all data in the
-      // current element.
-      if (element_file_bytes_remaining_ == 0)
-        advance_to_next_element = true;
-    }
-
-    if (advance_to_next_element)
-      AdvanceToNextElement();
+    if (element.BytesRemaining() == 0)
+        ++element_index_;
 
     if (is_chunked() && !merge_chunks_)
       break;
@@ -149,18 +85,6 @@ int UploadDataStream::Read(IOBuffer* buf, int buf_len) {
     return ERR_IO_PENDING;
 
   return bytes_copied;
-}
-
-void UploadDataStream::AdvanceToNextElement() {
-  ++element_index_;
-  element_offset_ = 0;
-  element_file_bytes_remaining_ = 0;
-  if (element_file_stream_.get()) {
-    // Temporarily allow until fix: http://crbug.com/72001.
-    base::ThreadRestrictions::ScopedAllowIO allow_io;
-    element_file_stream_->CloseSync();
-    element_file_stream_.reset();
-  }
 }
 
 bool UploadDataStream::IsEOF() const {

net/base/upload_data_stream.h
@@ -69,27 +69,12 @@ class NET_EXPORT UploadDataStream {
   static void set_merge_chunks(bool merge) { merge_chunks_ = merge; }
 
  private:
-  // Advances to the next element. Updates the internal states.
-  void AdvanceToNextElement();
-
   scoped_refptr<UploadData> upload_data_;
 
   // Index of the current upload element (i.e. the element currently being
   // read). The index is used as a cursor to iterate over elements in
   // |upload_data_|.
   size_t element_index_;
-
-  // The byte offset into the current element's data buffer if the current
-  // element is a TYPE_BYTES or TYPE_DATA element.
-  size_t element_offset_;
-
-  // A stream to the currently open file, for the current element if the
-  // current element is a TYPE_FILE element.
-  scoped_ptr<FileStream> element_file_stream_;
-
-  // The number of bytes remaining to be read from the currently open file
-  // if the current element is of TYPE_FILE.
-  uint64 element_file_bytes_remaining_;
 
   // Size and current read position within the upload data stream.
   uint64 total_size_;

commit 06d56af147729fcb31218228c90aa206644314bc

net: Fix a regression that broke file uploading with http authentication
When uploading a file with http authentication, UploadData is reused
for a new UploadDataStream. Hence, we should close the files in UploadData
if already opened and read, so we can reread the files from the beginning.

The regression was caused by r123677.  Special thanks to thomas.themel for
identifying the offending patch and helping us to fix the issue.

BUG=128574
TEST=added a unit test; manually confirmed that uploading a file with basic authentication, and multi-round authentication with NTLM and Negotiate worked.

Review URL: https://chromiumcodereview.appspot.com/10408042

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138168 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 0a716afa3254aef87f0d03967d6d2c67dff026c5

net/base/upload_data.cc
@@ -114,6 +114,20 @@ uint64 UploadData::Element::BytesRemaining() {
   return GetContentLength() - offset_;
 }
 
+void UploadData::Element::ResetOffset() {
+  offset_ = 0;
+
+  // Delete the file stream if already opened, so we can reread the file from
+  // the beginning.
+  if (file_stream_) {
+    // Temporarily allow until fix: http://crbug.com/72001.
+    base::ThreadRestrictions::ScopedAllowIO allow_io;
+    file_stream_->CloseSync();
+    delete file_stream_;
+    file_stream_ = NULL;
+  }
+}
+
 FileStream* UploadData::Element::OpenFileStream() {
   scoped_ptr<FileStream> file(new FileStream(NULL));
   int64 rv = file->OpenSync(

net/base/upload_data.h
@@ -129,8 +129,9 @@ class NET_EXPORT UploadData
     // Returns the number of bytes remaining to read.
     uint64 BytesRemaining();
 
-    // Resets the offset to zero, so that the element can be reread.
-    void ResetOffset() { offset_ = 0; }
+    // Resets the offset to zero and closes the file stream if opened, so
+    // that the element can be reread.
+    void ResetOffset();
 
    private:
     // Returns a FileStream opened for reading for this element, positioned

net/base/upload_data_stream_unittest.cc
@@ -27,6 +27,17 @@ const char kTestData[] = "0123456789";
 const size_t kTestDataSize = arraysize(kTestData) - 1;
 const size_t kTestBufferSize = 1 << 14;  // 16KB.
 
+// Reads data from the upload data stream, and returns the data as string.
+std::string ReadFromUploadDataStream(UploadDataStream* stream) {
+  std::string data_read;
+  scoped_refptr<IOBuffer> buf = new IOBuffer(kTestBufferSize);
+  while (!stream->IsEOF()) {
+    const int bytes_read = stream->Read(buf, kTestBufferSize);
+    data_read.append(buf->data(), bytes_read);
+  }
+  return data_read;
+}
+
 }  // namespace
 
 class UploadDataStreamTest : public PlatformTest {
@@ -140,6 +151,38 @@ TEST_F(UploadDataStreamTest, FileChanged) {
   FileChangedHelper(temp_file_path,
                     file_info.last_modified - base::TimeDelta::FromSeconds(1),
                     true);
+
+  file_util::Delete(temp_file_path, false);
+}
+
+TEST_F(UploadDataStreamTest, UploadDataReused) {
+  FilePath temp_file_path;
+  ASSERT_TRUE(file_util::CreateTemporaryFile(&temp_file_path));
+  ASSERT_EQ(static_cast<int>(kTestDataSize),
+            file_util::WriteFile(temp_file_path, kTestData, kTestDataSize));
+
+  // Prepare |upload_data_| that contains a file.
+  std::vector<UploadData::Element> elements;
+  UploadData::Element element;
+  element.SetToFilePath(temp_file_path);
+  elements.push_back(element);
+  upload_data_->SetElements(elements);
+  EXPECT_EQ(kTestDataSize, upload_data_->GetContentLengthSync());
+
+  // Confirm that the file is read properly.
+  {
+    UploadDataStream stream(upload_data_);
+    ASSERT_EQ(OK, stream.Init());
+    EXPECT_EQ(kTestData, ReadFromUploadDataStream(&stream));
+  }
+
+  // Reuse |upload_data_| for another UploadDataStream, and confirm that the
+  // file is read properly.
+  {
+    UploadDataStream stream(upload_data_);
+    ASSERT_EQ(OK, stream.Init());
+    EXPECT_EQ(kTestData, ReadFromUploadDataStream(&stream));
+  }
 
   file_util::Delete(temp_file_path, false);
 }