CyanogenMod/android_external_chromium_org

commit 1c4f29b45e005985f3eed81b79958bcf08579012

Get rid of browser::FindTabbedBrowser call in pyauto's OpenProfileWindow. The tests that use it all use it to create a new window.
BUG=129187
Review URL: https://chromiumcodereview.appspot.com/10695121

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145946 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 9b524e760e55347ff146cf1cbd9d136763eb2375

chrome/browser/automation/testing_automation_provider.cc
@@ -1220,16 +1220,6 @@ void TestingAutomationProvider::OpenProfileWindow(
         "Invalid or missing arg: 'num_loads'");
     return;
   }
-  Browser* browser = browser::FindTabbedBrowser(profile, false);
-  if (browser) {
-    // Already have browser.  Need to just activate.
-    ProfileManager::FindOrCreateNewWindowForProfile(
-        profile,
-        chrome::startup::IS_NOT_PROCESS_STARTUP,
-        chrome::startup::IS_NOT_FIRST_RUN,
-        0);
-    AutomationJSONReply(this, reply_message).SendSuccess(NULL);
-  } else {
     new BrowserOpenedWithExistingProfileNotificationObserver(
         this, reply_message, num_loads);
     ProfileManager::FindOrCreateNewWindowForProfile(
@@ -1238,7 +1228,6 @@ void TestingAutomationProvider::OpenProfileWindow(
         chrome::startup::IS_NOT_FIRST_RUN,
         0);
   }
-}
 
 void TestingAutomationProvider::GetWindowForBrowser(int browser_handle,
                                                     bool* success,

chrome/test/pyautolib/pyauto.py
@@ -3968,8 +3968,8 @@ class PyUITest(pyautolib.PyUITestBase, unittest.TestCase):
    This is equivalent to picking a profile from the multi-profile menu.
 
    Multi-profile should be enabled and the requested profile should already
-   exist. If a window for the given profile already exists, it'll merely get
-   activated. Use OpenNewBrowserWindowWithNewProfile() to create a new profile.
+   exist. Creates a new window for the given profile. Use
+   OpenNewBrowserWindowWithNewProfile() to create a new profile.
 
    Args:
      path: profile path of the profile to be opened.

commit 0041a08a0d98b290ba0d8b9acdad9fdc3ef1e6bb

Fix the pyauto test that I broke in r145946. Review URL: https://chromiumcodereview.appspot.com/10689149
git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146013 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 0946b9f4cbc2c00c5e29fa612ababb6760d52c18

chrome/browser/automation/testing_automation_provider.cc
@@ -1220,13 +1220,13 @@ void TestingAutomationProvider::OpenProfileWindow(
         "Invalid or missing arg: 'num_loads'");
     return;
   }
-    new BrowserOpenedWithExistingProfileNotificationObserver(
-        this, reply_message, num_loads);
-    ProfileManager::FindOrCreateNewWindowForProfile(
-        profile,
-        chrome::startup::IS_NOT_PROCESS_STARTUP,
-        chrome::startup::IS_NOT_FIRST_RUN,
-        0);
+  new BrowserOpenedWithExistingProfileNotificationObserver(
+      this, reply_message, num_loads);
+  ProfileManager::FindOrCreateNewWindowForProfile(
+      profile,
+      chrome::startup::IS_NOT_PROCESS_STARTUP,
+      chrome::startup::IS_NOT_FIRST_RUN,
+      0);
   }
 
 void TestingAutomationProvider::GetWindowForBrowser(int browser_handle,

chrome/test/functional/bookmarks.py
@@ -664,8 +664,7 @@ class BookmarksTest(pyauto.PyUITest):
     # Launch profile1, verify bookmark is same as before.
     self.OpenProfileWindow(path=profile1_path)
     self._AssertBookmark('GoogleNews', 'http://news.google.com/', windex=1)
-    # Launch default, verify bookmark is changed to 'YouTube'.
-    self.OpenProfileWindow(path=default_path)
+    self.CloseBrowserWindow(1)
     self._AssertBookmark('YouTube', 'http://www.youtube.com/')
 
   def testSearchBookmarksInMultiProfile(self):