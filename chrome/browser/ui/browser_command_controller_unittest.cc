nwjs/chromium.src

commit b65cc0bf4c5054b8158ee3714b809840c93ae33d

Merge 166410 - Constrained Window Cocoa: Disable fullscreen
This CL prevents windows from entering or exiting fullscreen if a constrained window is attached.

BUG=146451


Review URL: https://chromiumcodereview.appspot.com/10947019

TBR=sail@chromium.org
Review URL: https://codereview.chromium.org/11359148

git-svn-id: svn://svn.chromium.org/chrome/branches/1312/src@167006 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/browser_command_controller.cc
@@ -26,6 +26,7 @@
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/tab_contents/tab_contents.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/browser/ui/tabs/tab_strip_model_utils.h"
 #include "chrome/browser/ui/webui/sync_promo/sync_promo_ui.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/common/pref_names.h"
@@ -856,9 +857,6 @@ void BrowserCommandController::InitCommandState() {
   command_updater_.UpdateCommandEnabled(IDC_HOME, normal_window);
 
   // Window management commands
-  // TODO(rohitrao): Disable fullscreen on non-Lion?
-  command_updater_.UpdateCommandEnabled(IDC_FULLSCREEN,
-      !(browser_->is_type_panel() && browser_->is_app()));
   command_updater_.UpdateCommandEnabled(IDC_SELECT_NEXT_TAB, normal_window);
   command_updater_.UpdateCommandEnabled(IDC_SELECT_PREVIOUS_TAB,
                                         normal_window);
@@ -881,11 +879,7 @@ void BrowserCommandController::InitCommandState() {
       IDC_WIN8_DESKTOP_RESTART : IDC_WIN8_METRO_RESTART;
   command_updater_.UpdateCommandEnabled(restart_mode, normal_window);
 #endif
-#if defined(OS_MACOSX)
   command_updater_.UpdateCommandEnabled(IDC_TABPOSE, normal_window);
-  command_updater_.UpdateCommandEnabled(IDC_PRESENTATION_MODE,
-      !(browser_->is_type_panel() && browser_->is_app()));
-#endif
 
   // Find-in-page
   command_updater_.UpdateCommandEnabled(IDC_FIND, !browser_->is_devtools());
@@ -1099,9 +1093,20 @@ void BrowserCommandController::UpdateCommandsForFullscreenMode(
 #endif
 
   // Disable explicit fullscreen toggling when in metro snap mode.
-  command_updater_.UpdateCommandEnabled(
-      IDC_FULLSCREEN,
-      fullscreen_mode != FULLSCREEN_METRO_SNAP);
+  bool fullscreen_enabled = !browser_->is_type_panel() &&
+                            !browser_->is_app() &&
+                            fullscreen_mode != FULLSCREEN_METRO_SNAP;
+#if defined(OS_MACOSX)
+  // The Mac implementation doesn't support switching to fullscreen while
+  // a tab modal dialog is displayed.
+  int tabIndex = chrome::IndexOfFirstBlockedTab(browser_->tab_strip_model());
+  bool has_blocked_tab = tabIndex != browser_->tab_strip_model()->count();
+  fullscreen_enabled &= !has_blocked_tab;
+#endif
+
+  command_updater_.UpdateCommandEnabled(IDC_FULLSCREEN, fullscreen_enabled);
+  command_updater_.UpdateCommandEnabled(IDC_PRESENTATION_MODE,
+                                        fullscreen_enabled);
 
   UpdateCommandsForBookmarkBar();
   UpdateCommandsForMultipleProfiles();

chrome/browser/ui/browser.cc
@@ -1784,6 +1784,7 @@ void Browser::SetTabContentBlocked(content::WebContents* web_contents,
   }
   tab_strip_model_->SetTabBlocked(index, blocked);
   command_controller_->PrintingStateChanged();
+  command_controller_->FullscreenStateChanged();
   if (!blocked && chrome::GetActiveWebContents(this) == web_contents)
     web_contents->Focus();
 }

chrome/browser/ui/cocoa/browser_window_controller.mm
@@ -996,14 +996,6 @@ enum {
   }
 }
 
-- (BOOL)supportsFullscreen {
-  // TODO(avi, thakis): GTMWindowSheetController has no api to move
-  // tabsheets between windows. Until then, we have to prevent having to
-  // move a tabsheet between windows, e.g. no fullscreen toggling
-  NSArray* a = [[tabStripController_ sheetController] viewsWithAttachedSheets];
-  return [a count] == 0;
-}
-
 // Called to validate menu and toolbar items when this window is key. All the
 // items we care about have been set with the |-commandDispatch:| or
 // |-commandDispatchUsingKeyModifiers:| actions and a target of FirstResponder
@@ -1033,7 +1025,6 @@ enum {
             enable &= !![[static_cast<NSMenuItem*>(item) keyEquivalent] length];
           break;
         case IDC_FULLSCREEN: {
-          enable &= [self supportsFullscreen];
           if ([static_cast<NSObject*>(item) isKindOfClass:[NSMenuItem class]]) {
             NSString* menuTitle = l10n_util::GetNSString(
                 [self isFullscreen] && ![self inPresentationMode] ?
@@ -1047,7 +1038,6 @@ enum {
           break;
         }
         case IDC_PRESENTATION_MODE: {
-          enable &= [self supportsFullscreen];
           if ([static_cast<NSObject*>(item) isKindOfClass:[NSMenuItem class]]) {
             NSString* menuTitle = l10n_util::GetNSString(
                 [self inPresentationMode] ? IDS_EXIT_PRESENTATION_MAC :
@@ -1950,7 +1940,7 @@ willAnimateFromState:(bookmarks::VisualState)oldState
   if (fullscreen == [self isFullscreen])
     return;
 
-  if (![self supportsFullscreen])
+  if (!chrome::IsCommandEnabled(browser_.get(), IDC_FULLSCREEN))
     return;
 
   if (base::mac::IsOSLionOrLater()) {

chrome/browser/ui/cocoa/browser_window_controller_unittest.mm
@@ -658,10 +658,6 @@ class BrowserWindowFullScreenControllerTest : public CocoaProfileTest {
   BrowserWindowController* controller_;
 };
 
-@interface BrowserWindowController (PrivateAPI)
-- (BOOL)supportsFullscreen;
-@end
-
 // Check if the window is front most or if one of its child windows (such
 // as a status bubble) is front most.
 static bool IsFrontWindow(NSWindow *window) {

chrome/browser/ui/cocoa/constrained_window/constrained_window_mac_browsertest.mm
@@ -4,7 +4,9 @@
 
 #include "chrome/browser/ui/cocoa/constrained_window/constrained_window_mac2.h"
 
+#include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/cocoa/browser_window_controller.h"
 #include "chrome/browser/ui/tab_contents/tab_contents.h"
@@ -118,4 +120,17 @@ IN_PROC_BROWSER_TEST_F(ConstrainedWindowMacTest, TabClose) {
   EXPECT_TRUE(browser()->tab_strip_model()->CloseTabContentsAt(
       1, TabStripModel::CLOSE_USER_GESTURE));
   EXPECT_EQ(1, browser()->tab_count());
+}
+
+// Test that adding a sheet disables fullscreen.
+IN_PROC_BROWSER_TEST_F(ConstrainedWindowMacTest, Fullscreen) {
+  EXPECT_TRUE(chrome::IsCommandEnabled(browser(), IDC_FULLSCREEN));
+
+  // Dialog will delete it self when closed.
+  NiceMock<ConstrainedWindowDelegateMock> delegate;
+  ConstrainedWindowMac2 dialog(&delegate, tab1_, sheet_);
+
+  EXPECT_FALSE(chrome::IsCommandEnabled(browser(), IDC_FULLSCREEN));
+
+  dialog.CloseConstrainedWindow();
 }

CyanogenMod/android_external_chromium_org

commit 0abcdb09709e533ed1e2de0a24ffcb441f54780d

Enable fullscreen for apps windows
This CL allows apps windows to enter fullscreen on all platforms.

This fixes a regression introduced by r167006.

BUG=161246


Review URL: https://chromiumcodereview.appspot.com/11308085

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168694 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: bee9645265c07b29d7d65acdd4257bfa3c06d424

chrome/browser/ui/browser.h
@@ -518,6 +518,7 @@ class Browser : public TabStripModelObserver,
   FRIEND_TEST_ALL_PREFIXES(BrowserTest, AppIdSwitch);
   FRIEND_TEST_ALL_PREFIXES(BrowserWithTestWindowTest,
                            IsReservedCommandOrKeyIsApp);
+  FRIEND_TEST_ALL_PREFIXES(BrowserWithTestWindowTest, AppFullScreen);
   FRIEND_TEST_ALL_PREFIXES(FullscreenControllerTest,
                            TabEntersPresentationModeFromWindowed);
   FRIEND_TEST_ALL_PREFIXES(FullscreenExitBubbleControllerTest,

chrome/browser/ui/browser_command_controller.cc
@@ -1102,10 +1102,11 @@ void BrowserCommandController::UpdateCommandsForFullscreenMode(
   command_updater_.UpdateCommandEnabled(IDC_PROFILING_ENABLED, show_main_ui);
 #endif
 
-  // Disable explicit fullscreen toggling when in metro snap mode.
-  bool fullscreen_enabled = !browser_->is_type_panel() &&
-                            !browser_->is_app() &&
-                            fullscreen_mode != FULLSCREEN_METRO_SNAP;
+  // Disable explicit fullscreen toggling for app-panels and when in metro snap
+  // mode.
+  bool fullscreen_enabled =
+      !(browser_->is_type_panel() && browser_->is_app()) &&
+      fullscreen_mode != FULLSCREEN_METRO_SNAP;
 #if defined(OS_MACOSX)
   // The Mac implementation doesn't support switching to fullscreen while
   // a tab modal dialog is displayed.

chrome/browser/ui/browser_command_controller_unittest.cc
@@ -6,6 +6,7 @@
 
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_window_state.h"
 #include "chrome/test/base/browser_with_test_window_test.h"
 #include "content/public/browser/native_web_keyboard_event.h"
@@ -116,4 +117,29 @@ TEST_F(BrowserWithTestWindowTest, IsReservedCommandOrKeyIsApp) {
       IDC_FIND, content::NativeWebKeyboardEvent(
           ui::ET_KEY_PRESSED, false, ui::VKEY_F, ui::EF_CONTROL_DOWN, 0)));
 #endif  // USE_AURA
+}
+
+TEST_F(BrowserWithTestWindowTest, AppFullScreen) {
+  // Enable for tabbed browser.
+  EXPECT_TRUE(chrome::IsCommandEnabled(browser(), IDC_FULLSCREEN));
+
+  // Enabled for app windows.
+  browser()->app_name_ = "app";
+  ASSERT_TRUE(browser()->is_app());
+  browser()->command_controller()->FullscreenStateChanged();
+  EXPECT_TRUE(chrome::IsCommandEnabled(browser(), IDC_FULLSCREEN));
+
+  // Enabled for panels.
+  Browser::CreateParams panel_params(Browser::TYPE_PANEL, profile());
+  TestBrowserWindow panel_window;
+  panel_params.window = &panel_window;
+  Browser panel_browser(panel_params);
+  ASSERT_TRUE(panel_browser.is_type_panel());
+  EXPECT_TRUE(chrome::IsCommandEnabled(&panel_browser, IDC_FULLSCREEN));
+
+  // Disabled for app-panels.
+  panel_browser.app_name_ = "app";
+  ASSERT_TRUE(panel_browser.is_app());
+  panel_browser.command_controller()->FullscreenStateChanged();
+  EXPECT_FALSE(chrome::IsCommandEnabled(&panel_browser, IDC_FULLSCREEN));
 }