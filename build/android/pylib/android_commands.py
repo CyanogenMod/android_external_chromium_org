CyanogenMod/android_external_chromium_org

commit 438db72b9224d5f3f8b733427518d490cd0753a6

[Android] Attempt to fix timeouts on Android bots.
Use subprocess communicate for getting md5sum hashes on the host.

BUG=224382
TBR=craigdh@chromium.org

Review URL: https://codereview.chromium.org/13693002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192444 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: e2d81b055fbfcdaff73c52da161ce330ea25b0ab

build/android/pylib/android_commands.py
@@ -682,10 +682,9 @@ class AndroidCommands(object):
         self.RunShellCommand(self._util_wrapper + ' ' + MD5SUM_DEVICE_PATH +
                              ' ' + device_path))
     assert os.path.exists(local_path), 'Local path not found %s' % local_path
-    hashes_on_host = _ComputeFileListHash(
-        subprocess.Popen(
-            '%s_host %s' % (self._md5sum_path, local_path),
-            stdout=subprocess.PIPE, shell=True).stdout)
+    md5sum_output = cmd_helper.GetCmdOutput(
+        ['%s_host' % self._md5sum_path, local_path])
+    hashes_on_host = _ComputeFileListHash(md5sum_output)
     if hashes_on_device == hashes_on_host:
       return
 

commit 169da0ec5dd867f30aaa94117625365885d71972

[Android] Fix parsing of md5sum_bin_host's output broken in r192444.
BUG=

Review URL: https://codereview.chromium.org/13703002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192603 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: f226fbb9ac71ded68081bee5074d3cf33800a313

build/android/pylib/android_commands.py
@@ -684,7 +684,7 @@ class AndroidCommands(object):
     assert os.path.exists(local_path), 'Local path not found %s' % local_path
     md5sum_output = cmd_helper.GetCmdOutput(
         ['%s_host' % self._md5sum_path, local_path])
-    hashes_on_host = _ComputeFileListHash(md5sum_output)
+    hashes_on_host = _ComputeFileListHash(md5sum_output.splitlines())
     if hashes_on_device == hashes_on_host:
       return
 