CyanogenMod/android_external_chromium_org

commit 5ba108b9eb0fdf80c03d80d3de9a09c7bfb3128b

WebKit merge 41498:41530.
Review URL: http://codereview.chromium.org/41017

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11312 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: f296ba562c8a5170962d4282e6c782fbedf774be

DEPS
@@ -1,7 +1,7 @@
 vars = {
   "webkit_trunk":
     "http://svn.webkit.org/repository/webkit/trunk",
-  "webkit_revision": "41498",
+  "webkit_revision": "41530",
 }
 
 
@@ -19,7 +19,7 @@ deps = {
     "http://googletest.googlecode.com/svn/trunk@167",
 
   "src/third_party/WebKit":
-    "/trunk/deps/third_party/WebKit@11253",
+    "/trunk/deps/third_party/WebKit@11310",
 
   "src/third_party/icu38":
     "/trunk/deps/third_party/icu38@10692",

WEBKIT_MERGE_REVISION
@@ -1,1 +1,1 @@
-http://svn.webkit.org/repository/webkit/trunk@41498
+http://svn.webkit.org/repository/webkit/trunk@41530

webkit/build/WebCore/SConscript
@@ -684,6 +684,7 @@ input_files = [
     '$WEBCORE_DIR/rendering/RenderWidget.cpp',
     '$WEBCORE_DIR/rendering/RenderWordBreak.cpp',
     '$WEBCORE_DIR/rendering/RootInlineBox.cpp',
+    '$WEBCORE_DIR/rendering/ScrollBehavior.cpp',
     '$WEBCORE_DIR/rendering/SVGCharacterLayoutInfo.cpp',
     '$WEBCORE_DIR/rendering/SVGInlineFlowBox.cpp',
     '$WEBCORE_DIR/rendering/SVGInlineTextBox.cpp',

webkit/build/WebCore/WebCore.vcproj
@@ -3793,6 +3793,14 @@
 				>
 			</File>
 			<File
+				RelativePath="..\..\..\third_party\WebKit\WebCore\rendering\ScrollBehavior.cpp"
+				>
+			</File>
+			<File
+				RelativePath="..\..\..\third_party\WebKit\WebCore\rendering\ScrollBehavior.h"
+				>
+			</File>
+			<File
 				RelativePath="..\..\..\third_party\WebKit\WebCore\rendering\TableLayout.h"
 				>
 			</File>

webkit/glue/editor_client_impl.cc
@@ -23,6 +23,7 @@ MSVC_PUSH_WARNING_LEVEL(0);
 #include "KeyboardEvent.h"
 #include "PlatformKeyboardEvent.h"
 #include "PlatformString.h"
+#include "RenderObject.h"
 MSVC_POP_WARNING();
 
 #include "WebKit.h"

webkit/glue/searchable_form_data.cc
@@ -7,6 +7,7 @@
 #include "base/compiler_specific.h"
 
 MSVC_PUSH_WARNING_LEVEL(0);
+#include "CSSHelper.h"
 #include "CString.h"
 #include "Document.h"
 #include "DocumentLoader.h"
@@ -296,7 +297,7 @@ WebCore::HTMLInputElement* GetTextElement(
 } // namespace
 
 SearchableFormData* SearchableFormData::Create(WebCore::Element* element) {
-	if (!element->isFormControlElement() || !element->isHTMLElement()) {
+  if (!element->isFormControlElement() || !element->isHTMLElement()) {
     return NULL;
   }
 

webkit/glue/webframeloaderclient_impl.cc
@@ -1218,7 +1218,9 @@ void WebFrameLoaderClient::frameLoadCompleted() {
   // Note: Can be called multiple times.
   // Even if already complete, we might have set a previous item on a frame that
   // didn't do any data loading on the past transaction. Make sure to clear these out.
-  webframe_->frame()->loader()->setPreviousHistoryItem(0);
+
+  // FIXME: setPreviousHistoryItem() no longer exists. http://crbug.com/8566
+  // webframe_->frame()->loader()->setPreviousHistoryItem(0);
 }
 
 void WebFrameLoaderClient::saveViewStateToItem(HistoryItem*) {

webkit/port/bindings/v8/v8_proxy.cpp
@@ -132,6 +132,8 @@
 #include "Settings.h"
 #include "StyleSheet.h"
 #include "StyleSheetList.h"
+#include "SVGColor.h"
+#include "SVGPaint.h"
 #include "TextEvent.h"
 #include "TextMetrics.h"
 #include "TimeRanges.h"

commit 3ff6fe4990c3b814916471768cd9805398c9463d

NaCl: Update revision in DEPS, r11298 -> r11321
This pulls in the following Native Client changes:

r11299: (dschuff) Update toolchain tester to allow translation flags to be specified, and use them to enable EH.
r11300: (dschuff) Remove torture test shell script.
r11301: (kschimpf) Update PNaCl LLVM REV.
r11302: (jvoung) Remove pnacl/tc_component_packager.py, which was experimental and is unused.
r11303: (mseaborn) PNaCl: Move ExpandByVal pass to run later, after link-time optimisations
r11304: (kschimpf) Update PNaCL LLVM REV.
r11305: (eliben) Roll binutils in pnacl/DEPS to include the addition of the --allow-unresolved flag to the bitcode linker.
r11306: (eliben) PNaCl: Run LowerInvoke when exceptions are disabled even for mixed-object linking
r11307: (khim) Add presubmit script validator_ragel
r11308: (jvoung) Add -strip-dead-prototypes to PNaCl pass list to appease intrinsic verifier.
r11309: (dschuff) Don't build arm-flavored bitcode components
r11310: (mseaborn) Pull in Breakpad via DEPS in order to get the minidump format headers
r11312: (mseaborn) Add a minimal Breakpad-compatible minidump generator for crash reporting
r11313: (mseaborn) Fix checkdeps failure introduced by r11312
r11314: (khim) Replace dfacheckdecoder with dfacheckvalidator in PRESUBMIT.py
r11315: (eliben) Remove _Unwind_* stubs from pnacl_abi.bc
r11316: (mcgrathr) Use -march=armv7-a on Android/ARM
r11317: (sehr) Pull in divide-by-zero test change for ARM.
r11318: (jvoung) Disable ABI check for stack alignment test that uses __builtin_dwarf_cfa.
r11319: (bsy) fixes bug in mmap failures not being propagated
r11320: (mseaborn) libpthread: Add pthread_get_stack_end_np() API for use by crash dumper
r11321: (mseaborn) Update GDB revision in tools/REVISIONS to get core dump fix

BUG=none
TEST=nacl_integration

Review URL: https://codereview.chromium.org/14813005

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199231 0039d316-1c4b-4281-b951-d872f2087c98

DEPS
@@ -12,7 +12,7 @@ vars = {
   "chromium_git": "https://chromium.googlesource.com",
   "chromiumos_git": "https://chromium.googlesource.com/chromiumos",
   "swig_revision": "69281",
-  "nacl_revision": "11298",
+  "nacl_revision": "11321",
   # After changing nacl_revision, run 'glient sync' and check native_client/DEPS
   # to update other nacl_*_revision's.
   "nacl_tools_revision": "11040",  # native_client/DEPS: tools_rev