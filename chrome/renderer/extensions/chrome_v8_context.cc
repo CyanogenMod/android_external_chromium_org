CyanogenMod/android_external_chromium_org

commit b4ac42e311856c8bf1b152ef928ad12e2cebce62

Replace all occurrences of CallChromeHiddenMethod (and friends) with CallModuleMethod.
This also makes ExtensionMsg_MessageInvoke run a module system function rather
than a chromeHidden function, meaning we can now lazily load the extension
event handling code (i.e. miscellaneous_bindings).

BUG=55316
TBR=cdn@chromium.org, koz@chromium.org

Review URL: https://codereview.chromium.org/15855010

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204157 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 68e63ea168c14341044bbdb24526a06c6825fd71

chrome/renderer/extensions/chrome_v8_context.cc
@@ -4,7 +4,6 @@
 
 #include "chrome/renderer/extensions/chrome_v8_context.h"
 
-#include "base/debug/trace_event.h"
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/strings/string_split.h"
@@ -19,6 +18,7 @@
 #include "content/public/renderer/render_view.h"
 #include "content/public/renderer/v8_value_converter.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebFrame.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebScopedMicrotaskSuppression.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebView.h"
 #include "v8/include/v8.h"
 
@@ -27,11 +27,7 @@ using content::V8ValueConverter;
 namespace extensions {
 
 namespace {
-
 const char kChromeHidden[] = "chromeHidden";
-const char kValidateCallbacks[] = "validateCallbacks";
-const char kValidateAPI[] = "validateAPI";
-
 }  // namespace
 
 ChromeV8Context::ChromeV8Context(v8::Handle<v8::Context> v8_context,
@@ -55,7 +51,7 @@ ChromeV8Context::~ChromeV8Context() {
 }
 
 void ChromeV8Context::Invalidate() {
-  if (v8_context_.get().IsEmpty())
+  if (!is_valid())
     return;
   if (module_system_)
     module_system_->Invalidate();
@@ -77,16 +73,6 @@ v8::Handle<v8::Value> ChromeV8Context::GetOrCreateChromeHidden(
   if (hidden.IsEmpty() || hidden->IsUndefined()) {
     hidden = v8::Object::New();
     global->SetHiddenValue(v8::String::New(kChromeHidden), hidden);
-
-    if (DCHECK_IS_ON()) {
-      // Tell bindings.js to validate callbacks and events against their schema
-      // definitions.
-      v8::Local<v8::Object>::Cast(hidden)->Set(
-          v8::String::New(kValidateCallbacks), v8::True());
-      // Tell bindings.js to validate API for ambiguity.
-      v8::Local<v8::Object>::Cast(hidden)->Set(
-          v8::String::New(kValidateAPI), v8::True());
-    }
   }
 
   DCHECK(hidden->IsObject());
@@ -105,52 +91,23 @@ content::RenderView* ChromeV8Context::GetRenderView() const {
     return NULL;
 }
 
-bool ChromeV8Context::CallChromeHiddenMethod(
-    const std::string& function_name,
+v8::Local<v8::Value> ChromeV8Context::CallFunction(
+    v8::Handle<v8::Function> function,
     int argc,
-    v8::Handle<v8::Value>* argv,
-    v8::Handle<v8::Value>* result) const {
-  // ChromeV8ContextSet calls Invalidate() and then schedules a task to delete
-  // this object. This check prevents a race from attempting to execute script
-  // on a NULL web_frame_.
+    v8::Handle<v8::Value> argv[]) const {
+  v8::HandleScope handle_scope;
+  WebKit::WebScopedMicrotaskSuppression suppression;
+  if (!is_valid())
+    return handle_scope.Close(v8::Undefined());
+
+  v8::Handle<v8::Object> global = v8_context()->Global();
   if (!web_frame_)
-    return false;
-
-  v8::Context::Scope context_scope(v8_context_.get());
-
-  // Look up the function name, which may be a sub-property like
-  // "Port.dispatchOnMessage" in the hidden global variable.
-  v8::Local<v8::Value> value = v8::Local<v8::Value>::New(GetChromeHidden());
-  if (value.IsEmpty())
-    return false;
-
-  std::vector<std::string> components;
-  base::SplitStringDontTrim(function_name, '.', &components);
-  for (size_t i = 0; i < components.size(); ++i) {
-    if (!value.IsEmpty() && value->IsObject()) {
-      value = v8::Local<v8::Object>::Cast(value)->Get(
-          v8::String::New(components[i].c_str()));
-    }
-  }
-
-  if (value.IsEmpty() || !value->IsFunction()) {
-    VLOG(1) << "Could not execute chrome hidden method: " << function_name;
-    return false;
-  }
-
-  TRACE_EVENT1("v8", "v8.callChromeHiddenMethod",
-               "function_name", function_name);
-
-  v8::Local<v8::Function> function = v8::Local<v8::Function>::Cast(value);
-  v8::Handle<v8::Value> result_temp =
+    return handle_scope.Close(function->Call(global, argc, argv));
+  return handle_scope.Close(
       web_frame_->callFunctionEvenIfScriptDisabled(function,
-                                                   v8::Object::New(),
+                                                   global,
                                                    argc,
-                                                   argv);
-  if (result)
-    *result = result_temp;
-
-  return true;
+                                                   argv));
 }
 
 bool ChromeV8Context::IsAnyFeatureAvailableToContext(
@@ -171,8 +128,7 @@ Feature::Availability ChromeV8Context::GetAvailability(
 }
 
 void ChromeV8Context::DispatchOnUnloadEvent() {
-  v8::HandleScope handle_scope;
-  CallChromeHiddenMethod("dispatchOnUnload", 0, NULL, NULL);
+  module_system_->CallModuleMethod("unload_event", "dispatch");
 }
 
 std::string ChromeV8Context::GetContextTypeDescription() {
@@ -207,9 +163,9 @@ void ChromeV8Context::OnResponseReceived(const std::string& name,
     v8::String::New(error.c_str())
   };
 
-  v8::Handle<v8::Value> retval;
-  CHECK(CallChromeHiddenMethod("handleResponse", arraysize(argv), argv,
-                               &retval));
+  v8::Handle<v8::Value> retval = module_system_->CallModuleMethod(
+      "sendRequest", "handleResponse", arraysize(argv), argv);
+
   // In debug, the js will validate the callback parameters and return a
   // string if a validation error has occured.
   if (DCHECK_IS_ON()) {
@@ -218,6 +174,10 @@ void ChromeV8Context::OnResponseReceived(const std::string& name,
       DCHECK(false) << error;
     }
   }
+}
+
+bool ChromeV8Context::is_valid() const {
+  return !v8_context_.get().IsEmpty();
 }
 
 }  // namespace extensions

chrome/renderer/extensions/chrome_v8_context.h
@@ -85,18 +85,16 @@ class ChromeV8Context : public RequestSender::Source {
   // context is in the process of being destroyed.
   content::RenderView* GetRenderView() const;
 
+  // Runs |function| with appropriate scopes. Doesn't catch exceptions, callers
+  // must do that if they want.
+  //
+  // USE THIS METHOD RATHER THAN v8::Function::Call WHEREVER POSSIBLE.
+  v8::Local<v8::Value> CallFunction(v8::Handle<v8::Function> function,
+                                    int argc,
+                                    v8::Handle<v8::Value> argv[]) const;
+
   // Fires the onunload event on the chromeHidden object.
   void DispatchOnUnloadEvent();
-
-  // Call the named method of the chromeHidden object in this context.
-  // The function can be a sub-property like "Port.dispatchOnMessage". Returns
-  // the result of the function call in |result| if |result| is non-NULL. If the
-  // named method does not exist, returns false.
-  bool CallChromeHiddenMethod(
-      const std::string& function_name,
-      int argc,
-      v8::Handle<v8::Value>* argv,
-      v8::Handle<v8::Value>* result) const;
 
   // Returns the availability of the API |api_name|.
   Feature::Availability GetAvailability(const std::string& api_name);
@@ -118,6 +116,8 @@ class ChromeV8Context : public RequestSender::Source {
                                   const std::string& error) OVERRIDE;
 
  private:
+  bool is_valid() const;
+
   // The v8 context the bindings are accessible to.
   ScopedPersistent<v8::Context> v8_context_;
 

chrome/renderer/extensions/chrome_v8_context_set.cc
@@ -13,7 +13,6 @@
 #include "chrome/renderer/extensions/chrome_v8_context.h"
 #include "content/public/renderer/render_thread.h"
 #include "content/public/renderer/render_view.h"
-#include "content/public/renderer/v8_value_converter.h"
 #include "extensions/common/constants.h"
 #include "third_party/WebKit/public/platform/WebURL.h"
 #include "third_party/WebKit/public/platform/WebURLRequest.h"
@@ -23,7 +22,6 @@
 #include "v8/include/v8.h"
 
 using content::RenderThread;
-using content::V8ValueConverter;
 
 namespace extensions {
 
@@ -79,23 +77,16 @@ ChromeV8Context* ChromeV8ContextSet::GetByV8Context(
   return NULL;
 }
 
-void ChromeV8ContextSet::DispatchChromeHiddenMethod(
+void ChromeV8ContextSet::ForEach(
     const std::string& extension_id,
-    const std::string& method_name,
-    const base::ListValue& arguments,
-    content::RenderView* render_view) const {
-  v8::HandleScope handle_scope;
-
+    content::RenderView* render_view,
+    const base::Callback<void(ChromeV8Context*)>& callback) const {
   // We copy the context list, because calling into javascript may modify it
   // out from under us.
   ContextSet contexts = GetAll();
 
-  scoped_ptr<V8ValueConverter> converter(V8ValueConverter::create());
   for (ContextSet::iterator it = contexts.begin(); it != contexts.end();
        ++it) {
-    if ((*it)->v8_context().IsEmpty())
-      continue;
-
     if (!extension_id.empty()) {
       const Extension* extension = (*it)->extension();
       if (!extension || (extension_id != extension->id()))
@@ -109,17 +100,7 @@ void ChromeV8ContextSet::DispatchChromeHiddenMethod(
     if (render_view && render_view != context_render_view)
       continue;
 
-    v8::Local<v8::Context> context(*((*it)->v8_context()));
-    std::vector<v8::Handle<v8::Value> > v8_arguments;
-    for (size_t i = 0; i < arguments.GetSize(); ++i) {
-      const base::Value* item = NULL;
-      CHECK(arguments.Get(i, &item));
-      v8_arguments.push_back(converter->ToV8Value(item, context));
-    }
-
-    v8::Handle<v8::Value> retval;
-    (*it)->CallChromeHiddenMethod(
-        method_name, v8_arguments.size(), &v8_arguments[0], &retval);
+    callback.Run(*it);
   }
 }
 

chrome/browser/chromeos/extensions/file_manager/file_handler_util.cc
@@ -910,7 +910,7 @@ void ExtensionTaskExecutor::SetupPermissionsAndDispatchEvent(
   DictionaryValue* details = new DictionaryValue();
   event_args->Append(details);
   // Get file definitions. These will be replaced with Entry instances by
-  // chromeHidden.Event.dispatchEvent() method from event_binding.js.
+  // dispatchEvent() method from event_binding.js.
   ListValue* files_urls = new ListValue();
   details->Set("entries", files_urls);
   for (FileDefinitionList::const_iterator iter = file_list.begin();

chrome/browser/extensions/api/web_request/web_request_api_unittest.cc
@@ -120,9 +120,9 @@ void GetPartOfMessageArguments(IPC::Message* message,
                                ExtensionMsg_MessageInvoke::Param* param) {
   ASSERT_EQ(ExtensionMsg_MessageInvoke::ID, message->type());
   ASSERT_TRUE(ExtensionMsg_MessageInvoke::Read(message, param));
-  ASSERT_GE(param->c.GetSize(), 2u);
+  ASSERT_GE(param->d.GetSize(), 2u);
   const Value* value = NULL;
-  ASSERT_TRUE(param->c.Get(1, &value));
+  ASSERT_TRUE(param->d.Get(1, &value));
   const ListValue* list = NULL;
   ASSERT_TRUE(value->GetAsList(&list));
   ASSERT_EQ(1u, list->GetSize());
@@ -916,7 +916,7 @@ TEST_P(ExtensionWebRequestHeaderModificationTest, TestModifications) {
       continue;
     ExtensionMsg_MessageInvoke::Param message_tuple;
     ExtensionMsg_MessageInvoke::Read(message, &message_tuple);
-    ListValue& args = message_tuple.c;
+    ListValue& args = message_tuple.d;
 
     std::string event_name;
     if (!args.GetString(0, &event_name) ||

chrome/browser/extensions/app_window_contents.cc
@@ -95,6 +95,7 @@ void AppWindowContents::NativeWindowChanged(
   content::RenderViewHost* rvh = web_contents_->GetRenderViewHost();
   rvh->Send(new ExtensionMsg_MessageInvoke(rvh->GetRoutingID(),
                                            host_->extension()->id(),
+                                           "app.window",
                                            "updateAppWindowProperties",
                                            args,
                                            false));

chrome/browser/extensions/event_router.cc
@@ -45,8 +45,6 @@ namespace extensions {
 
 namespace {
 
-const char kDispatchEvent[] = "Event.dispatchEvent";
-
 void NotifyEventListenerRemovedOnIOThread(
     void* profile,
     const std::string& extension_id,
@@ -136,7 +134,8 @@ void EventRouter::DispatchExtensionMessage(IPC::Sender* ipc_sender,
   ipc_sender->Send(new ExtensionMsg_MessageInvoke(
       MSG_ROUTING_CONTROL,
       extension_id,
-      kDispatchEvent,
+      "event_bindings",
+      "dispatchEvent",
       args,
       user_gesture == USER_GESTURE_ENABLED));
 

chrome/chrome_renderer.gypi
@@ -201,6 +201,7 @@
         'renderer/resources/extensions/test_custom_bindings.js',
         'renderer/resources/extensions/tts_custom_bindings.js',
         'renderer/resources/extensions/tts_engine_custom_bindings.js',
+        'renderer/resources/extensions/unload_event.js',
         'renderer/resources/extensions/utils.js',
         'renderer/resources/extensions/web_request_custom_bindings.js',
         'renderer/resources/extensions/web_view.js',

commit 38af687a8a1bfc5bdea2e7afafa750dfecacd439

Check that ChromeV8Contexts are valid before running ChromeV8ContextSet::ForEach for them. This patch pre-empts likely breakage caused by r204157.
BUG=none
TBR=koz@chromium.org

Review URL: https://codereview.chromium.org/16469002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204403 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: dd218905608586ad381942d2cdda3cd5aa91111d

chrome/renderer/extensions/chrome_v8_context.cc
@@ -151,8 +151,4 @@ void ChromeV8Context::OnResponseReceived(const std::string& name,
   }
 }
 
-bool ChromeV8Context::is_valid() const {
-  return !v8_context_.get().IsEmpty();
-}
-
 }  // namespace extensions

chrome/renderer/extensions/chrome_v8_context.h
@@ -39,6 +39,12 @@ class ChromeV8Context : public RequestSender::Source {
   // ModuleSystem.
   void Invalidate();
 
+  // Returns true if this context is still valid, false if it isn't.
+  // A context becomes invalid via Invalidate().
+  bool is_valid() const {
+    return !v8_context_.get().IsEmpty();
+  }
+
   v8::Handle<v8::Context> v8_context() const {
     return v8_context_.get();
   }
@@ -100,8 +106,6 @@ class ChromeV8Context : public RequestSender::Source {
                                   const std::string& error) OVERRIDE;
 
  private:
-  bool is_valid() const;
-
   // The v8 context the bindings are accessible to.
   ScopedPersistent<v8::Context> v8_context_;
 

chrome/renderer/extensions/chrome_v8_context_set.cc
@@ -87,20 +87,26 @@ void ChromeV8ContextSet::ForEach(
 
   for (ContextSet::iterator it = contexts.begin(); it != contexts.end();
        ++it) {
+    ChromeV8Context* context = *it;
+
+    // For the same reason as above, contexts may become invalid while we run.
+    if (!context->is_valid())
+      continue;
+
     if (!extension_id.empty()) {
-      const Extension* extension = (*it)->extension();
+      const Extension* extension = context->extension();
       if (!extension || (extension_id != extension->id()))
         continue;
     }
 
-    content::RenderView* context_render_view = (*it)->GetRenderView();
+    content::RenderView* context_render_view = context->GetRenderView();
     if (!context_render_view)
       continue;
 
     if (render_view && render_view != context_render_view)
       continue;
 
-    callback.Run(*it);
+    callback.Run(context);
   }
 }
 