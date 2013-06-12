CyanogenMod/android_external_chromium_org

commit 78f22e505faee56c49c1640e54c87eb5450f717c

Don't compile a bunch of extension APIs that aren't needed on Android.
BUG=248318

Review URL: https://chromiumcodereview.appspot.com/16703009

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205899 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 0fa29f4213fae16ad36e0dd8c05d88177ce356f3

chrome/common/extensions/api/api.gyp
@@ -18,93 +18,109 @@
       ],
       'variables': {
         'chromium_code': 1,
-        'schema_files': [
-          'alarms.idl',
-          'activity_log_private.json',
-          'app_current_window_internal.idl',
-          'app_runtime.idl',
-          'app_window.idl',
-          'audio.idl',
-          'autotest_private.idl',
-          'bluetooth.idl',
-          'bookmark_manager_private.json',
-          'bookmarks.json',
-          'chromeos_info_private.json',
-          'cloud_print_private.json',
-          'command_line_private.json',
-          'content_settings.json',
-          'context_menus.json',
-          'cookies.json',
-          'debugger.json',
-          'developer_private.idl',
-          'dial.idl',
-          'downloads.idl',
-          'echo_private.json',
-          'downloads_internal.idl',
-          'enterprise_platform_keys_private.json',
-          'events.json',
-          'experimental_accessibility.json',
-          'experimental_discovery.idl',
-          'experimental_dns.idl',
-          'experimental_history.json',
-          'experimental_identity.idl',
-          'experimental_idltest.idl',
-          'experimental_infobars.json',
-          'location.idl',
-          'experimental_media_galleries.idl',
-          'experimental_record.json',
-          'system_info_memory.idl',
-          'experimental_system_info_storage.idl',
-          'extension.json',
-          'file_browser_handler_internal.json',
-          'file_system.idl',
-          'font_settings.json',
-          'history.json',
-          'i18n.json',
-          'identity.idl',
-          'identity_private.idl',
-          'idle.json',
-          'management.json',
-          'manifest_types.json',
-          'media_galleries.idl',
-          'media_galleries_private.idl',
-          'media_player_private.json',
-          'metrics_private.json',
-          'music_manager_private.idl',
-          'networking_private.json',
-          'notifications.idl',
-          'omnibox.json',
-          'page_capture.json',
-          'page_launcher.idl',
-          'permissions.json',
-          'power.idl',
-          'push_messaging.idl',
-          'rtc_private.idl',
-          'runtime.json',
-          'serial.idl',
-          'session_restore.json',
-          'socket.idl',
-          'storage.json',
-          'sync_file_system.idl',
-          'system_indicator.idl',
-          'system_info_cpu.idl',
-          'system_info_display.idl',
-          'system_private.json',
-          'tab_capture.idl',
-          'tabs.json',
-          'terminal_private.json',
-          'test.json',
-          'top_sites.json',
-          'usb.idl',
-          'wallpaper_private.json',
-          'web_navigation.json',
-          'web_request.json',
-          'web_socket_proxy_private.json',
-          'webview.json',
-          'windows.json',
-        ],
         'cc_dir': 'chrome/common/extensions/api',
         'root_namespace': 'extensions::api',
+
+        'conditions': [
+          ['OS=="android"', {
+            'schema_files': [  # Only these files are used on Android.
+              'activity_log_private.json',
+              'omnibox.json',
+              'storage.json',
+            ]
+          }],
+          ['chromeos==1', {
+            'schema_files': [  # Additional files for Chrome OS.
+              'file_browser_handler_internal.json',
+              'rtc_private.idl',
+            ]
+          }],
+          ['OS!="android"', {
+            'schema_files': [  # Files for all platforms except Android.
+              'alarms.idl',
+              'activity_log_private.json',
+              'app_current_window_internal.idl',
+              'app_runtime.idl',
+              'app_window.idl',
+              'audio.idl',
+              'autotest_private.idl',
+              'bluetooth.idl',
+              'bookmark_manager_private.json',
+              'bookmarks.json',
+              'chromeos_info_private.json',
+              'cloud_print_private.json',
+              'command_line_private.json',
+              'content_settings.json',
+              'context_menus.json',
+              'cookies.json',
+              'debugger.json',
+              'developer_private.idl',
+              'dial.idl',
+              'downloads.idl',
+              'echo_private.json',
+              'downloads_internal.idl',
+              'enterprise_platform_keys_private.json',
+              'events.json',
+              'experimental_accessibility.json',
+              'experimental_discovery.idl',
+              'experimental_dns.idl',
+              'experimental_history.json',
+              'experimental_identity.idl',
+              'experimental_idltest.idl',
+              'experimental_infobars.json',
+              'location.idl',
+              'experimental_media_galleries.idl',
+              'experimental_record.json',
+              'system_info_memory.idl',
+              'experimental_system_info_storage.idl',
+              'extension.json',
+              'file_system.idl',
+              'font_settings.json',
+              'history.json',
+              'i18n.json',
+              'identity.idl',
+              'identity_private.idl',
+              'idle.json',
+              'management.json',
+              'manifest_types.json',
+              'media_galleries.idl',
+              'media_galleries_private.idl',
+              'media_player_private.json',
+              'metrics_private.json',
+              'music_manager_private.idl',
+              'networking_private.json',
+              'notifications.idl',
+              'omnibox.json',
+              'page_capture.json',
+              'page_launcher.idl',
+              'permissions.json',
+              'power.idl',
+              'push_messaging.idl',
+              'runtime.json',
+              'serial.idl',
+              'session_restore.json',
+              'socket.idl',
+              'storage.json',
+              'sync_file_system.idl',
+              'system_indicator.idl',
+              'system_info_cpu.idl',
+              'system_info_display.idl',
+              'system_private.json',
+              'tab_capture.idl',
+              'tabs.json',
+              'terminal_private.json',
+              'test.json',
+              'top_sites.json',
+              'usb.idl',
+              'wallpaper_private.json',
+              'web_navigation.json',
+              'web_request.json',
+              'web_socket_proxy_private.json',
+              'webview.json',
+              'windows.json'
+            ]
+          }]
+        ]
       },
       'dependencies': [
         '<(DEPTH)/skia/skia.gyp:skia',

commit 1144ada32491c3050e874af62f099e1af5b0b9e6

Android release build fix
(Broken by r205899 even though trybots passed)

BUG=248318
TBR=mariakhomenko@chromium.org

Review URL: https://codereview.chromium.org/16818005

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205934 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: cadd30fea95dbec58c69078b959789be93917c37

chrome/common/extensions/api/api.gyp
@@ -27,6 +27,7 @@
               'activity_log_private.json',
               'omnibox.json',
               'storage.json',
+              'usb.idl',
             ]
           }],
           ['chromeos==1', {