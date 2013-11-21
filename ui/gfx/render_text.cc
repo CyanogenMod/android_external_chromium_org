CyanogenMod/android_external_chromium_org

commit a4af2453f8b54de94e5a50e97bc1cf09574766cf

Always aligns text at vertically center (Textfield, Label).
Re-applies http://crrev.com/25039002 with only the change s/numeric_limits<int>::max()/INT_MAX/

This CL makes Textfield and Label put the content text at vertically center respecting the cap height.  Also removes vertical alignment support (ALIGN_{TOP,BOTTOM,VCENTER}).

BUG=146236, 264436
TEST=Test manually + ui_unittests

Review URL: https://codereview.chromium.org/54353002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232374 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 56e5057b80bb8ce20bc3b6eae3e0d6cbf4078feb

ui/gfx/render_text.cc
@@ -5,6 +5,7 @@
 #include "ui/gfx/render_text.h"
 
 #include <algorithm>
+#include <climits>
 
 #include "base/i18n/break_iterator.h"
 #include "base/logging.h"
@@ -43,6 +44,28 @@ const SkScalar kUnderlineOffset = (SK_Scalar1 / 9);
 const SkScalar kLineThickness = (SK_Scalar1 / 18);
 // Fraction of the text size to use for a top margin of a diagonal strike.
 const SkScalar kDiagonalStrikeMarginOffset = (SK_Scalar1 / 4);
+
+// Invalid value of baseline.  Assigning this value to |baseline_| causes
+// re-calculation of baseline.
+const int kInvalidBaseline = INT_MAX;
+
+// Returns the baseline, with which the text best appears vertically centered.
+int DetermineBaselineCenteringText(const Rect& display_rect,
+                                   const FontList& font_list) {
+  const int display_height = display_rect.height();
+  const int font_height = font_list.GetHeight();
+  // Lower and upper bound of baseline shift as we try to show as much area of
+  // text as possible.  In particular case of |display_height| == |font_height|,
+  // we do not want to shift the baseline.
+  const int min_shift = std::min(0, display_height - font_height);
+  const int max_shift = std::abs(display_height - font_height);
+  const int baseline = font_list.GetBaseline();
+  const int cap_height = font_list.GetCapHeight();
+  const int internal_leading = baseline - cap_height;
+  const int baseline_shift =
+      (display_height - cap_height) / 2 - internal_leading;
+  return baseline + std::max(min_shift, std::min(max_shift, baseline_shift));
+}
 
 // Converts |gfx::Font::FontStyle| flags to |SkTypeface::Style| flags.
 SkTypeface::Style ConvertFontStyleToSkiaTypefaceStyle(int font_style) {
@@ -348,16 +371,9 @@ void RenderText::SetHorizontalAlignment(HorizontalAlignment alignment) {
   }
 }
 
-void RenderText::SetVerticalAlignment(VerticalAlignment alignment) {
-  if (vertical_alignment_ != alignment) {
-    vertical_alignment_ = alignment;
-    display_offset_ = Vector2d();
-    cached_bounds_and_offset_valid_ = false;
-  }
-}
-
 void RenderText::SetFontList(const FontList& font_list) {
   font_list_ = font_list;
+  baseline_ = kInvalidBaseline;
   cached_bounds_and_offset_valid_ = false;
   ResetLayout();
 }
@@ -414,6 +430,7 @@ void RenderText::SetMultiline(bool multiline) {
 
 void RenderText::SetDisplayRect(const Rect& r) {
   display_rect_ = r;
+  baseline_ = kInvalidBaseline;
   cached_bounds_and_offset_valid_ = false;
   lines_.clear();
 }
@@ -653,6 +670,13 @@ int RenderText::GetContentWidth() {
   return GetStringSize().width() + (cursor_enabled_ ? 1 : 0);
 }
 
+int RenderText::GetBaseline() {
+  if (baseline_ == kInvalidBaseline)
+    baseline_ = DetermineBaselineCenteringText(display_rect(), font_list());
+  DCHECK_NE(kInvalidBaseline, baseline_);
+  return baseline_;
+}
+
 void RenderText::Draw(Canvas* canvas) {
   EnsureLayout();
 
@@ -784,7 +808,6 @@ void RenderText::SetTextShadows(const ShadowValues& shadows) {
 
 RenderText::RenderText()
     : horizontal_alignment_(base::i18n::IsRTL() ? ALIGN_RIGHT : ALIGN_LEFT),
-      vertical_alignment_(ALIGN_VCENTER),
       directionality_mode_(DIRECTIONALITY_FROM_TEXT),
       text_direction_(base::i18n::UNKNOWN_DIRECTION),
       cursor_enabled_(true),
@@ -806,6 +829,7 @@ RenderText::RenderText()
       fade_tail_(false),
       background_is_transparent_(false),
       clip_to_display_rect_(true),
+      baseline_(kInvalidBaseline),
       cached_bounds_and_offset_valid_(false) {
 }
 
@@ -969,11 +993,7 @@ Vector2d RenderText::GetAlignmentOffset(size_t line_number) {
     if (horizontal_alignment_ == ALIGN_CENTER)
       offset.set_x(offset.x() / 2);
   }
-  if (vertical_alignment_ != ALIGN_TOP) {
-    offset.set_y(display_rect().height() - GetStringSize().height());
-    if (vertical_alignment_ == ALIGN_VCENTER)
-      offset.set_y(offset.y() / 2);
-  }
+  offset.set_y(GetBaseline() - GetLayoutTextBaseline());
   return offset;
 }
 

chrome/browser/ui/views/location_bar/content_setting_image_view.cc
@@ -34,7 +34,6 @@ ContentSettingImageView::ContentSettingImageView(
     ContentSettingsType content_type,
     LocationBarView* parent,
     const gfx::FontList& font_list,
-    int font_y_offset,
     SkColor text_color,
     SkColor parent_background_color)
     : parent_(parent),
@@ -53,8 +52,6 @@ ContentSettingImageView::ContentSettingImageView(
   AddChildView(icon_);
 
   text_label_->SetVisible(false);
-  text_label_->set_border(
-      views::Border::CreateEmptyBorder(font_y_offset, 0, 0, 0));
   text_label_->SetEnabledColor(text_color);
   // Calculate the actual background color for the label.  The background images
   // are painted atop |parent_background_color|.  We grab the color of the
@@ -210,7 +207,7 @@ void ContentSettingImageView::Layout() {
   text_label_->SetBounds(
       icon_->bounds().right() + LocationBarView::GetItemPadding(), 0,
       std::max(width() - GetTotalSpacingWhileAnimating() - icon_width, 0),
-      text_label_->GetPreferredSize().height());
+      height());
 }
 
 bool ContentSettingImageView::OnMousePressed(const ui::MouseEvent& event) {
@@ -280,4 +277,3 @@ void ContentSettingImageView::CreateBubble(content::WebContents* web_contents) {
   bubble_widget_->AddObserver(this);
   bubble_widget_->Show();
 }
-

chrome/browser/ui/views/location_bar/content_setting_image_view.h
@@ -39,7 +39,6 @@ class ContentSettingImageView : public gfx::AnimationDelegate,
   ContentSettingImageView(ContentSettingsType content_type,
                           LocationBarView* parent,
                           const gfx::FontList& font_list,
-                          int font_y_offset,
                           SkColor text_color,
                           SkColor parent_background_color);
   virtual ~ContentSettingImageView();

chrome/browser/ui/views/location_bar/ev_bubble_view.cc
@@ -13,13 +13,11 @@ const int kBackgroundImages[] = IMAGE_GRID(IDR_OMNIBOX_EV_BUBBLE);
 
 
 EVBubbleView::EVBubbleView(const gfx::FontList& font_list,
-                           int font_y_offset,
                            SkColor text_color,
                            SkColor parent_background_color,
                            LocationBarView* location_bar)
     : IconLabelBubbleView(kBackgroundImages, NULL, IDR_OMNIBOX_HTTPS_VALID,
-                          font_list, font_y_offset, text_color,
-                          parent_background_color, true),
+                          font_list, text_color, parent_background_color, true),
       page_info_helper_(this, location_bar) {
 }
 

chrome/browser/ui/views/location_bar/ev_bubble_view.h
@@ -14,7 +14,6 @@ class LocationBarView;
 class EVBubbleView : public IconLabelBubbleView {
  public:
   EVBubbleView(const gfx::FontList& font_list,
-               int font_y_offset,
                SkColor text_color,
                SkColor parent_background_color,
                LocationBarView* parent);
@@ -35,4 +34,3 @@ class EVBubbleView : public IconLabelBubbleView {
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_LOCATION_BAR_EV_BUBBLE_VIEW_H_
-

chrome/browser/ui/views/location_bar/icon_label_bubble_view.cc
@@ -19,7 +19,6 @@ IconLabelBubbleView::IconLabelBubbleView(const int background_images[],
                                          const int hover_background_images[],
                                          int contained_image,
                                          const gfx::FontList& font_list,
-                                         int font_y_offset,
                                          SkColor text_color,
                                          SkColor parent_background_color,
                                          bool elide_in_middle)
@@ -43,7 +42,6 @@ IconLabelBubbleView::IconLabelBubbleView(const int background_images[],
         views::Painter::CreateImageGridPainter(hover_background_images));
   }
 
-  label_->set_border(views::Border::CreateEmptyBorder(font_y_offset, 0, 0, 0));
   label_->SetEnabledColor(text_color);
   // Calculate the actual background color for the label.  The background images
   // are painted atop |parent_background_color|.  We grab the color of the
@@ -93,7 +91,7 @@ void IconLabelBubbleView::Layout() {
   const int pre_label_width = GetPreLabelWidth();
   label_->SetBounds(pre_label_width, 0,
                     width() - pre_label_width - GetBubbleOuterPadding(false),
-                    label_->GetPreferredSize().height());
+                    height());
 }
 
 gfx::Size IconLabelBubbleView::GetSizeForLabelWidth(int width) const {

chrome/browser/ui/views/location_bar/icon_label_bubble_view.h
@@ -28,14 +28,12 @@ class Painter;
 // tab-to-search UI.
 class IconLabelBubbleView : public views::View {
  public:
-  // The label will be positioned |font_y_offset| px. from the top of the view.
   // |hover_background_images| is an optional set of images to be used in place
   // of |background_images| during mouse hover.
   IconLabelBubbleView(const int background_images[],
                       const int hover_background_images[],
                       int contained_image,
                       const gfx::FontList& font_list,
-                      int font_y_offset,
                       SkColor text_color,
                       SkColor parent_background_color,
                       bool elide_in_middle);

chrome/browser/ui/views/location_bar/keyword_hint_view.cc
@@ -24,19 +24,18 @@
 
 KeywordHintView::KeywordHintView(Profile* profile,
                                  const gfx::FontList& font_list,
-                                 int font_y_offset,
                                  SkColor text_color,
                                  SkColor background_color)
     : profile_(profile),
       tab_image_(new views::ImageView()) {
   leading_label_ =
-      CreateLabel(font_list, font_y_offset, text_color, background_color);
+      CreateLabel(font_list, text_color, background_color);
   tab_image_->SetImage(
       ui::ResourceBundle::GetSharedInstance().GetImageSkiaNamed(
           IDR_OMNIBOX_KEYWORD_HINT_TAB));
   AddChildView(tab_image_);
   trailing_label_ =
-      CreateLabel(font_list, font_y_offset, text_color, background_color);
+      CreateLabel(font_list, text_color, background_color);
 }
 
 KeywordHintView::~KeywordHintView() {
@@ -84,21 +83,19 @@ void KeywordHintView::Layout() {
   bool show_labels = (width() != tab_width);
   gfx::Size leading_size(leading_label_->GetPreferredSize());
   leading_label_->SetBounds(0, 0, show_labels ? leading_size.width() : 0,
-                            leading_size.height());
+                            height());
   tab_image_->SetBounds(leading_label_->bounds().right(), 0, tab_width,
                         height());
   gfx::Size trailing_size(trailing_label_->GetPreferredSize());
   trailing_label_->SetBounds(tab_image_->bounds().right(), 0,
                              show_labels ? trailing_size.width() : 0,
-                             trailing_size.height());
+                             height());
 }
 
 views::Label* KeywordHintView::CreateLabel(const gfx::FontList& font_list,
-                                           int font_y_offset,
                                            SkColor text_color,
                                            SkColor background_color) {
   views::Label* label = new views::Label(string16(), font_list);
-  label->set_border(views::Border::CreateEmptyBorder(font_y_offset, 0, 0, 0));
   label->SetEnabledColor(text_color);
   label->SetBackgroundColor(background_color);
   AddChildView(label);

commit 3d1cc862f6e06338414a2d6375a36d4ea755a5f0

Fix multiline text offset calculation
This was broken since r232374.

BUG=320850

Review URL: https://codereview.chromium.org/75233003

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236635 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 6c0078f5314cbe48ebb591041776aa6bc26b2e6a

ui/gfx/render_text.cc
@@ -999,7 +999,16 @@ Vector2d RenderText::GetAlignmentOffset(size_t line_number) {
     if (horizontal_alignment_ == ALIGN_CENTER)
       offset.set_x(offset.x() / 2);
   }
-  offset.set_y(GetBaseline() - GetLayoutTextBaseline());
+
+  // Vertically center the text.
+  if (multiline_) {
+    const int text_height = lines_.back().preceding_heights +
+        lines_.back().size.height();
+    offset.set_y((display_rect_.height() - text_height) / 2);
+  } else {
+    offset.set_y(GetBaseline() - GetLayoutTextBaseline());
+  }
+
   return offset;
 }
 