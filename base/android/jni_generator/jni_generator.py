f7db7b5cdc12e2ad24aa5de4c98607b05b7f4e11
Arne Coucheron <arco68@gmail.com>
2013-11-28 13:07:43 +0100

    Revert "Some changes added to compile and run with Java 6 and Java 7."
     * Breaking build on Linux Mint with OpenJDK 6.
       http://pastebin.com/zciG3W68
    
    This reverts commit 85e22e7ff8f2c3d925b645a88f9b1410307d0f95.
    
    Change-Id: I438c4281654fcb66a317883302527bde3c560125

--- base/android/jni_generator/jni_generator.py
+++ base/android/jni_generator/jni_generator.py
@@ -500,13 +500,8 @@ class JNIFromJavaP(object):
 
   @staticmethod
   def CreateFromClass(class_file, namespace):
-    p = subprocess.Popen(args=['java', '-version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     class_name = os.path.splitext(os.path.basename(class_file))[0]
-    if re.match('.*?"1\.[56]', p.communicate()[1].split('\n')[0]) is not None:
-      javap_args = ['javap', class_name]
-    else:
-      javap_args = ['javap', '-XDcompat', class_name]
-    p = subprocess.Popen(args=javap_args,
+    p = subprocess.Popen(args=['javap', class_name],
                          cwd=os.path.dirname(class_file),
                          stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE)
