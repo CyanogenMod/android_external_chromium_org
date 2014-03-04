CyanogenMod/android_external_chromium_org

commit d3823d933694e516e85d720d3e2373ae7faa63be

Add SetOffset to PPB_Graphics2D_Dev
Add a SetOffset function to PPB_Graphics2D_Dev which allows the graphics context to be offset into the plugin element. This allows having a graphics context which is smaller than the plugin element and painting into only a portion of the plugin elements. This makes it possible to do viewporting for very large plugins.

R=piman@chromium.org, tsepez@chromium.org, yzshen@chromium.org

Review URL: https://codereview.chromium.org/25661002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235581 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: e00cae6ea8f2ce0dbfebbb43f6b010fd91ab6761

content/renderer/pepper/pepper_graphics_2d_host.cc
@@ -27,7 +27,6 @@
 #include "skia/ext/platform_canvas.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/gfx/blit.h"
-#include "ui/gfx/point.h"
 #include "ui/gfx/point_conversions.h"
 #include "ui/gfx/rect.h"
 #include "ui/gfx/rect_conversions.h"
@@ -136,7 +135,8 @@ struct PepperGraphics2DHost::QueuedOperation {
   enum Type {
     PAINT,
     SCROLL,
-    REPLACE
+    REPLACE,
+    SET_OFFSET
   };
 
   QueuedOperation(Type t)
@@ -160,6 +160,9 @@ struct PepperGraphics2DHost::QueuedOperation {
 
   // Valid when type == REPLACE.
   scoped_refptr<PPB_ImageData_Impl> replace_image;
+
+  // Valid when type == SET_OFFSET.
+  gfx::Point offset;
 };
 
 // static
@@ -240,6 +243,9 @@ int32_t PepperGraphics2DHost::OnResourceMessageReceived(
     PPAPI_DISPATCH_HOST_RESOURCE_CALL(
         PpapiHostMsg_Graphics2D_ReadImageData,
         OnHostMsgReadImageData)
+    PPAPI_DISPATCH_HOST_RESOURCE_CALL(
+        PpapiHostMsg_Graphics2D_SetOffset,
+        OnHostMsgSetOffset)
   IPC_END_MESSAGE_MAP()
   return PP_ERROR_FAILED;
 }
@@ -390,6 +396,8 @@ void PepperGraphics2DHost::Paint(blink::WebCanvas* canvas,
     pixel_origin.scale(inverse_scale);
     canvas->scale(scale_, scale_);
   }
+  pixel_origin.offset(SkIntToScalar(plugin_offset_.x()),
+                      SkIntToScalar(plugin_offset_.y()));
   canvas->drawBitmap(image, pixel_origin.x(), pixel_origin.y(), &paint);
 }
 
@@ -418,6 +426,12 @@ bool PepperGraphics2DHost::IsAlwaysOpaque() const {
 
 PPB_ImageData_Impl* PepperGraphics2DHost::ImageData() {
   return image_data_.get();
+}
+
+gfx::Size PepperGraphics2DHost::Size() const {
+  if (!image_data_)
+    return gfx::Size();
+  return gfx::Size(image_data_->width(), image_data_->height());
 }
 
 int32_t PepperGraphics2DHost::OnHostMsgPaintImageData(
@@ -549,6 +563,15 @@ int32_t PepperGraphics2DHost::OnHostMsgSetScale(
   return PP_ERROR_BADARGUMENT;
 }
 
+int32_t PepperGraphics2DHost::OnHostMsgSetOffset(
+    ppapi::host::HostMessageContext* context,
+    const PP_Point& offset) {
+  QueuedOperation operation(QueuedOperation::SET_OFFSET);
+  operation.offset = PP_ToGfxPoint(offset);
+  queued_operations_.push_back(operation);
+  return PP_OK;
+}
+
 int32_t PepperGraphics2DHost::OnHostMsgReadImageData(
     ppapi::host::HostMessageContext* context,
     PP_Resource image,
@@ -595,6 +618,7 @@ int32_t PepperGraphics2DHost::Flush(PP_Resource* old_image_data) {
   for (size_t i = 0; i < queued_operations_.size(); i++) {
     QueuedOperation& operation = queued_operations_[i];
     gfx::Rect op_rect;
+    gfx::Rect old_op_rect;
     switch (operation.type) {
       case QueuedOperation::PAINT:
         ExecutePaintImageData(operation.paint_image.get(),
@@ -618,7 +642,16 @@ int32_t PepperGraphics2DHost::Flush(PP_Resource* old_image_data) {
                                done_replace_contents ? NULL : old_image_data);
         done_replace_contents = true;
         break;
+      case QueuedOperation::SET_OFFSET:
+        old_op_rect = gfx::Rect(plugin_offset_.x(), plugin_offset_.y(),
+            image_data_->width(), image_data_->height());
+        plugin_offset_ = operation.offset;
+        // The offset is applied below for |op_rect|.
+        op_rect = gfx::Rect(image_data_->width(), image_data_->height());
+        break;
     }
+
+    op_rect.Offset(plugin_offset_.x(), plugin_offset_.y());
 
     // For correctness with accelerated compositing, we must issue an invalidate
     // on the full op_rect even if it is partially or completely off-screen.
@@ -626,8 +659,11 @@ int32_t PepperGraphics2DHost::Flush(PP_Resource* old_image_data) {
     // do nothing and we won't get any ViewFlushedPaint calls, leaving our
     // callback stranded. So we still need to check whether the repainted area
     // is visible to determine how to deal with the callback.
-    if (bound_instance_ && !op_rect.IsEmpty()) {
+    if (bound_instance_ && (!op_rect.IsEmpty() || !old_op_rect.IsEmpty())) {
       gfx::Point scroll_delta(operation.scroll_dx, operation.scroll_dy);
+      if (!ConvertToLogicalPixels(scale_, &old_op_rect, NULL)) {
+        NOTREACHED();
+      }
       if (!ConvertToLogicalPixels(scale_,
                                   &op_rect,
                                   operation.type == QueuedOperation::SCROLL ?
@@ -641,22 +677,30 @@ int32_t PepperGraphics2DHost::Flush(PP_Resource* old_image_data) {
 
       // Set |no_update_visible| to false if the change overlaps the visible
       // area.
-      gfx::Rect visible_changed_rect = gfx::IntersectRects(clip, op_rect);
-      if (!visible_changed_rect.IsEmpty())
+      if (!gfx::IntersectRects(clip, op_rect).IsEmpty() ||
+          !gfx::IntersectRects(clip, old_op_rect).IsEmpty()) {
         no_update_visible = false;
+      }
 
       // Notify the plugin of the entire change (op_rect), even if it is
       // partially or completely off-screen.
       if (operation.type == QueuedOperation::SCROLL) {
         bound_instance_->ScrollRect(scroll_delta.x(), scroll_delta.y(),
                                     op_rect);
+        DCHECK(old_op_rect.IsEmpty());
       } else {
-        bound_instance_->InvalidateRect(op_rect);
+        if (!op_rect.IsEmpty())
+          bound_instance_->InvalidateRect(op_rect);
+        if (!old_op_rect.IsEmpty())
+          bound_instance_->InvalidateRect(old_op_rect);
       }
       texture_mailbox_modified_ = true;
     }
   }
   queued_operations_.clear();
+
+  if (bound_instance_)
+    bound_instance_->UpdateLayerTransform();
 
   if (!bound_instance_) {
     // As promised in the API, we always schedule callback when unbound.

content/renderer/pepper/pepper_plugin_instance_impl.cc
@@ -111,9 +111,11 @@
 #include "third_party/skia/include/core/SkRect.h"
 #include "ui/gfx/image/image_skia.h"
 #include "ui/gfx/image/image_skia_rep.h"
+#include "ui/gfx/point_conversions.h"
 #include "ui/gfx/range/range.h"
 #include "ui/gfx/rect_conversions.h"
 #include "ui/gfx/scoped_ns_graphics_context_save_gstate_mac.h"
+#include "ui/gfx/size_conversions.h"
 #include "v8/include/v8.h"
 #include "webkit/renderer/compositor_bindings/web_layer_impl.h"
 
@@ -1880,6 +1882,7 @@ void PepperPluginInstanceImpl::UpdateLayer() {
   }
   if (want_layer) {
     bool opaque = false;
+    scoped_refptr<cc::Layer> plugin_layer;
     if (want_3d_layer) {
       DCHECK(bound_graphics_3d_.get());
       texture_layer_ = cc::TextureLayer::CreateForMailbox(NULL);
@@ -1887,14 +1890,24 @@ void PepperPluginInstanceImpl::UpdateLayer() {
       texture_layer_->SetTextureMailbox(
           cc::TextureMailbox(mailbox, 0),
           cc::SingleReleaseCallback::Create(base::Bind(&IgnoreCallback)));
+      plugin_layer = texture_layer_;
     } else {
       DCHECK(bound_graphics_2d_platform_);
       texture_layer_ = cc::TextureLayer::CreateForMailbox(this);
       bound_graphics_2d_platform_->AttachedToNewLayer();
       opaque = bound_graphics_2d_platform_->IsAlwaysOpaque();
       texture_layer_->SetFlipped(false);
+      texture_layer_->SetBounds(bound_graphics_2d_platform_->Size());
+      texture_layer_->SetIsDrawable(true);
+      // WebLayer sets the bounds of its containing layer to be the same size as
+      // the plugin DOM element. Because we want to allow scaling/positioning of
+      // the layer in the 2D case (for SetOffset) we need to add this
+      // intermediate layer. If we wanted to support this functionality for the
+      // 3D case, we would need an intermediate layer in that case also.
+      plugin_layer = cc::Layer::Create();
+      plugin_layer->AddChild(texture_layer_);
     }
-    web_layer_.reset(new webkit::WebLayerImpl(texture_layer_));
+    web_layer_.reset(new webkit::WebLayerImpl(plugin_layer));
     if (fullscreen_container_) {
       fullscreen_container_->SetLayer(web_layer_.get());
       // Ignore transparency in fullscreen, since that's what Flash always
@@ -1908,6 +1921,7 @@ void PepperPluginInstanceImpl::UpdateLayer() {
   }
   layer_bound_to_fullscreen_ = !!fullscreen_container_;
   layer_is_hardware_ = want_3d_layer;
+  UpdateLayerTransform();
 }
 
 unsigned PepperPluginInstanceImpl::PrepareTexture() {
@@ -1922,6 +1936,35 @@ bool PepperPluginInstanceImpl::PrepareTextureMailbox(
     return false;
   return bound_graphics_2d_platform_->PrepareTextureMailbox(
       mailbox, release_callback);
+}
+
+void PepperPluginInstanceImpl::UpdateLayerTransform() {
+  if (!bound_graphics_2d_platform_ || !texture_layer_) {
+    // Currently the transform is only applied for Graphics2D.
+    return;
+  }
+
+  // TODO(raymes): Get the scale from the Graphics2D host.
+  gfx::PointF scale(1.0f, 1.0f);
+
+  gfx::RectF backing_store(bound_graphics_2d_platform_->plugin_offset(),
+                           bound_graphics_2d_platform_->Size());
+  backing_store = ScaleRect(backing_store, scale.x(), scale.y());
+
+  gfx::Rect layer_rect(ToEnclosingRect(backing_store));
+  gfx::Size plugin_size(PP_ToGfxSize(view_data_.rect.size));
+  layer_rect.Intersect(gfx::Rect(plugin_size));
+  texture_layer_->SetPosition(layer_rect.origin());
+  texture_layer_->SetBounds(layer_rect.size());
+
+  gfx::PointF u(PointAtOffsetFromOrigin(
+      layer_rect.origin() - backing_store.origin()));
+  gfx::PointF v(PointAtOffsetFromOrigin(
+      layer_rect.bottom_right() - backing_store.origin()));
+  gfx::RectF uv(BoundingRect(u, v));
+  if (!backing_store.IsEmpty())
+    uv.Scale(1.0f / backing_store.width(), 1.0f / backing_store.height());
+  texture_layer_->SetUV(uv.origin(), uv.bottom_right());
 }
 
 void PepperPluginInstanceImpl::AddPluginObject(PluginObject* plugin_object) {

content/renderer/pepper/pepper_plugin_instance_impl.h
@@ -503,6 +503,9 @@ class CONTENT_EXPORT PepperPluginInstanceImpl
       scoped_ptr<cc::SingleReleaseCallback>* release_callback,
       bool use_shared_memory) OVERRIDE;
 
+  // Update any transforms that should be applied to the texture layer.
+  void UpdateLayerTransform();
+
  private:
   friend class base::RefCounted<PepperPluginInstanceImpl>;
   friend class PpapiUnittest;
@@ -596,8 +599,8 @@ class CONTENT_EXPORT PepperPluginInstanceImpl
 
   // Updates the layer for compositing. This creates a layer and attaches to the
   // container if:
-  // - we have a bound Graphics3D
-  // - the Graphics3D has a texture
+  // - we have a bound Graphics3D and the Graphics3D has a texture, OR
+  //   we have a bound Graphics2D and are using software compositing
   // - we are not in Flash full-screen mode (or transitioning to it)
   // Otherwise it destroys the layer.
   // It does either operation lazily.

content/content_tests.gypi
@@ -977,6 +977,7 @@
             'renderer/cpp_bound_class_unittest.cc',
             'renderer/dom_serializer_browsertest.cc',
             'renderer/mouse_lock_dispatcher_browsertest.cc',
+            'renderer/pepper/fake_pepper_plugin_instance.cc',
             'renderer/pepper/mock_renderer_ppapi_host.cc',
             'renderer/pepper/pepper_device_enumeration_host_helper_unittest.cc',
             'renderer/pepper/pepper_file_chooser_host_unittest.cc',

content/renderer/pepper/fake_pepper_plugin_instance.cc
@@ -1,0 +1,79 @@
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/renderer/pepper/fake_pepper_plugin_instance.h"
+
+#include "ppapi/c/pp_errors.h"
+#include "ppapi/shared_impl/ppapi_permissions.h"
+
+namespace content {
+
+FakePepperPluginInstance::~FakePepperPluginInstance() {}
+
+content::RenderView* FakePepperPluginInstance::GetRenderView() {
+  return NULL;
+}
+
+blink::WebPluginContainer* FakePepperPluginInstance::GetContainer() {
+  return NULL;
+}
+
+v8::Isolate* FakePepperPluginInstance::GetIsolate() const {
+  return NULL;
+}
+
+ppapi::VarTracker* FakePepperPluginInstance::GetVarTracker() {
+  return NULL;
+}
+
+const GURL& FakePepperPluginInstance::GetPluginURL() {
+  return gurl_;
+}
+
+base::FilePath FakePepperPluginInstance::GetModulePath() {
+  return base::FilePath();
+}
+
+PP_Resource FakePepperPluginInstance::CreateImage(gfx::ImageSkia* source_image,
+                                                  float scale) {
+  return 0;
+}
+
+PP_ExternalPluginResult FakePepperPluginInstance::SwitchToOutOfProcessProxy(
+    const base::FilePath& file_path,
+    ppapi::PpapiPermissions permissions,
+    const IPC::ChannelHandle& channel_handle,
+    base::ProcessId plugin_pid,
+    int plugin_child_id) {
+  return PP_EXTERNAL_PLUGIN_FAILED;
+}
+
+void FakePepperPluginInstance::SetAlwaysOnTop(bool on_top) {}
+
+bool FakePepperPluginInstance::IsFullPagePlugin() {
+  return false;
+}
+
+bool FakePepperPluginInstance::FlashSetFullscreen(bool fullscreen,
+                                                  bool delay_report) {
+  return false;
+}
+
+bool FakePepperPluginInstance::IsRectTopmost(const gfx::Rect& rect) {
+  return false;
+}
+
+int32_t FakePepperPluginInstance::Navigate(
+    const ppapi::URLRequestInfoData& request,
+    const char* target,
+    bool from_user_action) {
+  return PP_ERROR_FAILED;
+}
+
+int FakePepperPluginInstance::MakePendingFileRefRendererHost(
+    const base::FilePath& path) {
+  return 0;
+}
+
+}  // namespace content

content/renderer/pepper/fake_pepper_plugin_instance.h
@@ -1,0 +1,48 @@
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_RENDERER_PEPPER_FAKE_PEPPER_PLUGIN_INSTANCE_H_
+#define CONTENT_RENDERER_PEPPER_FAKE_PEPPER_PLUGIN_INSTANCE_H_
+
+#include "content/public/renderer/pepper_plugin_instance.h"
+#include "url/gurl.h"
+
+namespace content {
+
+class FakePepperPluginInstance : public PepperPluginInstance {
+ public:
+  virtual ~FakePepperPluginInstance();
+
+  // PepperPluginInstance overrides.
+  virtual content::RenderView* GetRenderView() OVERRIDE;
+  virtual blink::WebPluginContainer* GetContainer() OVERRIDE;
+  virtual v8::Isolate* GetIsolate() const OVERRIDE;
+  virtual ppapi::VarTracker* GetVarTracker() OVERRIDE;
+  virtual const GURL& GetPluginURL() OVERRIDE;
+  virtual base::FilePath GetModulePath() OVERRIDE;
+  virtual PP_Resource CreateImage(gfx::ImageSkia* source_image,
+                                  float scale) OVERRIDE;
+  virtual PP_ExternalPluginResult SwitchToOutOfProcessProxy(
+      const base::FilePath& file_path,
+      ppapi::PpapiPermissions permissions,
+      const IPC::ChannelHandle& channel_handle,
+      base::ProcessId plugin_pid,
+      int plugin_child_id) OVERRIDE;
+  virtual void SetAlwaysOnTop(bool on_top) OVERRIDE;
+  virtual bool IsFullPagePlugin() OVERRIDE;
+  virtual bool FlashSetFullscreen(bool fullscreen, bool delay_report) OVERRIDE;
+  virtual bool IsRectTopmost(const gfx::Rect& rect) OVERRIDE;
+  virtual int32_t Navigate(const ppapi::URLRequestInfoData& request,
+                           const char* target,
+                           bool from_user_action) OVERRIDE;
+  virtual int MakePendingFileRefRendererHost(
+      const base::FilePath& path) OVERRIDE;
+
+ private:
+  GURL gurl_;
+};
+
+}  // namespace content
+
+#endif  // CONTENT_RENDERER_PEPPER_FAKE_PEPPER_PLUGIN_INSTANCE_H_

content/renderer/pepper/mock_renderer_ppapi_host.cc
@@ -4,6 +4,7 @@
 
 #include "content/renderer/pepper/mock_renderer_ppapi_host.h"
 
+#include "content/renderer/pepper/fake_pepper_plugin_instance.h"
 #include "ui/gfx/point.h"
 
 namespace content {
@@ -14,7 +15,8 @@ MockRendererPpapiHost::MockRendererPpapiHost(RenderView* render_view,
       ppapi_host_(&sink_, ppapi::PpapiPermissions()),
       render_view_(render_view),
       pp_instance_(instance),
-      has_user_gesture_(false) {
+      has_user_gesture_(false),
+      plugin_instance_(new FakePepperPluginInstance) {
 }
 
 MockRendererPpapiHost::~MockRendererPpapiHost() {
@@ -30,8 +32,7 @@ bool MockRendererPpapiHost::IsValidInstance(PP_Instance instance) const {
 
 PepperPluginInstance* MockRendererPpapiHost::GetPluginInstance(
     PP_Instance instance) const {
-  NOTIMPLEMENTED();
-  return NULL;
+  return plugin_instance_.get();
 }
 
 RenderView* MockRendererPpapiHost::GetRenderViewForInstance(
@@ -74,7 +75,6 @@ IPC::PlatformFileForTransit MockRendererPpapiHost::ShareHandleWithRemote(
 }
 
 bool MockRendererPpapiHost::IsRunningInProcess() const {
-  NOTIMPLEMENTED();
   return false;
 }
 

content/renderer/pepper/mock_renderer_ppapi_host.h
@@ -6,12 +6,14 @@
 #define CONTENT_RENDERER_PEPPER_MOCK_RENDERER_PPAPI_HOST_H_
 
 #include "base/basictypes.h"
+#include "base/memory/scoped_ptr.h"
 #include "content/public/renderer/renderer_ppapi_host.h"
 #include "content/renderer/pepper/content_renderer_pepper_host_factory.h"
 #include "ppapi/host/ppapi_host.h"
 #include "ppapi/proxy/resource_message_test_sink.h"
 
 namespace content {
+class FakePepperPluginInstance;
 class PluginModule;
 
 // A mock RendererPpapiHost for testing resource hosts. Messages sent by
@@ -64,6 +66,8 @@ class MockRendererPpapiHost : public RendererPpapiHost {
   PP_Instance pp_instance_;
 
   bool has_user_gesture_;
+
+  scoped_ptr<FakePepperPluginInstance> plugin_instance_;
 
   DISALLOW_COPY_AND_ASSIGN(MockRendererPpapiHost);
 };

commit 1d40b852959fb7df88a9a74ec1eaaa8ad6e38c4b

Disable UpdateLayerTransform on PepperPluginInstanceImpl
This disables layer transforms that were applied as a result of SetOffset
and SetResizeMode in PPB_Graphics2D_Dev. These transforms are not currently
being used.

This fixes a regression introduced in r235581/https://codereview.chromium.org/25661002
which was caused due to the fact that an intermediate cc layer was added.
This resulted in certain properties (namely visibility) not being applied to the
child layer correctly.

The remainder of this CL will also be reverted since it's not currently being used
but this CL is designed to be merged, which is why it is being kept small.

BUG=348694

Review URL: https://codereview.chromium.org/183923022

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254732 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 9824fc54128a85c3d373370d0ec3bcd9ec59e9db

content/renderer/pepper/pepper_graphics_2d_host.cc
@@ -424,8 +424,6 @@ void PepperGraphics2DHost::ViewFlushedPaint() {
 void PepperGraphics2DHost::DidChangeView(const ppapi::ViewData& view_data) {
   gfx::Size old_plugin_size = current_plugin_size_;
   current_plugin_size_ = PP_ToGfxSize(view_data.rect.size);
-  if (bound_instance_)
-    bound_instance_->UpdateLayerTransform();
 }
 
 void PepperGraphics2DHost::SetScale(float scale) {
@@ -740,9 +738,6 @@ int32_t PepperGraphics2DHost::Flush(PP_Resource* old_image_data,
   queued_operations_.clear();
 
   flushed_plugin_size_ = flushed_plugin_size;
-  if (bound_instance_)
-    bound_instance_->UpdateLayerTransform();
-
   if (!bound_instance_) {
     // As promised in the API, we always schedule callback when unbound.
     ScheduleOffscreenFlushAck();

content/renderer/pepper/pepper_plugin_instance_impl.cc
@@ -115,11 +115,9 @@
 #include "third_party/skia/include/core/SkRect.h"
 #include "ui/gfx/image/image_skia.h"
 #include "ui/gfx/image/image_skia_rep.h"
-#include "ui/gfx/point_conversions.h"
 #include "ui/gfx/range/range.h"
 #include "ui/gfx/rect_conversions.h"
 #include "ui/gfx/scoped_ns_graphics_context_save_gstate_mac.h"
-#include "ui/gfx/size_conversions.h"
 #include "v8/include/v8.h"
 #include "webkit/renderer/compositor_bindings/web_layer_impl.h"
 
@@ -1936,7 +1934,6 @@ void PepperPluginInstanceImpl::UpdateLayer() {
   if ((want_layer == !!texture_layer_.get()) &&
       (want_3d_layer == layer_is_hardware_) &&
       layer_bound_to_fullscreen_ == !!fullscreen_container_) {
-    UpdateLayerTransform();
     return;
   }
 
@@ -1950,7 +1947,6 @@ void PepperPluginInstanceImpl::UpdateLayer() {
   }
   if (want_layer) {
     bool opaque = false;
-    scoped_refptr<cc::Layer> plugin_layer;
     if (want_3d_layer) {
       DCHECK(bound_graphics_3d_.get());
       texture_layer_ = cc::TextureLayer::CreateForMailbox(NULL);
@@ -1958,24 +1954,14 @@ void PepperPluginInstanceImpl::UpdateLayer() {
       texture_layer_->SetTextureMailbox(
           cc::TextureMailbox(mailbox, GL_TEXTURE_2D, 0),
           cc::SingleReleaseCallback::Create(base::Bind(&IgnoreCallback)));
-      plugin_layer = texture_layer_;
     } else {
       DCHECK(bound_graphics_2d_platform_);
       texture_layer_ = cc::TextureLayer::CreateForMailbox(this);
       bound_graphics_2d_platform_->AttachedToNewLayer();
       opaque = bound_graphics_2d_platform_->IsAlwaysOpaque();
       texture_layer_->SetFlipped(false);
-      texture_layer_->SetBounds(bound_graphics_2d_platform_->Size());
-      texture_layer_->SetIsDrawable(true);
-      // WebLayer sets the bounds of its containing layer to be the same size as
-      // the plugin DOM element. Because we want to allow scaling/positioning of
-      // the layer in the 2D case (for SetOffset) we need to add this
-      // intermediate layer. If we wanted to support this functionality for the
-      // 3D case, we would need an intermediate layer in that case also.
-      plugin_layer = cc::Layer::Create();
-      plugin_layer->AddChild(texture_layer_);
     }
-    web_layer_.reset(new webkit::WebLayerImpl(plugin_layer));
+    web_layer_.reset(new webkit::WebLayerImpl(texture_layer_));
     if (fullscreen_container_) {
       fullscreen_container_->SetLayer(web_layer_.get());
       // Ignore transparency in fullscreen, since that's what Flash always
@@ -1989,7 +1975,6 @@ void PepperPluginInstanceImpl::UpdateLayer() {
   }
   layer_bound_to_fullscreen_ = !!fullscreen_container_;
   layer_is_hardware_ = want_3d_layer;
-  UpdateLayerTransform();
 }
 
 unsigned PepperPluginInstanceImpl::PrepareTexture() {
@@ -2008,35 +1993,6 @@ bool PepperPluginInstanceImpl::PrepareTextureMailbox(
 
 void PepperPluginInstanceImpl::OnDestruct() {
   render_frame_ = NULL;
-}
-
-void PepperPluginInstanceImpl::UpdateLayerTransform() {
-  if (!bound_graphics_2d_platform_ || !texture_layer_) {
-    // Currently the transform is only applied for Graphics2D.
-    return;
-  }
-
-  gfx::RectF backing_store(bound_graphics_2d_platform_->plugin_offset(),
-                           bound_graphics_2d_platform_->Size());
-  gfx::PointF scale = gfx::ScalePoint(
-      bound_graphics_2d_platform_->GetResizeScale(),
-      bound_graphics_2d_platform_->GetScale());
-  backing_store = ScaleRect(backing_store, scale.x(), scale.y());
-
-  gfx::Rect layer_rect(ToEnclosingRect(backing_store));
-  gfx::Size plugin_size(PP_ToGfxSize(view_data_.rect.size));
-  layer_rect.Intersect(gfx::Rect(plugin_size));
-  texture_layer_->SetPosition(layer_rect.origin());
-  texture_layer_->SetBounds(layer_rect.size());
-
-  gfx::PointF u(PointAtOffsetFromOrigin(
-      layer_rect.origin() - backing_store.origin()));
-  gfx::PointF v(PointAtOffsetFromOrigin(
-      layer_rect.bottom_right() - backing_store.origin()));
-  gfx::RectF uv(BoundingRect(u, v));
-  if (!backing_store.IsEmpty())
-    uv.Scale(1.0f / backing_store.width(), 1.0f / backing_store.height());
-  texture_layer_->SetUV(uv.origin(), uv.bottom_right());
 }
 
 void PepperPluginInstanceImpl::AddPluginObject(PluginObject* plugin_object) {

content/renderer/pepper/pepper_plugin_instance_impl.h
@@ -514,9 +514,6 @@ class CONTENT_EXPORT PepperPluginInstanceImpl
   // RenderFrameObserver
   virtual void OnDestruct() OVERRIDE;
 
-  // Update any transforms that should be applied to the texture layer.
-  void UpdateLayerTransform();
-
  private:
   friend class base::RefCounted<PepperPluginInstanceImpl>;
   friend class PpapiUnittest;