CyanogenMod/android_external_chromium_org

commit b0fa3e42d6f144a1b2a8bb545a23c79d4de9e288

Files.app: Fix flakiness in chaining directory
I'm about to commit a patch to improve of filelist draw. But it makes some tests flaky because the currently directory changes faster than before. This patch makes the logic of directory changing more robust.

BUG=367123
TEST=browser_test passes

Review URL: https://codereview.chromium.org/312493002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275418 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 9e7e75d5f0d0d6e1ac2eba87e7385c02f175e3f5

ui/file_manager/file_manager/foreground/js/directory_model.js
@@ -32,6 +32,8 @@ function DirectoryModel(singleSelection, fileFilter, fileWatcher,
   this.scanFailures_ = 0;
   this.changeDirectorySequence_ = 0;
 
+  this.directoryChangeQueue_ = new AsyncUtil.Queue();
+
   this.fileFilter_ = fileFilter;
   this.fileFilter_.addEventListener('changed',
                                     this.onFilterChanged_.bind(this));
@@ -278,8 +280,14 @@ DirectoryModel.prototype.scheduleRescan = function(delay) {
     clearTimeout(this.rescanTimeoutId_);
   }
 
+  var sequence = this.changeDirectorySequence_;
+
   this.rescanTime_ = Date.now() + delay;
-  this.rescanTimeoutId_ = setTimeout(this.rescan.bind(this), delay);
+  this.rescanTimeoutId_ = setTimeout(function() {
+    this.rescanTimeoutId_ = null;
+    if (sequence === this.changeDirectorySequence_)
+      this.rescan();
+  }.bind(this), delay);
 };
 
 /**
@@ -312,9 +320,13 @@ DirectoryModel.prototype.rescan = function() {
   var dirContents = this.currentDirContents_.clone();
   dirContents.setFileList([]);
 
+  var sequence = this.changeDirectorySequence_;
+
   var successCallback = (function() {
-    this.replaceDirectoryContents_(dirContents);
-    cr.dispatchSimpleEvent(this, 'rescan-completed');
+    if (sequence === this.changeDirectorySequence_) {
+      this.replaceDirectoryContents_(dirContents);
+      cr.dispatchSimpleEvent(this, 'rescan-completed');
+    }
   }).bind(this);
 
   this.scan_(dirContents,
@@ -329,11 +341,12 @@ DirectoryModel.prototype.rescan = function() {
  *
  * @param {DirectoryContentes} newDirContents New DirectoryContents instance to
  *     replace currentDirContents_.
- * @param {function()=} opt_callback Called on success.
+ * @param {function(boolean)} callback Callback with result. True if the scan
+ *     is completed successfully, false if the scan is failed.
  * @private
  */
 DirectoryModel.prototype.clearAndScan_ = function(newDirContents,
-                                                  opt_callback) {
+                                                  callback) {
   if (this.currentDirContents_.isScanning())
     this.currentDirContents_.cancelScan();
   this.currentDirContents_ = newDirContents;
@@ -348,22 +361,46 @@ DirectoryModel.prototype.clearAndScan_ = function(newDirContents,
     this.runningScan_ = null;
   }
 
+  var sequence = this.changeDirectorySequence_;
+  var cancelled = false;
+
   var onDone = function() {
+    if (cancelled)
+      return;
+
     cr.dispatchSimpleEvent(this, 'scan-completed');
-    if (opt_callback)
-      opt_callback();
+    callback(true);
   }.bind(this);
 
   var onFailed = function() {
+    if (cancelled)
+      return;
+
     cr.dispatchSimpleEvent(this, 'scan-failed');
+    callback(false);
   }.bind(this);
 
   var onUpdated = function() {
+    if (cancelled)
+      return;
+
+    if (this.changeDirectorySequence_ !== sequence) {
+      cancelled = true;
+      cr.dispatchSimpleEvent(this, 'scan-cancelled');
+      callback(false);
+      return;
+    }
+
     cr.dispatchSimpleEvent(this, 'scan-updated');
   }.bind(this);
 
   var onCancelled = function() {
+    if (cancelled)
+      return;
+
+    cancelled = true;
     cr.dispatchSimpleEvent(this, 'scan-cancelled');
+    callback(false);
   }.bind(this);
 
   // Clear the table, and start scanning.
@@ -602,8 +639,7 @@ DirectoryModel.prototype.createDirectory = function(name,
     return;
   }
 
-  var tracker = this.createDirectoryChangeTracker();
-  tracker.start();
+  var sequence = this.changeDirectorySequence_;
 
   new Promise(entry.getDirectory.bind(
       entry, name, {create: true, exclusive: true})).
@@ -621,8 +657,7 @@ DirectoryModel.prototype.createDirectory = function(name,
       then(function(newEntry) {
         // Do not change anything or call the callback if current
         // directory changed.
-        tracker.stop();
-        if (tracker.hasChanged) {
+        if (this.changeDirectorySequence_ !== sequence) {
           abortCallback();
           return;
         }
@@ -641,7 +676,6 @@ DirectoryModel.prototype.createDirectory = function(name,
           successCallback(newEntry);
         }
       }.bind(this), function(reason) {
-        tracker.stop();
         errorCallback(reason);
       });
 };
@@ -668,26 +702,38 @@ DirectoryModel.prototype.changeDirectoryEntry = function(
   this.changeDirectorySequence_++;
   this.clearSearch_();
 
-  var promise = new Promise(
-      function(onFulfilled, onRejected) {
-        this.fileWatcher_.changeWatchedDirectory(dirEntry, onFulfilled);
-      }.bind(this)).
-
-      then(function(sequence) {
-        return new Promise(function(onFulfilled, onRejected) {
-          if (this.changeDirectorySequence_ !== sequence)
+  this.directoryChangeQueue_.run(function(sequence, queueTaskCallback) {
+    this.fileWatcher_.changeWatchedDirectory(
+        dirEntry,
+        function() {
+          if (this.changeDirectorySequence_ !== sequence) {
+            queueTaskCallback();
             return;
+          }
 
           var newDirectoryContents = this.createDirectoryContents_(
               this.currentFileListContext_, dirEntry, '');
-          if (!newDirectoryContents)
+          if (!newDirectoryContents) {
+            queueTaskCallback();
             return;
+          }
 
-          var previousDirEntry = this.currentDirContents_.getDirectoryEntry();
-          this.clearAndScan_(newDirectoryContents, opt_callback);
+          var previousDirEntry =
+              this.currentDirContents_.getDirectoryEntry();
+          this.clearAndScan_(
+              newDirectoryContents,
+              function(result) {
+                // Calls the callback of the method when successful.
+                if (result && opt_callback)
+                  opt_callback();
 
-          // For tests that open the dialog to empty directories, everything is
-          // loaded at this point.
+                // Notify that the current task of this.directoryChangeQueue_
+                // is completed.
+                setTimeout(queueTaskCallback);
+              });
+
+          // For tests that open the dialog to empty directories, everything
+          // is loaded at this point.
           util.testSendMessage('directory-change-complete');
 
           var event = new Event('directory-changed');
@@ -695,7 +741,7 @@ DirectoryModel.prototype.changeDirectoryEntry = function(
           event.newDirEntry = dirEntry;
           this.dispatchEvent(event);
         }.bind(this));
-      }.bind(this, this.changeDirectorySequence_));
+  }.bind(this, this.changeDirectorySequence_));
 };
 
 /**
@@ -903,25 +949,41 @@ DirectoryModel.prototype.search = function(query,
     return;
   }
 
-  if (!(query || '').trimLeft()) {
-    if (this.isSearching()) {
-      var newDirContents = this.createDirectoryContents_(
-          this.currentFileListContext_,
-          currentDirEntry);
-      this.clearAndScan_(newDirContents);
+  this.changeDirectorySequence_++;
+  this.directoryChangeQueue_.run(function(sequence, callback) {
+    if (this.changeDirectorySequence_ !== sequence) {
+      callback();
+      return;
     }
-    return;
-  }
 
-  var newDirContents = this.createDirectoryContents_(
-      this.currentFileListContext_, currentDirEntry, query);
-  if (!newDirContents)
-    return;
+    if (!(query || '').trimLeft()) {
+      if (this.isSearching()) {
+        var newDirContents = this.createDirectoryContents_(
+            this.currentFileListContext_,
+            currentDirEntry);
+        this.clearAndScan_(newDirContents,
+                           sequence,
+                           callback);
+      } else {
+        callback();
+      }
+      return;
+    }
 
-  this.onSearchCompleted_ = onSearchRescan;
-  this.onClearSearch_ = onClearSearch;
-  this.addEventListener('scan-completed', this.onSearchCompleted_);
-  this.clearAndScan_(newDirContents);
+    var newDirContents = this.createDirectoryContents_(
+        this.currentFileListContext_, currentDirEntry, query);
+    if (!newDirContents) {
+      callback();
+      return;
+    }
+
+    this.onSearchCompleted_ = onSearchRescan;
+    this.onClearSearch_ = onClearSearch;
+    this.addEventListener('scan-completed', this.onSearchCompleted_);
+    this.clearAndScan_(newDirContents,
+                       sequence,
+                       callback);
+  }.bind(this, this.changeDirectorySequence_));
 };
 
 /**

commit 2054f83628074047948aa4d769d79c94ad7dbc89

Files.app: Fix the bug on search
On search on Files.app, it fails due to exception, because of the regression caused by r275418. This patch fixes it.

BUG=382332
TEST=manually tested

Review URL: https://codereview.chromium.org/321753003

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275791 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: c4310f1a35ef1d8a744d41ab2bc472240f8e46fa

ui/file_manager/file_manager/foreground/js/directory_model.js
@@ -963,7 +963,6 @@ DirectoryModel.prototype.search = function(query,
             this.currentFileListContext_,
             currentDirEntry);
         this.clearAndScan_(newDirContents,
-                           sequence,
                            callback);
       } else {
         callback();
@@ -982,7 +981,6 @@ DirectoryModel.prototype.search = function(query,
     this.onClearSearch_ = onClearSearch;
     this.addEventListener('scan-completed', this.onSearchCompleted_);
     this.clearAndScan_(newDirContents,
-                       sequence,
                        callback);
   }.bind(this, this.changeDirectorySequence_));
 };