9cd59af7d33dc6791dc8a89b9010cdc39c431f42
Ben Murdoch <benm@google.com>
2014-08-01 23:09:35 +0100

    Attempt to fix the multi-arch webviewchromium build.
    Change-Id: Ib0de2c04413c64c341a9d8f90fa3e559e64a446a

--- android_webview/build/resources_config.mk
+++ android_webview/build/resources_config.mk
@@ -17,10 +17,10 @@ android_webview_manifest_file := $(call my-dir)/AndroidManifest.xml
 # We defeat the optimization by including an empty values.xml file in the list.
 android_webview_resources_dirs := \
     $(call my-dir)/res_hack \
-    $(call intermediates-dir-for,GYP,shared,,,$(TARGET_2ND_ARCH))/android_webview_jarjar_content_resources/jarjar_res \
-    $(call intermediates-dir-for,GYP,shared,,,$(TARGET_2ND_ARCH))/android_webview_jarjar_ui_resources/jarjar_res \
-    $(call intermediates-dir-for,GYP,ui_strings_grd,,,$(TARGET_2ND_ARCH))/ui_strings_grd/res_grit \
-    $(call intermediates-dir-for,GYP,content_strings_grd,,,$(TARGET_2ND_ARCH))/content_strings_grd/res_grit
+    $(call intermediates-dir-for,GYP,shared)/android_webview_jarjar_content_resources/jarjar_res \
+    $(call intermediates-dir-for,GYP,shared)/android_webview_jarjar_ui_resources/jarjar_res \
+    $(call intermediates-dir-for,GYP,ui_strings_grd)/ui_strings_grd/res_grit \
+    $(call intermediates-dir-for,GYP,content_strings_grd)/content_strings_grd/res_grit
 
 android_webview_asset_dirs := \
     $(call intermediates-dir-for,APPS,webviewchromium-paks)
