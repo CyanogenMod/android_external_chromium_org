647e5a2ea1586cff5729c048d0cada06cfa3acd1
vollick <vollick@chromium.org>
2014-09-13 13:36:44 -0700

    Cherry-pick: Fix RemoveFromScrollTree and RemoveFromClipTree
    Clean cherry-pick of chromium
    crrev.com/66133e86b6d79534605539aa684a248e6b6205bf
    
    BUG: 17612259
    
    Original description:
    
    These functions should set needs commit, but didn't. This lead to stale
    pointers in the impl tree.
    
    Review URL: https://codereview.chromium.org/572483002
    
    Change-Id: I59ba963f2ad44aa6f64366ca30c7a64a97570fb3
    Cr-Commit-Position: refs/heads/master@{#294749}

--- cc/layers/layer.cc
+++ cc/layers/layer.cc
@@ -87,6 +87,9 @@ Layer::~Layer() {
   layer_animation_controller_->RemoveValueObserver(this);
   layer_animation_controller_->remove_value_provider(this);
 
+  RemoveFromScrollTree();
+  RemoveFromClipTree();
+
   // Remove the parent reference from all children and dependents.
   RemoveAllChildren();
   if (mask_layer_.get()) {
@@ -97,9 +100,6 @@ Layer::~Layer() {
     DCHECK_EQ(this, replica_layer_->parent());
     replica_layer_->RemoveFromParent();
   }
-
-  RemoveFromScrollTree();
-  RemoveFromClipTree();
 }
 
 void Layer::SetLayerTreeHost(LayerTreeHost* host) {
@@ -1159,28 +1159,24 @@ bool Layer::SupportsLCDText() const {
 
 void Layer::RemoveFromScrollTree() {
   if (scroll_children_.get()) {
-    for (std::set<Layer*>::iterator it = scroll_children_->begin();
-        it != scroll_children_->end(); ++it)
-      (*it)->scroll_parent_ = NULL;
+    std::set<Layer*> copy = *scroll_children_;
+    for (std::set<Layer*>::iterator it = copy.begin(); it != copy.end(); ++it)
+      (*it)->SetScrollParent(NULL);
   }
 
-  if (scroll_parent_)
-    scroll_parent_->RemoveScrollChild(this);
-
-  scroll_parent_ = NULL;
+  DCHECK(!scroll_children_);
+  SetScrollParent(NULL);
 }
 
 void Layer::RemoveFromClipTree() {
   if (clip_children_.get()) {
-    for (std::set<Layer*>::iterator it = clip_children_->begin();
-        it != clip_children_->end(); ++it)
-      (*it)->clip_parent_ = NULL;
+    std::set<Layer*> copy = *clip_children_;
+    for (std::set<Layer*>::iterator it = copy.begin(); it != copy.end(); ++it)
+      (*it)->SetClipParent(NULL);
   }
 
-  if (clip_parent_)
-    clip_parent_->RemoveClipChild(this);
-
-  clip_parent_ = NULL;
+  DCHECK(!clip_children_);
+  SetClipParent(NULL);
 }
 
 void Layer::RunMicroBenchmark(MicroBenchmark* benchmark) {
