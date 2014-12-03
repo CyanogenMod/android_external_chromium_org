48eef54c197d31144759c7f837427ef2d3a63a36
Hui Shu <hush@google.com>
2014-12-03 10:15:54 -0800

    Cherry pick: Attach ImeAdapter in CVC initialization.
    This is a clean cherry pick from:
    https://crrev.com/06091eaf4543692caf7a0bcaf5c3a550dfca7078
    
    We need to do this because in the Android WebView pop up flow, the
    native WebContents object could possibly have received RenderViewReady
    IPC message before the CVC is constructed and starts to observe the
    WebContents.
    
    BUG: 18527015
    Change-Id: Ibccac065c0607bd2ec45c0a9e152128ea8805b7e

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -625,6 +625,7 @@ public class ContentViewCore
         mRenderCoordinates.reset();
         initPopupZoomer(mContext);
         mImeAdapter = createImeAdapter(mContext);
+        attachImeAdapter();
 
         mAccessibilityInjector = AccessibilityInjector.newInstance(this);
 
