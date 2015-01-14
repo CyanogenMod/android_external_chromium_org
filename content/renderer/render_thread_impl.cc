mirror/chromium

prune resources in MemoryCache
The CL depends on https://codereview.chromium.org/774253007

BUG=

Review URL: https://codereview.chromium.org/757933004

content/renderer/render_thread_impl.cc
@@ -121,6 +121,7 @@
 #include "skia/ext/event_tracer_impl.h"
 #include "third_party/WebKit/public/platform/WebString.h"
 #include "third_party/WebKit/public/platform/WebThread.h"
+#include "third_party/WebKit/public/web/WebCache.h"
 #include "third_party/WebKit/public/web/WebColorName.h"
 #include "third_party/WebKit/public/web/WebDatabase.h"
 #include "third_party/WebKit/public/web/WebDocument.h"
@@ -1682,6 +1683,8 @@ void RenderThreadImpl::OnMemoryPressure(
     size_t font_cache_limit = SkGraphics::SetFontCacheLimit(0);
     SkGraphics::SetFontCacheLimit(font_cache_limit);
   }
+
+  blink::WebCache::pruneAll();
 }
 
 scoped_refptr<base::MessageLoopProxy>

CyanogenMod/android_external_chromium_org

Cr-Commit-Position: refs/heads/master@{#309641}
(cherry picked from commit e50637b323ad4cf0748085bd2cce54ec977f34c0 [formerly a35a480df159e2cc26e6088d7021e4e1763ba3b0])
Conflicts:
	content/renderer/render_thread_impl.cc
	third_party/speex
Change-Id: Ic8d9db8bbe4cda983bc6abcf050f850291c1d3cf
Former-commit-id: c7d54b15e4ed4478a55c910aba875d2bc666fbf3

content/renderer/render_thread_impl.cc
@@ -107,6 +107,7 @@
 #include "net/base/net_util.h"
 #include "skia/ext/event_tracer_impl.h"
 #include "third_party/WebKit/public/platform/WebString.h"
+#include "third_party/WebKit/public/platform/WebThread.h"
 #include "third_party/WebKit/public/web/WebColorName.h"
 #include "third_party/WebKit/public/web/WebDatabase.h"
 #include "third_party/WebKit/public/web/WebDocument.h"
@@ -1624,6 +1625,8 @@ void RenderThreadImpl::OnMemoryPressure(
     SkGraphics::PurgeFontCache();
     SkGraphics::SetFontCacheLimit(font_cache_limit);
   }
+
+  blink::WebCache::pruneAll();
 }
 
 scoped_refptr<base::MessageLoopProxy>